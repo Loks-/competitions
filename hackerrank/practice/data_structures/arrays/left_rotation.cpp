4839bf06e9a95bb252fa84756ed778cd099c6d3b
Alexey Poyarkov <alexeypoyarkov@gmail.com>
2018-06-03 23:09:08 -0700

    Add solution for "Left Rotation".

---
+++ hackerrank/practice/data_structures/arrays/left_rotation.cpp
@@ -0,0 +1,18 @@
+// https://www.hackerrank.com/challenges/array-left-rotation/
+
+#include "../../../../common/base.h"
+
+int main_left_rotation()
+{
+	unsigned n, d;
+	cin >> n >> d;
+	vector<unsigned> v(n);
+	for (unsigned i = 0; i < n; ++i)
+	{
+		cin >> v[(i + n - d) % n];
+	}
+	for (unsigned i : v)
+		cout << i << " ";
+	cout << endl;
+	return 0;
+}
