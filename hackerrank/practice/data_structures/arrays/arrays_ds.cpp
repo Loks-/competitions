dfd0230cbc992df0245e7c16562ba0adfaa59b4f
Alexey Poyarkov <alexeypoyarkov@gmail.com>
2018-06-03 22:43:21 -0700

    Add solution for "Arrays - DS" problem.

---
+++ hackerrank/practice/data_structures/arrays/arrays_ds.cpp
@@ -0,0 +1,17 @@
+// https://www.hackerrank.com/challenges/arrays-ds
+
+#include "../../../../common/base.h"
+
+int main_arrays_ds()
+{
+	unsigned n;
+	cin >> n;
+	vector<int> v(n);
+	for (unsigned i = 0; i < n; ++i)
+		cin >> v[i];
+	reverse(v.begin(), v.end());
+	for (unsigned j : v)
+		cout << j << " ";
+	cout << endl;
+	return 0;
+}
