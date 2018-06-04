15e38dac90b50b4979bb4d19d4348d2d68cbc0d4
Alexey Poyarkov <alexeypoyarkov@gmail.com>
2018-06-03 23:24:43 -0700

    Add solution for "Array Manipulation."

---
+++ hackerrank/practice/data_structures/arrays/array_manipulation.cpp
@@ -0,0 +1,25 @@
+// https://www.hackerrank.com/challenges/crush
+
+#include "../../../../common/base.h"
+
+int main_array_manipulation()
+{
+	unsigned n, m, a, b;
+	int64_t k;
+	cin >> n >> m;
+	vector<int64_t> v(n + 1, 0);
+	for (unsigned i = 0; i < m; ++i)
+	{
+		cin >> a >> b >> k;
+		v[a - 1] += k;
+		v[b] -= k;
+	}
+	int64_t current = 0, best = 0;
+	for (unsigned i = 0; i < n; ++i)
+	{
+		current += v[i];
+		best = max(best, current);
+	}
+	cout << best << endl;
+	return 0;
+}
