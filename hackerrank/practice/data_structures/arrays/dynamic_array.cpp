ec61aad9217bfe24927fc433c473f64fa32bfbdf
Alexey Poyarkov <alexeypoyarkov@gmail.com>
2018-06-03 23:03:56 -0700

    Add solution for "Dynamic Array".

---
+++ hackerrank/practice/data_structures/arrays/dynamic_array.cpp
@@ -0,0 +1,28 @@
+// https://www.hackerrank.com/challenges/dynamic-array
+
+#include "../../../../common/base.h"
+
+int main_dynamic_array()
+{
+	unsigned n, m, l = 0;
+	cin >> n >> m;
+	vector<vector<unsigned>> v(n);
+	for (unsigned i = 0; i < m; ++i)
+	{
+		unsigned t, x, y;
+		cin >> t >> x >> y;
+		if (t == 1)
+		{
+			v[(x ^ l) % n].push_back(y);
+		}
+		else if (t == 2)
+		{
+			auto& vr = v[(x ^ l) % n];
+			l = vr[y % (vr.size())];
+			cout << l << endl;
+		}
+		else
+			assert(false);
+	}
+	return 0;
+}
