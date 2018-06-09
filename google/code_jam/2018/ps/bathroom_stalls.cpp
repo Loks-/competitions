1388a5f08784aaca521240497673d12178cf1560
Alexey Poyarkov <alexeypoyarkov@gmail.com>
2018-06-09 15:10:49 -0700

    Add solution for "Bathroom Stalls" problem.

---
+++ google/code_jam/2018/ps/bathroom_stalls.cpp
@@ -0,0 +1,34 @@
+#include "../../../../common/base.h"
+
+int main_bathroom_stalls()
+{
+	unsigned T;
+	cin >> T;
+	for (unsigned it = 1; it <= T; ++it)
+	{
+		uint64_t N, K;
+		cin >> N >> K;
+		map<uint64_t, uint64_t> m;
+		m[N] = 1;
+		uint64_t l, k;
+		for (; ;)
+		{
+			auto it = m.rbegin();
+			l = it->first;
+			k = it->second;
+			if (k >= K)
+			{
+				break;
+			}
+			else
+			{
+				K -= k;
+				m.erase(l);
+			}
+			m[l / 2] += k;
+			m[(l - 1) / 2] += k;
+		}
+		cout << "Case #" << it << ": " << l / 2 << " " << (l - 1) / 2 << endl;
+	}
+	return 0;
+}
