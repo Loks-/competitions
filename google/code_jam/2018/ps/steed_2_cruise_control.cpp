d56ae5ca35dd9fc67f319f595cf7bd31012a87dd
Alexey Poyarkov <alexeypoyarkov@gmail.com>
2018-06-09 15:07:34 -0700

    Add solution for "Steed 2 Cruise Control" problem.

---
+++ google/code_jam/2018/ps/steed_2_cruise_control.cpp
@@ -0,0 +1,23 @@
+#include "../../../../common/base.h"
+
+int main_steed_2_cruise_control()
+{
+	unsigned T;
+	cin >> T;
+	for (unsigned it = 1; it <= T; ++it)
+	{
+		unsigned D, N;
+		cin >> D >> N;
+		double t = 0;
+		for (unsigned i = 0; i < N; ++i)
+		{
+			unsigned k, s;
+			cin >> k >> s;
+			t = max(t, double(D - k) / s);
+		}
+		cout << "Case #" << it << ": ";
+		printf("%.6lf", D / t);
+		cout << endl;
+	}
+	return 0;
+}
