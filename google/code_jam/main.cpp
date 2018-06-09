ff2d1c7050f6f209af42642f021f8413e7268082
Alexey Poyarkov <alexeypoyarkov@gmail.com>
2018-06-08 20:35:17 -0700

    Add project for Google Code Jam.

---
+++ google/code_jam/main.cpp
@@ -0,0 +1,15 @@
+#include "../../common/base.h"
+
+int main()
+{
+	cout << "Hello from distributed code jam main." << endl;
+	unsigned T;
+	cin >> T;
+	for (unsigned it = 1; it <= T; ++it)
+	{
+		cout << "Case #" << it << ": ";
+		// ...
+		cout << endl;
+	}
+	return 0;
+}
