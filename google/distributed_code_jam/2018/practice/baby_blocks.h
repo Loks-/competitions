986d1b26ad3983ca7d6de4b02b7c25ce0bccb5c8
Alexey Poyarkov <alexeypoyarkov@gmail.com>
2018-06-09 19:20:24 -0700

    Drop second input from baby_blocks.h

--- google/distributed_code_jam/2018/practice/baby_blocks.h
+++ google/distributed_code_jam/2018/practice/baby_blocks.h
@@ -15,20 +15,3 @@ long long GetBlockWeight(long long i) {
 	}
 	return 0;
 }
-
-//long long GetNumberOfBlocks() {
-//	return 6LL;
-//}
-//
-//long long GetBlockWeight(long long i) {
-//	switch ((int)i) {
-//	case 0: return 1LL;
-//	case 1: return 2LL;
-//	case 2: return 1LL;
-//	case 3: return 4LL;
-//	case 4: return 3LL;
-//	case 5: return 1LL;
-//	default: assert(0);
-//	}
-//	return 0;
-//}
