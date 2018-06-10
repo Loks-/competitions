a5bbe42c944e02829e8d7766a4397c93ffce8736
Alexey Poyarkov <alexeypoyarkov@gmail.com>
2018-06-09 20:21:55 -0700

    Add solution for "Big Party" problem.

---
+++ google/code_jam/2018/r1a/big_party.cpp
@@ -0,0 +1,43 @@
+#include "../../../../common/base.h"
+
+int main_big_party()
+{
+	unsigned T;
+	cin >> T;
+	for (unsigned it = 1; it <= T; ++it)
+	{
+		uint64_t R, B, C, ss = 0, sp = 0;
+		cin >> R >> B >> C;
+		vector<uint64_t> vM(C), vS(C), vP(C), vC(C);
+		for (unsigned i = 0; i < C; ++i)
+		{
+			cin >> vM[i] >> vS[i] >> vP[i];
+			ss = std::max(ss, vS[i]);
+			sp = std::max(sp, vP[i]);
+		}
+		uint64_t t1 = 0, t2 = ss * B + sp;
+		while (t2 - t1 > 1)
+		{
+			uint64_t tm = (t1 + t2) / 2;
+			for (unsigned i = 0; i < C; ++i)
+			{
+				vC[i] = (vP[i] >= tm) ? 0 : ((tm - vP[i]) / vS[i]);
+				vC[i] = min(vC[i], vM[i]);
+			}
+			if (R < C)
+			{
+				sort(vC.begin(), vC.end());
+				reverse(vC.begin(), vC.end());
+			}
+			uint64_t total = 0;
+			for (unsigned i = 0; i < R; ++i)
+				total += vC[i];
+			if (total < B)
+				t1 = tm;
+			else
+				t2 = tm;
+		}
+		cout << "Case #" << it << ": " << t2 << endl;
+	}
+	return 0;
+}
