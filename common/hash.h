92cee0b437451e9abf495d29d618fcd9934696b1
Alexey Poyarkov <alexeypoyarkov@gmail.com>
2018-06-11 23:32:17 -0700

    Add hash for pairs.

---
+++ common/hash.h
@@ -0,0 +1,19 @@
+#pragma once
+
+#include "base.h"
+
+namespace std {
+	inline size_t hash_combine(size_t hvalue1, size_t hvalue2)
+	{
+		return hvalue1 ^ (hvalue2 + 0x9e3779b9 + (hvalue1 << 6) + (hvalue1 >> 2));
+	}
+
+	template<class T1, class T2>
+	struct hash<pair<T1, T2>>
+	{
+		size_t operator()(const pair<T1, T2>& value) const
+		{
+			return hash_combine(hash<T1>{}(value.first), hash<T2>{}(value.second));
+		}
+	};
+}
