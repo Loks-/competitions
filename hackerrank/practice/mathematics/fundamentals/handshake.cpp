b81a9463bd498d237b1e7b0240920afd46bd2faa
Alexey Poyarkov <alexeypoyarkov@gmail.com>
2018-06-14 10:03:30 -0700

    Add solution for "Handshake" problem.

---
+++ hackerrank/practice/mathematics/fundamentals/handshake.cpp
@@ -0,0 +1,16 @@
+// https://www.hackerrank.com/challenges/handshake
+
+#include "common/base.h"
+
+int main_handshake()
+{
+	unsigned T;
+	cin >> T;
+	for (unsigned it = 0; it < T; ++it)
+	{
+		uint64_t n;
+		cin >> n;
+		cout << (n * (n - 1)) / 2 << endl;
+	}
+	return 0;
+}
