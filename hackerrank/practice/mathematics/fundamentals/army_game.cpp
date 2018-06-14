a9e71710989f29bd33951322f2405bb7be67eddf
Alexey Poyarkov <alexeypoyarkov@gmail.com>
2018-06-14 10:14:08 -0700

    Add solution for "Army Game" problem.

---
+++ hackerrank/practice/mathematics/fundamentals/army_game.cpp
@@ -0,0 +1,11 @@
+// https://www.hackerrank.com/challenges/game-with-cells
+
+#include "common/base.h"
+
+int main_army_game()
+{
+	uint64_t n, m;
+	cin >> n >> m;
+	cout << ((n + 1) / 2) * ((m + 1) / 2) << endl;
+	return 0;
+}
