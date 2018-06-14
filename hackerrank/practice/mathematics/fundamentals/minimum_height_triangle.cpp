344e2f58f707cc114fbd5ee5c5c1eecea900ce1c
Alexey Poyarkov <alexeypoyarkov@gmail.com>
2018-06-14 10:09:11 -0700

    Add solution for "Minimum Height Triangle" problem.

---
+++ hackerrank/practice/mathematics/fundamentals/minimum_height_triangle.cpp
@@ -0,0 +1,11 @@
+// https://www.hackerrank.com/challenges/lowest-triangle
+
+#include "common/base.h"
+
+int main_minimum_height_triangle()
+{
+	uint64_t a, b;
+	cin >> b >> a;
+	cout << 1 + (2 * a - 1) / b << endl;
+	return 0;
+}
