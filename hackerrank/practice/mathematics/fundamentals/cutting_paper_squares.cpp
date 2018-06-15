bdbe7f557b5025cf0ad4299c9063e95504e9c70c
Alexey Poyarkov <alexeypoyarkov@gmail.com>
2018-06-14 21:38:50 -0700

    Add solution for "Cutting Paper Squares" problem.

---
+++ hackerrank/practice/mathematics/fundamentals/cutting_paper_squares.cpp
@@ -0,0 +1,11 @@
+// https://www.hackerrank.com/challenges/p1-paper-cutting
+
+#include "common/base.h"
+
+int main_cutting_paper_squares()
+{
+	uint64_t n, m;
+	cin >> n >> m;
+	cout << n * m - 1 << endl;
+	return 0;
+}
