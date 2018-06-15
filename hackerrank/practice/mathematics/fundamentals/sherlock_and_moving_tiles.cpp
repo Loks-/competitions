9ba962624a569793c4acd5e271b56402937a0727
Alexey Poyarkov <alexeypoyarkov@gmail.com>
2018-06-14 21:51:48 -0700

    Add solution for "Sherlock and Moving Tiles" problem.

---
+++ hackerrank/practice/mathematics/fundamentals/sherlock_and_moving_tiles.cpp
@@ -0,0 +1,19 @@
+// https://www.hackerrank.com/challenges/sherlock-and-moving-tiles
+
+#include "common/base.h"
+
+int main_sherlock_and_moving_tiles()
+{
+	int64_t l, s1, s2, ds, Q, q;
+	cin >> l >> s1 >> s2 >> Q;
+	ds = abs(s1 - s2);
+	double ds2 = sqrt((double)(2.0));
+	for (unsigned i = 0; i < Q; ++i)
+	{
+		cin >> q;
+		double x = sqrt(double(q));
+		double t = (l - x) / ds * ds2;
+		printf("%.6lf\n", t);
+	}
+	return 0;
+}
