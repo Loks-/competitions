78ca92d0da56bdb8b8a74d29acb953d4890a0886
Alexey Poyarkov <alexeypoyarkov@gmail.com>
2018-06-15 20:55:38 -0700

    Add solution for "Restaurant" problem.

---
+++ hackerrank/practice/mathematics/fundamentals/restaurant.cpp
@@ -0,0 +1,18 @@
+// https://www.hackerrank.com/challenges/restaurant
+
+#include "common/base.h"
+#include "common/numeric/utils.h"
+
+int main_restaurant()
+{
+	unsigned T;
+	cin >> T;
+	for (unsigned it = 0; it < T; ++it)
+	{
+		uint64_t l, b;
+		cin >> l >> b;
+		uint64_t m = NumbericUtils::GCD(l, b);
+		cout << l * b / m / m << endl;
+	}
+	return 0;
+}
