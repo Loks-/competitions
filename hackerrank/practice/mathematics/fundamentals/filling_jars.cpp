4a979f04a7b8da85e0633cab024663713997b1e6
Alexey Poyarkov <alexeypoyarkov@gmail.com>
2018-06-15 21:55:59 -0700

    Add solution for "Filling Jars" problem.

---
+++ hackerrank/practice/mathematics/fundamentals/filling_jars.cpp
@@ -0,0 +1,16 @@
+// https://www.hackerrank.com/challenges/filling-jars
+
+#include "common/base.h"
+
+int main_filling_jars()
+{
+	uint64_t N, M, T = 0, a, b, k;
+	cin >> N >> M;
+	for (uint64_t it = 0; it < M; ++it)
+	{
+		cin >> a >> b >> k;
+		T += (b - a + 1) * k;
+	}
+	cout << (T / N) << endl;
+	return 0;
+}
