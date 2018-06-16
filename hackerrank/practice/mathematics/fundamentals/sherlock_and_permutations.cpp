7fbda9ddb34acdfcaaddee8ee02cf2f4e80d9fe5
Alexey Poyarkov <alexeypoyarkov@gmail.com>
2018-06-15 21:30:53 -0700

    Add solution for "Sherlock and Permutations" problem.

---
+++ hackerrank/practice/mathematics/fundamentals/sherlock_and_permutations.cpp
@@ -0,0 +1,21 @@
+// https://www.hackerrank.com/challenges/sherlock-and-permutations
+
+#include "common/base.h"
+#include "common/numeric/modular.h"
+#include "common/numeric/factorial_modular.h"
+
+using TModular = Modular<>;
+using TFactorial = FactorialModular<TModular, true>;
+
+int main_sherlock_and_permutations()
+{
+	TFactorial f;
+	unsigned T, N, M;
+	cin >> T;
+	for (unsigned it = 0; it < T; ++it)
+	{
+		cin >> N >> M;
+		cout << f.BinomialCoefficient(N + M - 1, N).Get() << endl;
+	}
+	return 0;
+}
