30962640c737339f14c6c597c1c74110166cfc62
Alexey Poyarkov <alexeypoyarkov@gmail.com>
2018-06-15 21:11:03 -0700

    Add solution for "Diwali Lights" problem.

---
+++ hackerrank/practice/mathematics/fundamentals/diwali_lights.cpp
@@ -0,0 +1,19 @@
+// https://www.hackerrank.com/challenges/diwali-lights
+
+#include "common/base.h"
+#include "common/numeric/modular.h"
+
+using TModular = TModular_C32<100000>;
+
+int main_diwali_lights()
+{
+	unsigned T, N;
+	TModular two(2);
+	cin >> T;
+	for (unsigned it = 0; it < T; ++it)
+	{
+		cin >> N;
+		cout << two.PowU(N).Get() - 1 << endl;
+	}
+	return 0;
+}
