f1b5e4b69d34a5ce3eec64302c2c25bbd32847c9
Alexey Poyarkov <alexeypoyarkov@gmail.com>
2018-06-16 14:01:37 -0700

    Add solution for "Most Distant" problem.

---
+++ hackerrank/practice/mathematics/fundamentals/most_distant.cpp
@@ -0,0 +1,22 @@
+// https://www.hackerrank.com/challenges/most-distant
+
+#include "common/base.h"
+
+int main_most_distant()
+{
+	unsigned N;
+	double x, xl = 1e10, xr = -1e10, y, yl = 1e10, yh = -1e10;
+	cin >> N;
+	for (unsigned it = 0; it < N; ++it)
+	{
+		cin >> x >> y;
+		xl = min(x, xl);
+		xr = max(x, xr);
+		yl = min(y, yl);
+		yh = max(y, yh);
+	}
+	double max_distance = max(xr - xl, yh - yl);
+	max_distance = max(max_distance, sqrt(max(xl*xl, xr*xr) + max(yl*yl, yh*yh)));
+	printf("%.6lf", max_distance);
+	return 0;
+}
