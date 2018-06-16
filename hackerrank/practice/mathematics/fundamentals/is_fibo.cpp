264202b7ba2615675ad2a495578d24bb07ee0cc3
Alexey Poyarkov <alexeypoyarkov@gmail.com>
2018-06-15 22:02:11 -0700

    Add solution for "Is Fibo" problem.

---
+++ hackerrank/practice/mathematics/fundamentals/is_fibo.cpp
@@ -0,0 +1,18 @@
+// https://www.hackerrank.com/challenges/is-fibo
+
+#include "common/base.h"
+
+int main_is_fibo()
+{
+	vector<uint64_t> vf{ 0, 1 };
+	unsigned T;
+	cin >> T;
+	for (unsigned it = 0; it < T; ++it)
+	{
+		uint64_t N;
+		cin >> N;
+		for (; vf.back() <= N; ) vf.push_back(vf[vf.size() - 2] + vf[vf.size() - 1]);
+		cout << (binary_search(vf.begin(), vf.end(), N) ? "IsFibo" : "IsNotFibo") << endl;
+	}
+	return 0;
+}
