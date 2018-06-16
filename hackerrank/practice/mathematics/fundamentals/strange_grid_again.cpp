548e13317b009d953c227312f26b9d7c1defbbdd
Alexey Poyarkov <alexeypoyarkov@gmail.com>
2018-06-15 21:05:28 -0700

    Add solution for "Strange Grid Again" problem.

---
+++ hackerrank/practice/mathematics/fundamentals/strange_grid_again.cpp
@@ -0,0 +1,11 @@
+// https://www.hackerrank.com/challenges/strange-grid
+
+#include "common/base.h"
+
+int main_strange_grid_again()
+{
+	uint64_t r, c;
+	cin >> r >> c;
+	cout << 10 * ((r - 1) / 2) + 2 * (c - 1) + (1 - r % 2) << endl;
+	return 0;
+}
