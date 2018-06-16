a134b2629e0c82b3211af5c8abf8a2d8f7676143
Alexey Poyarkov <alexeypoyarkov@gmail.com>
2018-06-16 14:05:24 -0700

    Add solution for "Jim and the Jokes" problem.

---
+++ hackerrank/practice/mathematics/fundamentals/jim_and_the_jokes.cpp
@@ -0,0 +1,26 @@
+// https://www.hackerrank.com/challenges/jim-and-the-jokes
+
+#include "common/base.h"
+
+int main_jim_and_the_jokes()
+{
+	unordered_map<unsigned, uint64_t> um;
+	unsigned n, m, d, x;
+	cin >> n;
+	for (; n; --n)
+	{
+		cin >> m >> d;
+		unsigned d1 = (d % 10), d2 = (d / 10);
+		if ((d1 >= m) || (d2 >= m))
+			continue;
+		x = d1 + d2 * m;
+		um[x] += 1;
+	}
+	uint64_t r = 0;
+	for (auto p : um)
+	{
+		r += (p.second * (p.second - 1)) / 2;
+	}
+	cout << r << endl;
+	return 0;
+}
