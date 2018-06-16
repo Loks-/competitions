c0dc9eab6353ea20c541b974874f94fef8b60527
Alexey Poyarkov <alexeypoyarkov@gmail.com>
2018-06-15 21:36:12 -0700

    Add solution for "Even Odd Query" problem.

---
+++ hackerrank/practice/mathematics/fundamentals/even_odd_query.cpp
@@ -0,0 +1,23 @@
+// https://www.hackerrank.com/challenges/even-odd-query
+
+#include "common/base.h"
+
+int main_even_odd_query()
+{
+	int N, Q, x, y;
+	cin >> N;
+	vector<int> vx{ 1 };
+	for (int i = 0; i < N; ++i)
+	{
+		cin >> x;
+		vx.push_back(x);
+	}
+	cin >> Q;
+	for (; Q; --Q)
+	{
+		cin >> x >> y;
+		bool b = (x > y) ? true : (vx[x] == 0) ? false : (vx[x] & 1) ? true : (x >= y) ? false : (vx[x + 1] == 0);
+		cout << (b ? "Odd" : "Even") << endl;
+	}
+	return 0;
+}
