2d1e68c2d5ec02503ee0627eed4b37407dddb64e
Alexey Poyarkov <alexeypoyarkov@gmail.com>
2018-06-15 20:55:19 -0700

    Add GCD function.

--- common/numeric/utils.h
+++ common/numeric/utils.h
@@ -5,6 +5,18 @@
 class NumbericUtils
 {
 public:
+	template<class T>
+	static T GCD(T x, T y)
+	{
+		for (; y; )
+		{
+			T z = x % y;
+			x = y;
+			y = z;
+		}
+		return x;
+	}
+
 	static uint64_t DigitsSum(uint64_t x, uint64_t base = 10)
 	{
 		uint64_t s = 0;
