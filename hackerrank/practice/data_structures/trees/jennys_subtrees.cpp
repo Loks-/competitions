f946308bf9ba99cae15dc6e40bdc02e5511fbaef
Alexey Poyarkov <alexeypoyarkov@gmail.com>
2018-06-23 13:30:32 -0700

    Add solution for "Jenny's Subtrees" problem.

---
+++ hackerrank/practice/data_structures/trees/jennys_subtrees.cpp
@@ -0,0 +1,27 @@
+// https://www.hackerrank.com/challenges/jenny-subtrees
+
+#include "common/base.h"
+#include "common/graph/base_tree.h"
+#include "common/graph/base_subgraph.h"
+#include "common/graph/base_tree_isomorphic_hash.h"
+
+int main_jennys_subtrees()
+{
+	unsigned n, r;
+	cin >> n >> r;
+	BaseTree tree(n), tree_temp;
+	tree.ReadEdges();
+	unordered_set<size_t> hash_set;
+	vector<bool> keep(n);
+	vector<unsigned> distance;
+	for (unsigned i = 0; i < n; ++i)
+	{
+		distance = tree.GetDistances(i);
+		for (unsigned j = 0; j < n; ++j)
+			keep[j] = (distance[j] <= r);
+		BaseSubgraph::CreateSubgraph(tree, keep, tree_temp);
+		hash_set.insert(BaseTreeIsomorphicHash::Hash(tree_temp));
+	}
+	cout << hash_set.size() << endl;
+	return 0;
+}
