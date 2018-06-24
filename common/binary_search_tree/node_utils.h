944e672560f2cbe4dfa1c0f032a0d26f0d8ba7e5
Alexey Poyarkov <alexeypoyarkov@gmail.com>
2018-06-24 15:47:22 -0700

    Move static utils functions out of class to unnamed namespace.

--- common/binary_search_tree/node_utils.h
+++ common/binary_search_tree/node_utils.h
@@ -1,22 +1,22 @@
 #pragma once
 
-class BSTNodeUtils
+namespace {
+
+template<class TNode>
+TNode* FindByKey(TNode* root, const typename TNode::TKey& key)
 {
-public:
-	template<class TNode>
-	static TNode* Find(TNode* root, const typename TNode::TKey& key)
+	static_assert(TNode::use_key);
+	for (TNode * node = root; node != 0; )
 	{
-		static_assert(TNode::use_key);
-		for (TNode * node = root; node != 0; )
-		{
-			node->action.Apply(node);
-			if (node->x < key)
-				node = node->l;
-			else if (key < node->x)
-				node = node->r;
-			else
-				return node;
-		}
-		return 0;
+		node->action.Apply(node);
+		if (node->x < key)
+			node = node->l;
+		else if (key < node->x)
+			node = node->r;
+		else
+			return node;
 	}
-};
+	return 0;
+}
+
+} // namespace
