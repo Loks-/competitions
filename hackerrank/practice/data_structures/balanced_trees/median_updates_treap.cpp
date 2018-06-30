c56a1ae9e7391b5cfe909a68c7437b88590f40ef
Alexey Poyarkov <alexeypoyarkov@gmail.com>
2018-06-30 14:24:19 -0700

    Add solution for "Median Updates" using Treap.

---
+++ hackerrank/practice/data_structures/balanced_trees/median_updates_treap.cpp
@@ -0,0 +1,51 @@
+// https://www.hackerrank.com/challenges/median
+
+#include "common/base.h"
+#include "common/binary_search_tree/find_by_key.h"
+#include "common/binary_search_tree/find_by_order.h"
+#include "common/binary_search_tree/treap.h"
+
+using TTreap = Treap<true, true, TEmpty>;
+using TNode = TTreap::TNode;
+
+int main_median_updates_treap()
+{
+	unsigned N;
+	cin >> N;
+	TTreap treap(N);
+	TNode * root = 0;
+	for (unsigned i = 0; i < N; ++i)
+	{
+		char c; int64_t x;
+		cin >> c >> x;
+		x *= 2;
+		if (c == 'a')
+		{
+			root = treap.Insert(root, TEmpty(), x);
+		}
+		else if (c == 'r')
+		{
+			TNode * node = FindByKey(root, x);
+			if (node == 0)
+			{
+				cout << "Wrong!" << endl;
+				continue;
+			}
+			root = treap.RemoveAndRelease(node);
+		}
+		if (root == 0)
+		{
+			cout << "Wrong!" << endl;
+		}
+		else
+		{
+			unsigned size = root->info.size;
+			int64_t s = (FindByOrder(root, (size - 1) / 2)->key + FindByOrder(root, size / 2)->key) / 2;
+			if (s & 1)
+				printf("%.1lf\n", s / 2.0);
+			else
+				printf("%d\n", int(s / 2));
+		}
+	}
+	return 0;
+}
