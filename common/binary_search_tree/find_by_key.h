251634e918fb27cbb117993ccb44b511e22696cb
Alexey Poyarkov <alexeypoyarkov@gmail.com>
2018-07-01 05:48:42 -0700

    Fix find_by_key.h

--- common/binary_search_tree/find_by_key.h
+++ common/binary_search_tree/find_by_key.h
@@ -8,7 +8,7 @@ TNode* FindByKey(TNode* root, const typename TNode::TKey& key)
 	static_assert(TNode::use_key);
 	for (TNode * node = root; node != 0; )
 	{
-		node->action.Apply(node);
+		node->ApplyAction();
 		if (node->key < key)
 			node = node->r;
 		else if (key < node->key)
