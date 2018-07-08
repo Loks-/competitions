89f901df011eadd8b89b3e281d16148430cf7a57
Alexey Poyarkov <alexeypoyarkov@gmail.com>
2018-07-08 11:34:41 -0700

    Simplify BSTSibling.

--- common/binary_search_tree/sibling.h
+++ common/binary_search_tree/sibling.h
@@ -3,9 +3,6 @@
 namespace {
 
 template<class TNode>
-TNode* BSTSibling(TNode* node, TNode* parent)
-{
-	return reinterpret_cast<TNode*>(reinterpret_cast<size_t>(node) ^ reinterpret_cast<size_t>(parent->l) ^ reinterpret_cast<size_t>(parent->r));
-}
+TNode* BSTSibling(TNode* node, TNode* parent) {	return parent ? (parent->l - node + parent->r) : 0; }
 
 } // namespace
