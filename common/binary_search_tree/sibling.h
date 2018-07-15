670f7508a724d2e48498b51aa52110d697119a8e
Alexey Poyarkov <alexeypoyarkov@gmail.com>
2018-07-15 08:57:29 -0700

    Fix BSTSibling for safety.

--- common/binary_search_tree/sibling.h
+++ common/binary_search_tree/sibling.h
@@ -3,6 +3,6 @@
 namespace {
 
 template<class TNode>
-TNode* BSTSibling(TNode* node, TNode* parent) {	return parent ? (parent->l - node + parent->r) : 0; }
+TNode* BSTSibling(TNode* node, TNode* parent) {	return parent ? (parent->l == node ? parent->r : parent->l) : 0; }
 
 } // namespace
