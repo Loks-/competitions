a17b7f87d51b368f100f4fcc348399a39c0da790
Alexey Poyarkov <alexeypoyarkov@gmail.com>
2018-07-18 19:38:52 -0700

    Add solution for "Balanced Brackets" problem.

---
+++ hackerrank/practice/data_structures/stacks/balanced_brackets.cpp
@@ -0,0 +1,46 @@
+// https://www.hackerrank.com/challenges/balanced-brackets
+
+#include "common/base.h"
+
+namespace {
+	unordered_map<char, char> m;
+
+	bool Correct(const string& s)
+	{
+		stack<char> st;
+		for (char c : s)
+		{
+			auto it = m.find(c);
+			if (it == m.end())
+				st.push(c);
+			else
+			{
+				if (st.empty())
+					return false;
+				if (st.top() != it->second)
+					return false;
+				st.pop();
+			}
+		}
+		return st.empty();
+	}
+}
+
+int main_balanced_brackets()
+{
+	m[')'] = '(';
+	m[']'] = '[';
+	m['}'] = '{';
+	int T;
+	cin >> T;
+	for (; T; --T)
+	{
+		string s;
+		cin >> s;
+		if (Correct(s))
+			cout << "YES" << endl;
+		else
+			cout << "NO" << endl;
+	}
+	return 0;
+}
