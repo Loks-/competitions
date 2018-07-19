eb753ad3270a307c32de96b540f1e0793f43679e
Alexey Poyarkov <alexeypoyarkov@gmail.com>
2018-07-18 20:17:17 -0700

    Add solution for "AND xor OR" problem.

---
+++ hackerrank/practice/data_structures/stacks/and_xor_or.cpp
@@ -0,0 +1,23 @@
+// https://www.hackerrank.com/challenges/and-xor-or
+
+#include "common/base.h"
+
+int main_and_xor_or()
+{
+	unsigned N, a, best = 0;
+	cin >> N;
+	stack<unsigned> s;
+	for (unsigned i = 0; i < N; ++i)
+	{
+		cin >> a;
+		for (; !s.empty() && (s.top() >= a); s.pop())
+		{
+			best = max(best, s.top() ^ a);
+		}
+		if (!s.empty())
+			best = max(best, s.top() ^ a);
+		s.push(a);
+	}
+	cout << best << endl;
+	return 0;
+}
