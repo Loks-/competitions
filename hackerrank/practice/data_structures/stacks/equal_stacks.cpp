16603ecb5a1be4ccd77d9ba5ad1d95c96b38883e
Alexey Poyarkov <alexeypoyarkov@gmail.com>
2018-07-18 19:34:27 -0700

    Add solution for "Equal Stacks" problem.

---
+++ hackerrank/practice/data_structures/stacks/equal_stacks.cpp
@@ -0,0 +1,29 @@
+// https://www.hackerrank.com/challenges/equal-stacks
+
+#include "common/base.h"
+#include "common/vector/utils.h"
+
+int main_equal_stacks()
+{
+	vector<unsigned> vn = ReadVector<unsigned>(3);
+	unordered_map<unsigned, unsigned> m;
+	for (unsigned i = 0; i < 3; ++i)
+	{
+		vector<unsigned> vt = ReadVector<unsigned>(vn[i]);
+		reverse(vt.begin(), vt.end());
+		unsigned h = 0;
+		for (unsigned ih : vt)
+		{
+			h += ih;
+			m[h] += 1;
+		}
+	}
+	unsigned best = 0;
+	for (auto p : m)
+	{
+		if (p.second == 3)
+			best = max(best, p.first);
+	}
+	cout << best << endl;
+	return 0;
+}
