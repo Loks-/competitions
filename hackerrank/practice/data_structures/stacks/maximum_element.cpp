630cb64e00238f2ce3278068328a6131d019cc53
Alexey Poyarkov <alexeypoyarkov@gmail.com>
2018-07-18 19:24:38 -0700

    Add solution for "Maximum Element" problem.

---
+++ hackerrank/practice/data_structures/stacks/maximum_element.cpp
@@ -0,0 +1,26 @@
+// https://www.hackerrank.com/challenges/maximum-element
+
+#include "common/base.h"
+
+int main_maximum_element()
+{
+	unsigned N, x, t;
+	cin >> N;
+	stack<unsigned> s;
+	for (unsigned it = 0; it < N; ++it)
+	{
+		cin >> t;
+		if (t == 1)
+		{
+			cin >> x;
+			if (!s.empty())
+				x = max(x, s.top());
+			s.push(x);
+		}
+		else if (t == 2)
+			s.pop();
+		else if (t == 3)
+			cout << s.top() << endl;
+	}
+	return 0;
+}
