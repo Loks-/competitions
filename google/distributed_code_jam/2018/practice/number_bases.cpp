89c6b2db7cf58b97fc490ea24adbde991d89b4d4
Loks- <alexeypoyarkov@gmail.com>
2018-08-05 15:04:15 -0700

    Add solution for "number_bases" problem.

---
+++ google/distributed_code_jam/2018/practice/number_bases.cpp
@@ -0,0 +1,68 @@
+#include "message.h"
+#include "number_bases.h"
+
+#include "common/base.h"
+
+int main_number_bases()
+{
+	int nodes = NumberOfNodes();
+	int node_id = MyNodeId();
+    int64_t length = GetLength();
+
+    int64_t ifirst = node_id * length / nodes;
+	int64_t ilast = (node_id + 1) * length / nodes;
+
+    int64_t b0 = 2, b1 = 1000000000, x, y, z, s = 0;
+    if (ifirst > 0)
+    {
+        x = GetDigitX(ifirst - 1);
+        y = GetDigitY(ifirst - 1);
+        z = GetDigitZ(ifirst - 1);
+        if (z < x + y)
+            s = 1;
+    }
+    for (int64_t i = ifirst; i < ilast; ++i)
+    {
+        x = GetDigitX(i);
+        y = GetDigitY(i);
+        z = GetDigitZ(i);
+        b0 = max(b0, x + 1);
+        b0 = max(b0, y + 1);
+        b0 = max(b0, z + 1);
+        if (z > x + y + s)
+        {
+            b1 = 1; break;
+        }
+        else if (z == x + y + s)
+        {
+            s = 0;
+        }
+        else
+        {
+            b0 = max(b0, x + y + s - z);
+            b1 = min(b1, x + y + s - z);
+            s = 1;
+        }
+    }
+    if ((ilast == length) && (s == 1))
+        b1 = 1;
+
+    if (node_id == 0)
+    {
+        for (int i = 1; i < nodes; ++i)
+        {
+            Receive(i);
+            b0 = max(b0, int64_t(GetLL(i)));
+            b1 = min(b1, int64_t(GetLL(i)));
+        }
+        cout << ((b1 < b0) ? "IMPOSSIBLE" : (b1 > b0) ? "NON-UNIQUE" : to_string(b0)) << endl;
+    }
+    else
+    {
+        PutLL(0, b0);
+        PutLL(0, b1);
+        Send(0);
+    }
+
+    return 0;
+}
---
+++ google/distributed_code_jam/2018/practice/number_bases.h
@@ -0,0 +1,34 @@
+// Sample input 1, in CPP.
+
+#include <cassert>
+
+long long GetLength() {
+  return 3LL;
+}
+
+long long GetDigitX(long long i) {
+  switch ((int)i) {
+    case 0: return 3LL;
+    case 1: return 2LL;
+    case 2: return 1LL;
+    default: assert(0);
+  }
+}
+
+long long GetDigitY(long long i) {
+  switch ((int)i) {
+    case 0: return 6LL;
+    case 1: return 5LL;
+    case 2: return 4LL;
+    default: assert(0);
+  }
+}
+
+long long GetDigitZ(long long i) {
+  switch ((int)i) {
+    case 0: return 0LL;
+    case 1: return 8LL;
+    case 2: return 5LL;
+    default: assert(0);
+  }
+}
