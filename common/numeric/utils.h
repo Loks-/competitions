7946e9bc395dc2dfbbbc226d6d8125be0812fe24
Loks- <alexeypoyarkov@gmail.com>
2018-08-10 18:39:38 -0700

    Add solution for "Closest Number" problem.

--- common/numeric/utils.h
+++ common/numeric/utils.h
@@ -4,6 +4,14 @@
 
 namespace {
 
+uint64_t DigitsSum(uint64_t x, uint64_t base = 10)
+{
+	uint64_t s = 0;
+	for (; x; x /= base)
+		s += x % base;
+	return s;
+}
+
 template<class T>
 T GCD(T x, T y)
 {
@@ -16,12 +24,17 @@ T GCD(T x, T y)
 	return x;
 }
 
-uint64_t DigitsSum(uint64_t x, uint64_t base = 10)
+template<class T>
+T PowU(T x, uint64_t pow)
 {
-	uint64_t s = 0;
-	for (; x; x /= base)
-		s += x % base;
-	return s;
+	T ans = 1;
+	for (; pow; pow >>= 1)
+	{
+		if (pow & 1)
+			ans *= x;
+		x *= x;
+	}
+	return ans;	
 }
 
 }  // namespace
---
+++ hackerrank/practice/mathematics/number_theory/closest_number.cpp
@@ -0,0 +1,27 @@
+// https://www.hackerrank.com/challenges/closest-number
+
+#include "common/base.h"
+#include "common/numeric/utils.h"
+
+int main_closets_number()
+{
+	unsigned T;
+	cin >> T;
+	for (unsigned it = 0; it < T; ++it)
+	{
+        int64_t a, b, x, r;
+        cin >> a >> b >> x;
+        if ((a == 1) || (b == 0))
+            r = (x > 1) ? 0 : 1;
+        else if (b < 0)
+            r = 0;
+        else
+        {
+            int64_t apb = PowU(a, uint64_t(b));
+            int64_t xl = apb - (apb % x), xr = xl + x;
+            r = (xl + xr >= 2 * apb) ? xl : xr;
+        }
+        cout << r << endl;
+	}
+	return 0;
+}
