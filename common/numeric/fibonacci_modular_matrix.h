c502b2b4ada07ab2028882f057df90fe62f3be3d
Loks- <alexeypoyarkov@gmail.com>
2018-08-10 18:54:49 -0700

    Add solution for "Fibonacci Finding (easy)" problem.

---
+++ common/numeric/fibonacci_modular_matrix.h
@@ -0,0 +1,22 @@
+#pragma once
+
+#include "../matrix/matrix_static_size.h"
+
+template<class TModular>
+class FibonacciModularMatrix
+{
+public:
+	using TMatrix = TSquareMatrixStaticSize<TModular, 2>;
+
+	static TModular GetU(uint64_t n, TModular f0, TModular f1)
+	{
+		TMatrix transition;
+		transition(0, 0) = 0;
+		transition(0, 1) = 1;
+		transition(1, 0) = 1;
+		transition(1, 1) = 1;
+		TMatrix fn = transition.PowU(n);
+		return fn(0, 0) * f0 + fn(0, 1) * f1;
+	}
+	static TModular GetU(uint64_t n) { return GetU(n, 0, 1); }
+};
---
+++ hackerrank/practice/mathematics/number_theory/fibonacci_finding_easy.cpp
@@ -0,0 +1,20 @@
+// https://www.hackerrank.com/challenges/fibonacci-finding-easy
+
+#include "common/base.h"
+#include "common/numeric/fibonacci_modular_matrix.h"
+#include "common/numeric/modular.h"
+
+using TFibonacci = FibonacciModularMatrix<Modular<1000000007>>;
+
+int main_fibonacci_finding_easy()
+{
+	unsigned T;
+	cin >> T;
+	for (unsigned it = 0; it < T; ++it)
+	{
+		uint64_t A, B, N;
+		cin >> A >> B >> N;
+		cout << TFibonacci::GetU(N, A, B).Get() << endl;
+	}
+	return 0;
+}
