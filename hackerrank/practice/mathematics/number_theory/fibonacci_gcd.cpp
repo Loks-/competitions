527526d7689f7b2d2570f2c0ad98eef63a9e1e0e
Loks- <alexeypoyarkov@gmail.com>
2018-08-11 17:35:41 -0700

    Add solution for "Fibonacci GCD" problem.

---
+++ hackerrank/practice/mathematics/number_theory/fibonacci_gcd.cpp
@@ -0,0 +1,23 @@
+// https://www.hackerrank.com/challenges/fibonacci-gcd
+
+#include "common/base.h"
+#include "common/numeric/fibonacci_modular_matrix.h"
+#include "common/numeric/modular.h"
+#include "common/numeric/utils.h"
+
+using TFibonacci = FibonacciModularMatrix<Modular<1000000007>>;
+
+int main_fibonacci_gcd()
+{
+	unsigned N;
+	cin >> N;
+	uint64_t a, b;
+	cin >> a;
+	for (unsigned i = 1; i < N; ++i)
+	{
+		cin >> b;
+		a = GCD(b, a);
+	}
+	cout << TFibonacci::GetU(a).Get() <<endl;
+	return 0;
+}
