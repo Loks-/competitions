9bf0a37befd5506c96933d69026932ba7d171307
Alexey Poyarkov <alexeypoyarkov@gmail.com>
2018-08-14 22:05:27 -0700

    Add solution for "Salary Blues" problem.

---
+++ hackerrank/practice/mathematics/number_theory/salary_blues.cpp
@@ -0,0 +1,23 @@
+// https://www.hackerrank.com/challenges/salary-blues
+
+#include "common/base.h"
+#include "common/numeric/utils/gcd.h"
+#include "common/vector/utils.h"
+
+int main_salary_blues()
+{
+	unsigned N, T;
+	cin >> N >> T;
+	vector<uint64_t> v = ReadVector<uint64_t>(N);
+	uint64_t min_value = v[0], gcd = 0, K;
+	for (uint64_t a : v)
+		min_value = min(min_value, a);
+	for (uint64_t a : v)
+		gcd = GCD(gcd, a - min_value);
+	for (unsigned it = 0; it < T; ++it)
+	{
+		cin >> K;
+		cout << GCD(gcd, min_value + K) << endl;
+	}
+	return 0;
+}
