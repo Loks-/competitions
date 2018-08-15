d58a415812abc4b5617a0d09e9e6ecbb5b4f7e14
Alexey Poyarkov <alexeypoyarkov@gmail.com>
2018-08-14 20:48:24 -0700

    Add solution for "Help Mike" problem.

---
+++ hackerrank/practice/mathematics/number_theory/help_mike.cpp
@@ -0,0 +1,23 @@
+// https://www.hackerrank.com/challenges/help-mike
+
+#include "common/base.h"
+
+int main_help_mike()
+{
+	unsigned T;
+	cin >> T;
+	for (unsigned it = 0; it < T; ++it)
+	{
+		uint64_t N, K, s = 0;
+		cin >> N >> K;
+		for (uint64_t i = 1; i <= K; ++i)
+		{
+			if ((i == K) || (2 * i == K))
+				s += ((N + K - i) / K) * ((N - i) / K);
+			else
+				s += ((N + K - i) / K) * ((N + i) / K);
+		}
+		cout << s / 2 << endl;
+	}
+	return 0;
+}
