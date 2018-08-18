66e987b76e3133a5a1e76df70b0d8af237282ea2
Alexey Poyarkov <alexeypoyarkov@gmail.com>
2018-08-17 22:04:11 -0700

    Refactor BSTSwap part 1.

--- common/binary_search_tree/swap.h
+++ common/binary_search_tree/swap.h
@@ -7,14 +7,9 @@
 
 namespace {
 
-template<class TNode, bool update_info = true, bool apply_action = false>
+template<class TNode>
 void BSTSwapINotRelated(TNode* node1, TNode* node2)
 {
-	if (apply_action)
-	{
-		ApplyActionRootToNode(node1);
-		ApplyActionRootToNode(node2);
-	}
 	if (node1->l) node1->l->p = node2;
 	if (node1->r) node1->r->p = node2;
 	if (node2->l) node2->l->p = node1;
@@ -36,18 +31,11 @@ void BSTSwapINotRelated(TNode* node1, TNode* node2)
 	swap(node1->l, node2->l);
 	swap(node1->r, node2->r);
 	swap(node1->p, node2->p);
-	if (update_info)
-	{
-		UpdateInfoNodeToRoot(node1);
-		UpdateInfoNodeToRoot(node2);
-	}
 }
 
-template<class TNode, bool update_info = true, bool apply_action = false>
+template<class TNode>
 void BSTSwapIChildParent(TNode* child, TNode* parent)
 {
-	if (apply_action)
-		ApplyActionRootToNode(child);
 	if (child->l) child->l->p = parent;
 	if (child->r) child->r->p = parent;
 	TNode* sibling = BSTSibling(child, parent);
@@ -67,8 +55,6 @@ void BSTSwapIChildParent(TNode* child, TNode* parent)
 		child->l = parent;
 	else
 		child->r = parent;
-	if (update_info)
-		UpdateInfoNodeToRoot(parent);
 }
 
 template<class TNode, bool update_info = true, bool apply_action = false>
@@ -78,11 +64,35 @@ void BSTSwap(TNode* node1, TNode* node2)
 	assert(node1 && node2);
 	if (node1 == node2) return;
 	if (node1->p == node2)
-		BSTSwapIChildParent<TNode, update_info, apply_action>(node1, node2);
+	{
+		if (apply_action)
+			ApplyActionRootToNode(node1);
+		BSTSwapIChildParent(node1, node2);
+		if (update_info)
+			UpdateInfoNodeToRoot(node2);
+	}
 	else if (node2->p == node1)
-		BSTSwapIChildParent<TNode, update_info, apply_action>(node2, node1);
+	{
+		if (apply_action)
+			ApplyActionRootToNode(node2);
+		BSTSwapIChildParent(node2, node1);
+		if (update_info)
+			UpdateInfoNodeToRoot(node1);
+	}
 	else
-		BSTSwapINotRelated<TNode, update_info, apply_action>(node1, node2);
+	{
+		if (apply_action)
+		{
+			ApplyActionRootToNode(node1);
+			ApplyActionRootToNode(node2);
+		}
+		BSTSwapINotRelated(node1, node2);
+		if (update_info)
+		{
+			UpdateInfoNodeToRoot(node1);
+			UpdateInfoNodeToRoot(node2);
+		}
+	}
 }
 
 } // namespace
