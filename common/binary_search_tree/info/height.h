4d2ced3e219059f564faa59344f2fd505ec80e11
Alexey Poyarkov <alexeypoyarkov@gmail.com>
2018-08-18 12:51:03 -0700

    Add BSTInfoHeight class.

---
+++ common/binary_search_tree/info/height.h
@@ -0,0 +1,22 @@
+#pragma once
+
+#include "../info.h"
+
+template<class TInfo = BSTInfoSize>
+class BSTInfoHeight : public TInfo
+{
+public:
+	using TBase = TInfo;
+	using TSelf = BSTInfoHeight<TInfo>;
+	static const bool is_none = false;
+
+	// Leaf node has height = 1.
+	unsigned height;
+
+	template<class TNode>
+	void Update(TNode* node)
+	{
+		TBase::Update(node);
+		height = 1 + max((node->l ? node->l->info.height : 0), (node->r ? node->r->info.height : 0));
+	}
+};
