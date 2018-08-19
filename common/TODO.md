921e3bf027743446b856a0def720359b0ccad03f
Alexey Poyarkov <alexeypoyarkov@gmail.com>
2018-08-18 20:13:03 -0700

    Add FixedTree class to CommonLibrary.

--- common/TODO.md
+++ common/TODO.md
@@ -1,8 +1,6 @@
 # TODO list
 * Numeric
 	* Simplify modular pow for primes using a^(p-1) = 1.
-* Binary Trees
-	* FixedTree
 * Persistent Binary Trees
 * BIT
 * SegmentTree
---
+++ common/binary_search_tree/fixed_tree.h
@@ -0,0 +1,59 @@
+#pragma once
+
+#include "node.h"
+#include "nodes_manager.h"
+#include "action/none.h"
+#include "info/size.h"
+
+template <
+	bool _use_parent,
+	class TTData,
+	class TTInfo = BSTInfoSize,
+	class TTAction = BSTActionNone,
+	class TTKey = int64_t>
+class FixedTree : public BSTNodesManager<BSTNode<TTData, TTInfo, TTAction, true, _use_parent, false, TTKey>>
+{
+public:
+	static const bool use_key = true;
+	static const bool use_parent = _use_parent;
+	static const bool use_height = false;
+
+	using TData = TTData;
+	using TInfo = TTInfo;
+	using TAction = TTAction;
+	using TKey = TTKey;
+	using TNode = BSTNode<TData, TInfo, TAction, use_key, use_parent, use_height, TKey>;
+	using TSelf = FixedTree<use_parent, TData, TTInfo, TAction, TKey>;
+	using TNodesManager = BSTNodesManager<TNode>;
+
+public:
+	FixedTree(unsigned max_nodes) : TNodesManager(max_nodes) {}
+
+	static TNode* BuildTreeI(const vector<TNode*>& vnodes, unsigned first, unsigned last)
+	{
+		if (first >= last) return 0;
+		unsigned m = (first + last) / 2;
+		TNode* root = vnodes[m];
+		root->SetL(BuildTreeI(vnodes, first, m));
+		root->SetR(BuildTreeI(vnodes, m + 1, last));
+		root->UpdateInfo();
+		return root;
+	}
+
+	static TNode* BuildTree(const vector<TNode*>& vnodes) { return BuildTreeI(vnodes, 0, unsigned(vnodes.size())); }
+
+	TNode* Build(const vector<TData>& data, const vector<TKey>& keys)
+	{
+		assert(data.size() == keys.size());
+		assert(TNodesManager::AvailableNodes() >= data.size());
+		if (data.size() == 0) return 0;
+		vector<pair<TKey, TNode*>> vp(data.size());
+		for (unsigned i = 0; i < data.size(); ++i)
+			vp[i] = make_pair(keys[i], TNodesManager::GetNewNode(data[i], keys[i]));
+		sort(vp.begin(), vp.end());
+		vector<TNode*> v(vp.size());
+		for (unsigned i = 0; i < vp.size(); ++i)
+			v[i] = vp[i].second;
+		return TMe::BuildTree(v);
+	}
+};
