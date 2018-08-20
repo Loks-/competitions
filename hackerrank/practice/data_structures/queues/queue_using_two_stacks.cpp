9b1e8e22f3724c2481c408a3d8b4cee478d6c42c
Alexey Poyarkov <alexeypoyarkov@gmail.com>
2018-08-19 17:13:16 -0700

    Add solution for "Queue using Two Stacks" problem. Solution uses queue, not 2 stacks.

---
+++ hackerrank/practice/data_structures/queues/queue_using_two_stacks.cpp
@@ -0,0 +1,29 @@
+// https://www.hackerrank.com/challenges/queue-using-two-stacks
+
+#include "common/stl_base.h"
+
+#include <queue>
+
+int main_queue_using_two_stacks()
+{
+	unsigned Q, type;
+	int x;
+	queue<int> q;
+	cin >> Q;
+	for (unsigned iq = 0; iq < Q; ++iq)
+	{
+		cin >> type;
+		if (type == 1)
+		{
+			cin >> x;
+			q.push(x);
+		}
+		else if (type == 2)
+			q.pop();
+		else if (type == 3)
+			cout << q.front() << endl;
+		else
+			assert(false);
+	}
+	return 0;
+}
