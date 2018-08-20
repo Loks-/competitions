057237a1f8b28f76e6e66a3beaa03f0ac08c9165
Alexey Poyarkov <alexeypoyarkov@gmail.com>
2018-08-19 18:48:24 -0700

    Add solution to "QHEAP1" problem using std::set.

---
+++ hackerrank/practice/data_structures/heap/qheap1.cpp
@@ -0,0 +1,31 @@
+// https://www.hackerrank.com/challenges/qheap1
+
+#include "common/stl_base.h"
+
+#include <set>
+
+int main_qheap1()
+{
+	set<int> s;
+	int Q, q, x;
+	cin >> Q;
+	for (; Q; --Q)
+	{
+		cin >> q;
+		if (q == 1)
+		{
+			cin >> x;
+			s.insert(x);
+		}
+		else if (q == 2)
+		{
+			cin >> x;
+			s.erase(x);
+		}
+		else if (q == 3)
+		{
+			cout << *s.begin() << endl;
+		}
+	}
+	return 0;
+}
