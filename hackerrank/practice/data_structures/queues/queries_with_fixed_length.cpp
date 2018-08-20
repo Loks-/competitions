a410cd429a053a9405ce7a08c3f4e43fcf03149c
Alexey Poyarkov <alexeypoyarkov@gmail.com>
2018-08-19 18:03:03 -0700

    Add solution for "Queries with Fixed Length" problem.

---
+++ hackerrank/practice/data_structures/queues/queries_with_fixed_length.cpp
@@ -0,0 +1,32 @@
+// https://www.hackerrank.com/challenges/queries-with-fixed-length
+
+#include "common/vector/utils.h"
+#include "common/stl_base.h"
+
+#include <deque>
+
+int main_queries_with_fixed_length()
+{
+	unsigned N, Q, d;
+	cin >> N >> Q;
+	vector<unsigned> v = ReadVector<unsigned>(N);
+	for (unsigned iq = 0; iq < Q; ++iq)
+	{
+		cin >> d;
+		unsigned best = 1000000;
+		deque<pair<unsigned, unsigned>> dq;
+		for (unsigned i = 0; i < N; ++i)
+		{
+			if (i >= d)
+				best = min(best, dq.front().first);
+			for (; !dq.empty() && (dq.back().first <= v[i]);)
+				dq.pop_back();
+			dq.push_back({ v[i], i });
+			if (dq.front().second + d <= i)
+				dq.pop_front();
+		}
+		best = min(best, dq.front().first);
+		cout << best << endl;
+	}
+	return 0;
+}
