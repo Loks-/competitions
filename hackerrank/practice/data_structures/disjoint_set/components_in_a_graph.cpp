a92d1dfaedbe6b42803f3b097508dff4ea971a7d
Alexey Poyarkov <alexeypoyarkov@gmail.com>
2018-08-20 21:05:40 -0700

    Add solution for "Components in a graph" problem.

---
+++ hackerrank/practice/data_structures/disjoint_set/components_in_a_graph.cpp
@@ -0,0 +1,28 @@
+// https://www.hackerrank.com/challenges/components-in-graph
+
+#include "common/disjoint_set.h"
+#include "common/stl_base.h"
+
+int main_components_in_a_graphs()
+{
+	unsigned N, i, j;
+	cin >> N;
+	DisjointSet ds(2 * N + 1);
+	for (unsigned k = 0; k < N; ++k)
+	{
+		cin >> i >> j;
+		ds.Union(i, j);
+	}
+	unsigned mins = 2 * N, maxs = 1;
+	for (unsigned k = 1; k <= N; ++k)
+	{
+		unsigned s = ds.GetSize(k);
+		if (s > 1)
+		{
+			mins = min(mins, s);
+			maxs = max(maxs, s);
+		}
+	}
+	cout << mins << " " << maxs;
+	return 0;
+}
