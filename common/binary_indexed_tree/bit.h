8e27d558825e791f6b8092da54818832c9245b1e
Alexey Poyarkov <alexeypoyarkov@gmail.com>
2018-08-21 21:32:28 -0700

    Add solution for "Kindergarten Adventures" problem using BIT.

---
+++ common/binary_indexed_tree/bit.h
@@ -0,0 +1,45 @@
+#pragma once
+
+#include "../base.h"
+#include <algorithm>
+#include <vector>
+
+// Binary Indexed Tree (same as Fenwick tree)
+template<class TValue = int64_t>
+class BIT
+{
+protected:
+	unsigned size;
+	std::vector<TValue> values;
+
+public:
+	BIT(unsigned _size) : size(_size) { values.resize(size, 0);	}
+
+	void Reset(unsigned new_size)
+	{
+		size = new_size;
+		values.resize(size);
+		fill(values.begin(), values.end(), TValue(0));
+	}
+
+	void Add(unsigned index, TValue value = TValue(1))
+	{
+		for (; index < size; index |= (index + 1))
+			values[index] += value;
+	}
+
+	// Get sum of all elements before end.
+	TValue Sum(unsigned end) const
+	{
+		TValue sum = 0;
+		for (unsigned index, index1 = end; index1; index1 &= index)
+		{
+			index = index1 - 1;
+			sum += values[index];
+		}
+		return sum;
+	}
+	TValue Sum(unsigned begin, unsigned end) const { return Sum(end) - Sum(begin); }
+
+	TValue Get(unsigned index) const { return Sum(index, index + 1); }
+};
---
+++ hackerrank/practice/data_structures/advanced/kindergarten_adventures_bit.cpp
@@ -0,0 +1,33 @@
+// https://www.hackerrank.com/challenges/kindergarten-adventures
+
+#include "common/binary_indexed_tree/bit.h"
+#include "common/stl_base.h"
+
+int main_kindergarten_adventures_bit()
+{
+	unsigned n;
+	cin >> n;
+	BIT<int> bit(2 * n + 1);
+	for (unsigned i = 0; i < n; ++i)
+	{
+		unsigned t;
+		cin >> t;
+		if (t < n)
+		{
+			bit.Add(i + 1, 1);
+			bit.Add(i + n - t + 1, -1);
+		}
+	}
+	int best = 0; unsigned p = 0;
+	for (unsigned i = 0; i < n; ++i)
+	{
+		int x = bit.Sum(i + 1) + bit.Sum(i + n + 1);
+		if (best < x)
+		{
+			best = x;
+			p = i;
+		}
+	}
+	cout << p + 1 << endl;
+	return 0;
+}
