7ecfce526726bf3207bb5961a9b9841de92a169c
Alexey Poyarkov <alexeypoyarkov@gmail.com>
2018-08-29 23:23:50 -0700

    Style update.

--- google/code_jam/2018/r1c/ant_stack.cpp
+++ google/code_jam/2018/r1c/ant_stack.cpp
@@ -20,11 +20,8 @@ int main_ant_stack()
             size_t j0 = upper_bound(best_weights.begin(), best_weights.end(), w) - best_weights.begin();
             size_t j1 = upper_bound(best_weights.begin(), best_weights.end(), 6 * w) - best_weights.begin();
             for (size_t j = j1; j >= j0; --j)
-            {
                 best_weights[j] = min(best_weights[j], best_weights[j-1] + w);
-            }
         }
-
 		cout << "Case #" << it << ": " << (lower_bound(best_weights.begin(), best_weights.end(), max_weight) - best_weights.begin()) - 1 << endl;
 	}
 	return 0;
