119ce86ecce8ed38367394343b39a256786b51ec
Loks- <alexeypoyarkov@gmail.com>
2018-09-02 20:06:37 -0700

    Use lambda instead of macros.

--- hackerrank/practice/mathematics/fundamentals/die_hard_3.cpp
+++ hackerrank/practice/mathematics/fundamentals/die_hard_3.cpp
@@ -16,35 +16,30 @@ int main_die_hard_3()
 		qp.push(make_pair(0u, 0u));
 		vb[0][0] = true;
 		bool found = false;
-		for (; !qp.empty();)
+
+		auto CheckAndAdd = [&](unsigned u, unsigned v) {
+			if ((u == c) || (v == c))
+				found = true;
+			else if (!vb[u][v])
+				qp.push(make_pair(u, v));
+			vb[u][v] = true;
+		};
+		for (; !found && !qp.empty(); qp.pop())
 		{
 			pair<unsigned, unsigned> p = qp.front();
-			qp.pop();
 			unsigned x = p.first, y = p.second;
-#define CHECK_AND_ADD(u, v) \
-            if ((u == c) || (v == c)) {found = true; break; } \
-            if (!vb[u][v]) { qp.push(make_pair(u, v)); vb[u][v] = true; }
-
-			CHECK_AND_ADD(x, 0);
-			CHECK_AND_ADD(0, y);
-			CHECK_AND_ADD(x, b);
-			CHECK_AND_ADD(a, y);
+			CheckAndAdd(x, 0);
+			CheckAndAdd(0, y);
+			CheckAndAdd(x, b);
+			CheckAndAdd(a, y);
 			if (x + y < a)
-			{
-				CHECK_AND_ADD(x + y, 0);
-			}
+				CheckAndAdd(x + y, 0);
 			if (x + y < b)
-			{
-				CHECK_AND_ADD(0, x + y);
-			}
+				CheckAndAdd(0, x + y);
 			if (x + y > a)
-			{
-				CHECK_AND_ADD(a, x + y - a);
-			}
+				CheckAndAdd(a, x + y - a);
 			if (x + y > b)
-			{
-				CHECK_AND_ADD(x + y - b, b);
-			}
+				CheckAndAdd(x + y - b, b);
 		}
 		cout << (found ? "YES" : "NO") << endl;
 	}
