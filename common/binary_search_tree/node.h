16b63919e93048a33b37125e441fb712a2e71ae8
Loks- <alexeypoyarkov@gmail.com>
2018-09-10 16:04:29 +0000

    Refactor bst/node.h .

--- common/binary_search_tree/node.h
+++ common/binary_search_tree/node.h
@@ -1,178 +1,100 @@
 #pragma once
 
-template <
-	class TTData,
-	class TTInfo,
-	class TTAction>
-class TBSTNodeBase
+#include "../base.h"
+
+template<bool use_height, class THeight>
+class TBSTNodeProxyHeight {};
+
+template<class THeight> 
+class TBSTNodeProxyHeight<false, THeight> {};
+
+template<class THeight>
+class TBSTNodeProxyHeight<true, THeight>
 {
 public:
+	THeight height;
+};
+
+template<bool use_key, bool use_height, class TKey, class Height>
+class TBSTNodeProxyKey {};
+
+template<bool use_height, class TKey, class THeight>
+class TBSTNodeProxyKey<false, use_height, TKey, THeight> : 
+	public TBSTNodeProxyHeight<use_height, THeight> {};
+
+template<bool use_height, class TKey, class THeight>
+class TBSTNodeProxyKey<true, use_height, TKey, THeight> : 
+	public TBSTNodeProxyHeight<use_height, THeight>
+{
+public:
+	TKey key;
+};
+
+template<bool use_key, bool use_parent, bool use_height, class TKey, class THeight, class TSelf>
+class TBSTNodeProxyParent {};
+
+template<bool use_key, bool use_height, class TKey, class THeight, class TSelf>
+class TBSTNodeProxyParent<use_key, false, use_height, TKey, THeight, TSelf> :
+	public TBSTNodeProxyKey<use_key, use_height, TKey, THeight>
+{
+public:
+	TSelf *l = 0, *r = 0;
+
+	void SetL(TSelf* node) { l = node; }
+	void SetR(TSelf* node) { r = node; }
+	void SetParentLink(TSelf* node) {}
+	void ResetLinks() { l = r = 0; }
+};
+
+template<bool use_key, bool use_height, class TKey, class THeight, class TSelf>
+class TBSTNodeProxyParent<use_key, true, use_height, TKey, THeight, TSelf> :
+	public TBSTNodeProxyKey<use_key, use_height, TKey, THeight>
+{
+public:
+	TSelf *l = 0, *r = 0, *p = 0;
+
+	void SetL(TSelf* node) { l = node; if (node) node->p = static_cast<TSelf*>(this); }
+	void SetR(TSelf* node) { r = node; if (node) node->p = static_cast<TSelf*>(this); }
+	void SetParentLink(TSelf* node) { p = node; }
+	void ResetLinks() { l = r = p = 0; }
+};
+
+template<
+	class TTData,
+	class TTInfo,
+	class TTAction,
+	bool _use_key,
+	bool _use_parent = true,
+	bool _use_height = false,
+	class TTKey = int64_t,
+	class TTHeight = unsigned>
+class BSTNode :
+	public TBSTNodeProxyParent<_use_key, _use_parent, _use_height, TTKey, TTHeight,
+		BSTNode<TTData, TTInfo, TTAction, _use_key, _use_parent, _use_height, TTKey, TTHeight>>
+{
+public:
+	static const bool use_key = _use_key;
+	static const bool use_parent = _use_parent;
+	static const bool use_height = _use_height;
+
 	using TData = TTData;
 	using TInfo = TTInfo;
 	using TAction = TTAction;
+	using TKey = TTKey;
+	using THeight = TTHeight;
+	using TSelf = BSTNode<TData, TInfo, TAction, use_key, use_parent, use_height, TKey, THeight>;
+	using TProxyParent = TBSTNodeProxyParent<use_key, use_parent, use_height, TKey, THeight, TSelf>;
 
 	TData data;
 	TInfo info;
 	TAction action;
 
 	void ClearAction() { action.Clear(); }
+	void UpdateInfo() { info.Update(this); }
+	template <class TActionValue> void AddAction(const TActionValue& value) { action.Add(this, value); }
+	void ApplyAction() { action.Apply(this); }
+	void ResetLinksAndUpdateInfo() { TProxyParent::ResetLinks(); UpdateInfo(); }
 };
-
-template<
-	class TData,
-	class TInfo,
-	class TAction,
-	bool _use_height,
-	class TTHeight>
-class TBSTNodeHeight {};
-
-template<
-	class TData,
-	class TInfo,
-	class TAction,
-	class TTHeight>
-class TBSTNodeHeight<TData, TInfo, TAction, false, TTHeight> : 
-	public TBSTNodeBase<TData,  TInfo, TAction> 
-{
-public:
-	static const bool use_height = false;
-	using THeight = TTHeight;
-};
-
-template<
-	class TData,
-	class TInfo,
-	class TAction,
-	class TTHeight>
-class TBSTNodeHeight<TData, TInfo, TAction, true, TTHeight> : 
-	public TBSTNodeBase<TData, TInfo, TAction>
-{
-public:
-	static const bool use_height = true;
-	using THeight = TTHeight;
-
-	THeight height;
-};
-
-template<
-	class TData,
-	class TInfo,
-	class TAction,
-	bool _use_key,
-	bool use_height,
-	class TTKey,
-	class THeight>
-class TBSTNodeKey {};
-
-template<
-	class TData,
-	class TInfo,
-	class TAction,
-	bool use_height,
-	class TTKey,
-	class THeight>
-class TBSTNodeKey<TData, TInfo, TAction, false, use_height, TTKey, THeight> : 
-	public TBSTNodeHeight<TData, TInfo, TAction, use_height, THeight>
-{
-public:
-	static const bool use_key = false;
-	using TKey = TTKey;
-};
-
-template<
-	class TData,
-	class TInfo,
-	class TAction,
-	bool use_height,
-	class TTKey,
-	class THeight>
-class TBSTNodeKey<TData, TInfo, TAction, true, use_height, TTKey, THeight> : 
-	public TBSTNodeHeight<TData, TInfo, TAction, use_height, THeight>
-{
-public:
-	static const bool use_key = true;
-	using TKey = TTKey;
-
-	TKey key;
-};
-
-template<
-	class TData,
-	class TInfo,
-	class TAction,
-	bool use_key,
-	bool _use_parent,
-	bool use_height,
-	class TKey,
-	class THeight>
-class TBSTNodeFinal {};
-
-template<
-	class TData,
-	class TInfo,
-	class TAction,
-	bool use_key,
-	bool use_height,
-	class TKey,
-	class THeight>
-class TBSTNodeFinal<TData, TInfo, TAction, use_key, false, use_height, TKey, THeight> :
-	public TBSTNodeKey<TData, TInfo, TAction, use_key, use_height, TKey, THeight>
-{
-public:
-	static const bool use_parent = false;
-	using TBase = TBSTNodeBase<TData, TInfo, TAction>;
-	using TSelf = TBSTNodeFinal<TData, TInfo, TAction, use_key, false, use_height, TKey, THeight>;
-
-	TSelf *l = 0, *r = 0;
-
-	void ResetLinks() { l = r = 0; }
-	void SetParentLink(TSelf* new_parent) {}
-	void SetL(TSelf* node) { l = node; }
-	void SetR(TSelf* node) { r = node; }
-	void UpdateInfo() { TBase::info.Update(this); }
-	template <class TActionValue> void AddAction(const TActionValue& value) { TBase::action.Add(this, value); }
-	void ApplyAction() { TBase::action.Apply(this); }
-	void ResetLinksAndUpdateInfo() { ResetLinks(); UpdateInfo(); }
-};
-
-template<
-	class TData,
-	class TInfo,
-	class TAction,
-	bool use_key,
-	bool use_height,
-	class TKey,
-	class THeight>
-class TBSTNodeFinal<TData, TInfo, TAction, use_key, true, use_height, TKey, THeight> :
-	public TBSTNodeKey<TData, TInfo, TAction, use_key, use_height, TKey, THeight>
-{
-public:
-	static const bool use_parent = true;
-	using TBase = TBSTNodeBase<TData, TInfo, TAction>;
-	using TSelf = TBSTNodeFinal<TData, TInfo, TAction, use_key, true, use_height, TKey, THeight>;
-
-	TSelf *l = 0, *r = 0, *p = 0;
-
-	void ResetLinks() { l = r = p = 0; }
-	void SetParentLink(TSelf* new_parent) { p = new_parent; }
-	void SetL(TSelf* node) { l = node; if (node) node->p = this; }
-	void SetR(TSelf* node) { r = node; if (node) node->p = this; }
-	void UpdateInfo() { TBase::info.Update(this); }
-	template <class TActionValue> void AddAction(const TActionValue& value) { TBase::action.Add(this, value); }
-	void ApplyAction() { TBase::action.Apply(this); }
-	void ResetLinksAndUpdateInfo() { ResetLinks(); UpdateInfo(); }
-};
-
-template <
-	class TData,
-	class TInfo,
-	class TAction,
-	bool use_key,
-	bool use_parent = true,
-	bool use_height = false,
-	class TKey = int64_t,
-	class THeight = unsigned>
-using BSTNode = TBSTNodeFinal<TData, TInfo, TAction, use_key, use_parent, use_height, TKey, THeight>;
 
 template<class TNode>
 inline TNode* Root(TNode* node)
