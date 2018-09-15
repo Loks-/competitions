1919f4bfded419a0697aebd827aa856612efd1f7
Loks- <alexeypoyarkov@gmail.com>
2018-09-15 03:45:14 +0000

    Allow TSTInfoSize inherit base class.

--- common/segment_tree/info/size.h
+++ common/segment_tree/info/size.h
@@ -1,11 +1,14 @@
 #pragma once
 
-#include "none.h"
 #include "proxy.h"
 
-class TSTInfoSize : public TSTInfoNone
+template<class TInfo>
+class TSTInfoSize : public TInfo
 {
 public:
+    using TBase = TInfo;
+	using TSelf = STInfoSize<TBase>;
+
 	static const bool is_none = false;
 	static const bool has_size = true;
 
@@ -14,9 +17,10 @@ public:
     unsigned GetSize() const { return size; }
 
 	template<class TData>
-	void UpdateData(const TData* data) { size = 1; }
+	void UpdateData(const TData* data) { TBase::UpdateData(data); size = 1; }
 
-    void UpdateLR(const TSelf& l, const TSelf& r) { size = l.size + r.size; }
+    void UpdateLR(const TSelf& l, const TSelf& r) { TBAse::UpdateLR(l, r); size = l.size + r.size; }
 };
 
-using STInfoSize = STInfoProxy<TSTInfoSize>;
+template<class TInfo>
+using STInfoSize = STInfoProxy<TSTInfoSize<TInfo>>;
