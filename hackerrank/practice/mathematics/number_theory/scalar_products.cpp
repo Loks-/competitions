d7c98a8b9052bcaa85454f6ca90553b6de3865e8
Alexey Poyarkov <alexeypoyarkov@gmail.com>
2018-09-16 22:34:08 -0700

    Small update.

--- hackerrank/practice/mathematics/number_theory/scalar_products.cpp
+++ hackerrank/practice/mathematics/number_theory/scalar_products.cpp
@@ -22,9 +22,7 @@ int main_scalar_products()
 	}
 	unordered_set<uint64_t> us;
 	for (int kl = 3; kl < 2 * n; ++kl)
-	{
 		us.insert((s2[kl + 1] + s2[kl]) % M);
-	}
 	cout << us.size() << endl;
 	return 0;
 }
