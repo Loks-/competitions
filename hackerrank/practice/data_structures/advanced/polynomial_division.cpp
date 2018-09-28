61df7c375b3ef71f90f58dc676bac602f9961253
Alexey Poyarkov <alexeypoyarkov@gmail.com>
2018-09-28 05:05:16 -0700

    Simplify solution.

--- hackerrank/practice/data_structures/advanced/polynomial_division.cpp
+++ hackerrank/practice/data_structures/advanced/polynomial_division.cpp
@@ -2,6 +2,7 @@
 
 #include "common/binary_indexed_tree/bit.h"
 #include "common/numeric/modular.h"
+#include "common/vector/read.h"
 #include "common/stl/base.h"
 
 using TModular = Modular<>;
@@ -12,32 +13,38 @@ int main_polynomial_division()
 	unsigned n, a, b, q, c;
 	cin >> n >> a >> b >> q;
 	TModular x = -TModular(b) / TModular(a);
-	vector<TModular> vx(n), vc(n);
-	vx[0] = 1;
-	for (unsigned i = 1; i < n; ++i)
-		vx[i] = vx[i - 1] * x;
-	TBIT bit(n);
-	for (unsigned i = 0; i < n; ++i)
+	if (x.Get())
 	{
-		cin >> c;
-		vc[i] = c;
-		bit.Add(i, vx[i] * vc[i]);
-	}
-	for (unsigned it = 0; it < q; ++it)
-	{
-		unsigned t, l, r;
-		cin >> t >> l >> r;
-		if (t == 1)
+		vector<TModular> vx(n);
+		vx[0] = 1;
+		for (unsigned i = 1; i < n; ++i)
+			vx[i] = vx[i - 1] * x;
+		TBIT bit(n);
+		for (unsigned i = 0; i < n; ++i)
 		{
-			TModular cnew = r;
-			bit.Add(l, vx[l] * (cnew - vc[l]));
-			vc[l] = cnew;
+			cin >> c;
+			bit.Add(i, vx[i] * c);
 		}
-		else if (t == 2)
+		for (unsigned it = 0; it < q; ++it)
 		{
-			if (x.Get() != 0)
-				cout << ((bit.Sum(l, r + 1).Get() == 0) ? "Yes" : "No") << endl;
-			else
+			unsigned t, l, r;
+			cin >> t >> l >> r;
+			if (t == 1)
+				bit.Add(l, vx[l] * TModular(r) - bit.Get(l));
+			else if (t == 2)
+				cout << ((bit.Sum(l, r + 1) == 0) ? "Yes" : "No") << endl;
+		}
+	}
+	else
+	{
+		vector<unsigned> vc = ReadVector<unsigned>(n);
+		for (unsigned it = 0; it < q; ++it)
+		{
+			unsigned t, l, r;
+			cin >> t >> l >> r;
+			if (t == 1)
+				vc[l] = r;
+			else if (t == 2)
 				cout << ((vc[l] == 0) ? "Yes" : "No") << endl;
 		}
 	}
