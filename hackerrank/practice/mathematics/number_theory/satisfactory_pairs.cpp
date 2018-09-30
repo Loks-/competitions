601781313c2771f954e572ba89a80f4f11f1d69c
Loks- <alexeypoyarkov@gmail.com>
2018-09-30 13:07:20 +0000

    Drop PrimesList and speed up solution for "Satisfactory Pairs" problem.

--- hackerrank/practice/mathematics/number_theory/satisfactory_pairs.cpp
+++ hackerrank/practice/mathematics/number_theory/satisfactory_pairs.cpp
@@ -1,6 +1,5 @@
 // https://www.hackerrank.com/challenges/pairs-again
 
-#include "common/numeric/primes_list.h"
 #include "common/vector/unsigned_set.h"
 #include "common/stl/base.h"
 
@@ -8,15 +7,12 @@
 
 int main_satisfactory_pairs()
 {
-    PrimesList pl(600);
     unsigned n;
     cin >> n;
     vector<vector<uint64_t>> vvd(n);
-    for (unsigned i = 1; i < n; ++i)
-    {
-        vvd[i] = pl.GetDivisors(i);
-        sort(vvd[i].begin(), vvd[i].end());
-    }
+    for (unsigned i = n - 1; i; --i)
+        for (unsigned ix = i; ix < n; ix += i)
+            vvd[ix].push_back(i);
     UnsignedSet us(n);
     uint64_t total = 0;
     for (unsigned i = 1; i < n; ++i)
@@ -26,6 +22,8 @@ int main_satisfactory_pairs()
             for (unsigned j : vvd[n - ix])
                 if (i < j)
                     us.Insert(j);
+                else
+                    break;
         }
         total += us.Size();
         us.Clear();
