430f28b821b0647f014495507d1401d044ba17de
Loks- <alexeypoyarkov@gmail.com>
2018-10-01 05:54:18 +0000

    Add solution and tests for "Divisor Exploration II" problem.

---
+++ hackerrank/practice/mathematics/number_theory/divisor_exploration_ii.cpp
@@ -0,0 +1,26 @@
+// https://www.hackerrank.com/challenges/divisor-exploration-2
+
+#include "common/numeric/modular.h"
+#include "common/numeric/primes_list.h"
+#include "common/stl/base.h"
+
+using TModular = Modular<>;
+
+int main_divisor_exploration_ii()
+{
+    PrimesList pl(10000000);
+	unsigned T, m, a;
+	cin >> T;
+	for (unsigned it = 0; it < T; ++it)
+	{
+        TModular r = 1, one = 1;
+        cin >> m >> a;
+        for (unsigned i = 0; i < m; ++i)
+        {
+            TModular p = pl.vprimes[i];
+            r *= ((p.PowU(a + i + 2) - one) / (p - one) * p - TModular(a + i + 2)) / (p - one);
+        }
+        cout << r.Get() << endl;
+	}
+	return 0;
+}
