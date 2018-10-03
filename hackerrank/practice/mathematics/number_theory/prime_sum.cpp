bbf0ebec78bc7f0234cf225ee352dd39ff303011
Loks- <alexeypoyarkov@gmail.com>
2018-10-03 11:53:02 +0000

    Add solution and tests for "Prime Sum" problem.

---
+++ hackerrank/practice/mathematics/number_theory/prime_sum.cpp
@@ -0,0 +1,25 @@
+// https://www.hackerrank.com/challenges/prime-sum
+
+#include "common/numeric/primality_test.h"
+#include "common/stl/base.h"
+
+int main_prime_sum()
+{
+    MillerRabinPrimalityTest pt;
+	unsigned T;
+	cin >> T;
+	for (unsigned it = 0; it < T; ++it)
+	{
+        uint64_t N, K;
+        cin >> N >> K;
+        bool ok;
+        if (K == 1)
+            ok = pt.IsPrime(N);
+        else if ((N & 1) && (K == 2))
+            ok = (N > 2) && pt.IsPrime(N - 2);
+        else
+            ok = (N >= 2 * K);
+        cout << (ok ? "Yes" : "No") << endl;
+	}
+	return 0;
+}
