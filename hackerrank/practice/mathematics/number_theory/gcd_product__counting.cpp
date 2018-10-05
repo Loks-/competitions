fad815398a457f75cd5c77e4f24aee60c8ed5db7
Loks- <alexeypoyarkov@gmail.com>
2018-10-05 11:17:57 -0700

    Speed up solution.

--- hackerrank/practice/mathematics/number_theory/gcd_product__counting.cpp
+++ hackerrank/practice/mathematics/number_theory/gcd_product__counting.cpp
@@ -14,13 +14,22 @@ int main_gcd_product__counting()
     vector<uint64_t> vc(m + 1);
     for (uint64_t i = 1; i <= m; ++i)
         vc[i] = (n / i) * (m / i);
-    TModular r = 1;
-    for (uint64_t i = m; i > 1; --i)
+    vector<unsigned> vskip(max(n, uint64_t(4000)) + 1, 1);
+    for (uint64_t i = 2; i <= 4000; ++i)
+        vskip[i] = 0;
+    for (uint64_t k = 1; 4000 * k <= n; ++k)
+        vskip[m / k] = vskip[n / k] = 0;
+    for (uint64_t i = m / 2; i > 1; --i)
     {
-        for (uint64_t j = 2 * i; j <= m; j += i)
-            vc[i] -= vc[j];
-        r *= TModular(i).PowU(vc[i]);
+        if (vskip[i])
+            vc[i] = vc[i+1];
+        else
+            for (uint64_t j = 2 * i; j <= m; j += i)
+                vc[i] -= vc[j];
     }
+    TModular r = 1;
+    for (uint64_t i = 2; i <= m; ++i)
+        r *= TModular(i).PowU(vc[i]);
     cout << r.Get() << endl;
 	return 0;
 }
