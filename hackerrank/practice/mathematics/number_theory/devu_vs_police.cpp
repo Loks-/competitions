b061318bdc177b48cb56c57e448ed6684d5f1004
Loks- <alexeypoyarkov@gmail.com>
2018-10-05 03:24:14 +0000

    Switch to TPLEEulersTotient class.

--- hackerrank/practice/mathematics/number_theory/devu_vs_police.cpp
+++ hackerrank/practice/mathematics/number_theory/devu_vs_police.cpp
@@ -1,8 +1,7 @@
 // https://www.hackerrank.com/challenges/devu-police
 
-#include "common/numeric/factorization/eulers_totient.h"
 #include "common/numeric/modular_arithmetic.h"
-#include "common/numeric/primes_list.h"
+#include "common/numeric/primes_list_extended/eulers_totient.h"
 #include "common/numeric/utils/pow.h"
 #include "common/stl/base.h"
 
@@ -10,7 +9,7 @@ using TModular = TModularArithmetic_C32U;
 
 int main_devu_vs_police()
 {
-    PrimesList primes_list(10000);
+    TPLEEulersTotient primes_list(10000);
 	unsigned T;
 	cin >> T;
 	for (unsigned it = 0; it < T; ++it)
@@ -19,7 +18,7 @@ int main_devu_vs_police()
         cin >> n1 >> k1 >> n2 >> k2 >> n;
         bool small_power = ((n2 == 0) || (n2 == 1) || (k2 == 0) || ((n2 == 2) && (k2 <= 5)) || ((n2 == 3) && (k2 <= 2)) || ((n2 <= 24) && (k2 == 1)));
         n1 = TModular::PowU(n1, k1, n);
-        n3 = EulersTotient(n, primes_list.Factorize(n));
+        n3 = primes_list.EulersTotient(n);
         n2 = (small_power ? PowU(n2, k2) : 24 * n3 + TModular::PowU(n2, k2, n3));
         cout << TModular::PowU(n1, n2, n) % n << endl;
 	}
