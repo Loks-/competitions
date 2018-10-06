8f9b3f04c1f3945b72868232c670e2bcd2af7c41
Loks- <alexeypoyarkov@gmail.com>
2018-10-05 18:22:58 -0700

    Small fix.

--- ipcs/2018/raw_data.py
+++ ipcs/2018/raw_data.py
@@ -2,7 +2,7 @@ import requests
 
 session = requests.Session()
 URL_LOGIN = "https://ipsc.ksp.sk/2018/login"
-login_request = session.post(url = URL_LOGIN, data={"username" : "Loks", "password" : "02121982"})
+login_request = session.post(url = URL_LOGIN, data={"username" : "Loks", "password" : "q1w2e3"})
 URL_R1 = "https://ipsc.ksp.sk/2018/practice/problems/r1"
 r = session.post(url = URL_R1) 
 r = session.post(url = URL_R1) 
