2ce2cf7a3a81f7c056b537fa986566ae507fa785
Loks- <alexeypoyarkov@gmail.com>
2018-10-06 13:35:04 +0000

    Finalize solution for "A Very Special Multiple" problem.

--- hackerrank/practice/mathematics/number_theory/a_very_special_multiple.cpp
+++ hackerrank/practice/mathematics/number_theory/a_very_special_multiple.cpp
@@ -1,24 +1,38 @@
 // https://www.hackerrank.com/challenges/a-very-special-multiple
 
+#include "common/numeric/factorization/eulers_totient.h"
+#include "common/numeric/factorization/divisors.h"
+#include "common/numeric/modular_arithmetic.h"
+#include "common/numeric/primes_list.h"
 #include "common/numeric/utils/pow.h"
 #include "common/stl/base.h"
 
+using TModular = ModularArithmetic<false, false, uint64_t>;
+
 int main_a_very_special_multiple()
 {
+    PrimesList primes_list(100000);
 	unsigned T;
 	cin >> T;
 	for (unsigned it = 0; it < T; ++it)
 	{
         uint64_t x;
         cin >> x;
-        uint64_t xt = x, p2 = 0, p5 = 0;
-        for (; (xt % 2) == 0; xt /= 2) ++p2;
-        for (; (xt % 5) == 0; xt /= 5) ++p5;
-        uint64_t b = max<uint64_t>(p5, max<uint64_t>(p2, 2) - 2), a = 1;
-        for (uint64_t t = (4 * PowU<uint64_t>(10, b)) % x, ts = t; ts; ++a)
+        uint64_t p2 = 0, p5 = 0;
+        for (; (x % 2) == 0; x /= 2) ++p2;
+        for (; (x % 5) == 0; x /= 5) ++p5;
+        uint64_t b = max<uint64_t>(p5, max<uint64_t>(p2, 2) - 2), a;
+        x *= 9;
+        uint64_t phi = EulersTotient(x, primes_list.Factorize(x));
+        vector<uint64_t> vd = GetDivisors(primes_list.Factorize(phi));
+        sort(vd.begin(), vd.end());
+        for (uint64_t d : vd)
         {
-            t = (t * 10) % x;
-            ts = (ts + t) % x;
+            if (TModular::PowU(10, d, x) == 1)
+            {
+                a = d;
+                break;
+            }
         }
         cout << 2 * a + b << endl;
 	}
