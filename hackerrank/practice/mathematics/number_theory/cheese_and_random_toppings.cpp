4a80f4f0ee692adc4d7a9b4b8be4cce755b0f16c
Loks- <alexeypoyarkov@gmail.com>
2018-10-10 02:03:43 +0000

    Rename BinomialCoefficient -> BinomialCoefficientPrimeModular.

--- hackerrank/practice/mathematics/number_theory/cheese_and_random_toppings.cpp
+++ hackerrank/practice/mathematics/number_theory/cheese_and_random_toppings.cpp
@@ -1,7 +1,7 @@
 // https://www.hackerrank.com/challenges/cheese-and-random-toppings
 
 #include "common/numeric/primes_list.h"
-#include "common/numeric/utils/binomial_coefficient.h"
+#include "common/numeric/utils/binomial_coefficient_prime_modular.h"
 #include "common/numeric/utils/merge_remainders.h"
 #include "common/stl/base.h"
 
@@ -17,7 +17,7 @@ int main_cheese_and_random_toppings()
         TFactorization vf = primes_list.Factorize(M);
         for (auto pf : vf)
         {
-            uint64_t rf = BinomialCoefficient(N, R, unsigned(pf.first));
+            uint64_t rf = BinomialCoefficientPrimeModular(N, R, unsigned(pf.first));
             r = MergeRemainders<TModularArithmetic_P32U>(m, r, pf.first, rf);
             m *= pf.first;
         }
