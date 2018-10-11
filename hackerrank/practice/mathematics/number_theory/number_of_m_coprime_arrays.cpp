4e0e7579ddd0c09dd56fbf1615659ee9af2b1fef
Loks- <alexeypoyarkov@gmail.com>
2018-10-11 11:16:23 -0700

    Simplify and speed up solution for "Number of M-Coprime Arrays" problem.

--- hackerrank/practice/mathematics/number_theory/number_of_m_coprime_arrays.cpp
+++ hackerrank/practice/mathematics/number_theory/number_of_m_coprime_arrays.cpp
@@ -1,7 +1,6 @@
 // https://www.hackerrank.com/challenges/number-of-m-coprime-arrays
 
-#include "common/linear_algebra/matrix.h"
-#include "common/linear_algebra/vector.h"
+#include "common/linear_algebra/matrix_static_size.h"
 #include "common/numeric/modular.h"
 #include "common/numeric/primes_list_extended/primes_powers.h"
 #include "common/stl/base.h"
@@ -9,12 +8,14 @@
 #include <numeric>
 
 using TModular = Modular<>;
-using TVector = Vector<TModular>;
-using TMatrix = Matrix<TModular>;
+using TMatrix = TSquareMatrixStaticSize<TModular, 2>;
 
 int main_number_of_m_coprime_arrays()
 {
     PLEPrimesPowers primes_list(1000000);
+    TMatrix m;
+    m(0, 0) = m(0, 1) = 1;
+
 	unsigned T;
 	cin >> T;
 	for (unsigned it = 0; it < T; ++it)
@@ -22,32 +23,13 @@ int main_number_of_m_coprime_arrays()
         uint64_t N, M;
         cin >> N >> M;
         vector<unsigned> fp = primes_list.GetPrimesPowers(M);
-        cout << fp.size() << "\t" << M << endl;
-
-        unsigned p2fp = (1u << fp.size());
-        vector<uint64_t> vc(p2fp, 1);
-        for (unsigned i = 0; i < p2fp; ++i)
+        TModular r = 1;
+        for (unsigned q : fp)
         {
-            for (unsigned j = 0; j < fp.size(); ++j)
-            {
-                if (i & (1u << j))
-                    vc[i] *= fp[j];
-            }
+            m(1, 0) = q;
+            r *= m.PowU(N + 1)(0, 0);
         }
-
-        TVector v(p2fp, 0); v(0) = 1;
-        TMatrix m(p2fp, p2fp);
-        for (unsigned i = 0; i < p2fp; ++i)
-        {
-            TModular im(vc[i]);
-            for (unsigned j = 0; j < p2fp; ++j)
-            {
-                if ((i & j) == 0)
-                    m(i, j) = im;
-            }
-        }
-        TVector vf = m.PowU(N) * v;
-        cout << accumulate(vf.begin(), vf.end(), TModular(0)).Get() << endl;
+        cout << r.Get() << endl;
 	}
 	return 0;
 }
