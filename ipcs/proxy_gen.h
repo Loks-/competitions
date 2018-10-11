30c976cc891e2efc52c30f4a8f6ec8c08f50b4fd
Loks- <alexeypoyarkov@gmail.com>
2018-10-11 01:44:49 +0000

    Remove auto generated files from git.

--- ipcs/proxy_gen.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-int main_armed_bandit();
-int main_encrypted_romance_1();
-int main_jumping_over_walls();
-int main_partitioning_a_square();
-int main_qizz_quzz_q1();
-int main_qizz_quzz_q2();
