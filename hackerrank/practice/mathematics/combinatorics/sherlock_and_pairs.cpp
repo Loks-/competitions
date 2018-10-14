5c1b03ad9890e8cfc6d64f984ab48ca82947c29c
Loks- <alexeypoyarkov@gmail.com>
2018-10-14 00:52:08 +0000

    Add solution and tests for "Sherlock and Pairs" problem.

---
+++ hackerrank/practice/mathematics/combinatorics/sherlock_and_pairs.cpp
@@ -0,0 +1,25 @@
+// https://www.hackerrank.com/challenges/sherlock-and-pairs
+
+#include "common/vector/read.h"
+#include "common/stl/base.h"
+
+#include <unordered_map>
+
+int main_sherlock_and_pairs()
+{
+    unsigned T, N;
+    cin >> T;
+    for (unsigned i = 0; i < T; ++i)
+    {
+        cin >> N;
+        vector<unsigned> va = ReadVector<unsigned>(N);
+        unordered_map<unsigned, uint64_t> m;
+        for (unsigned a : va)
+            m[a] += 1;
+        uint64_t s = 0;
+        for (auto p : m)
+            s += p.second * (p.second - 1);
+        cout << s << endl;
+    }
+    return 0;
+}
