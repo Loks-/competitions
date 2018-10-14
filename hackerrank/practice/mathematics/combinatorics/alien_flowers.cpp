dad2851b8dd4bdfc389c7a05ed385c3a09449d10
Alexey Poyarkov <alexeypoyarkov@gmail.com>
2018-10-13 19:30:18 -0700

    Add solution and tests for "Alien Flowers" problem.

---
+++ hackerrank/practice/mathematics/combinatorics/alien_flowers.cpp
@@ -0,0 +1,32 @@
+// https://www.hackerrank.com/challenges/alien-flowers
+
+#include "common/modular/factorial_modular.h"
+#include "common/modular/modular.h"
+#include "common/stl/base.h"
+
+using TModular = Modular<>;
+using TFactorial = FactorialModular<TModular>;
+
+int main_alien_flowers()
+{
+	TFactorial f;
+	unsigned A, B, C, D;
+	cin >> A >> B >> C >> D;
+	TModular res = 0;
+	if (B == D + 1)
+		res = f.BinomialCoefficient(D + A, D) * f.BinomialCoefficient(D + C, D);
+	else if (D == B + 1)
+		res = f.BinomialCoefficient(B + A, B) * f.BinomialCoefficient(B + C, B);
+	else if (B == D)
+	{
+		if (D > 0)
+			res = f.BinomialCoefficient(D + A, D) * f.BinomialCoefficient(D + C - 1, D - 1) +
+			f.BinomialCoefficient(D + A - 1, D - 1) * f.BinomialCoefficient(D + C, D);
+		else if (A + C == 0)
+			res = 2;
+		else if (A * C == 0)
+			res = 1;
+	}
+	cout << res.Get() << endl;
+	return 0;
+}
