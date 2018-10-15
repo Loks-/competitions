e20fd02ba3f744242fe7c08f7a6795cb9236938c
Loks- <alexeypoyarkov@gmail.com>
2018-10-15 09:46:10 -0700

    Speed up solution for "GCD mocktail" problem.

--- hackerrank/practice/mathematics/number_theory/gcd_mocktail.cpp
+++ hackerrank/practice/mathematics/number_theory/gcd_mocktail.cpp
@@ -1,6 +1,7 @@
 // https://www.hackerrank.com/challenges/gcd-mocktail
 
 #include "common/modular/modular.h"
+// #include "common/numeric/utils/usqrt.h"
 #include "common/stl/base.h"
 
 using TModular = Modular<30000001>;
@@ -12,20 +13,40 @@ int main_gcd_mocktail()
     for (unsigned iT = 0; iT < T; ++iT)
     {
         cin >> N >> D >> Q;
-        vector<TModular> v(N + 1);
-        for (unsigned k = 1; k <= N; ++k)
-            v[k] = TModular(N / k).PowU(D);
-        for (unsigned k = N / 2; k; --k)
+        // unsigned sN = USqrt<unsigned>(N);
+        vector<unsigned> vk;
+        vector<TModular> vc;
+        // O(N + 2 * sqrt(N) log D)
+        for (unsigned k = 1; k <= N + 1; ++k)
         {
-            for (unsigned l = 2 * k; l <= N; l += k)
-                v[k] -= v[l];
+            if ((k > 1) && (N / k == N / (k-1)))
+                continue;
+            vk.push_back(k);
+            vc.push_back(TModular(N / k).PowU(D));
+            // cout << "\t" << vk.back() << "\t" << vc.back().Get() << endl;
         }
+        // O(N * log N * log N)
+        for (unsigned i = vk.size(); i--; )
+        {
+            unsigned k = vk[i];
+            for (unsigned l = 2 * k; l <= N; l += k)
+            {
+                auto itk = upper_bound(vk.begin(), vk.end(), l);
+                vc[i] -= vc[itk - vk.begin() - 1];
+            }
+            // cout << "\t" << k << "\t" << vc[i].Get() << endl;
+        }
+        // O(Q * N * log L)
         for (unsigned iQ = 0; iQ < Q; ++iQ)
         {
             cin >> L;
             TModular r = 0;
-            for (unsigned k = 1; k <= N; ++k)
-                r += v[k] * TModular(k).PowU(L);
+            for (unsigned i = 0, k = 1; k <= N; ++k)
+            {
+                if (vk[i + 1] <= k)
+                    ++i;
+                r += vc[i] * TModular(k).PowU(L);
+            }
             cout << r.Get() << endl;
         }
     }
