f040b99dc6c257fad2e967e2c5e0576484cd1e53
Loks- <alexeypoyarkov@gmail.com>
2018-10-17 09:22:00 -0700

    Add solution and tests for "Eugene and Big Number" problem.

---
+++ hackerrank/practice/mathematics/number_theory/eugene_and_big_number.cpp
@@ -0,0 +1,21 @@
+// https://www.hackerrank.com/challenges/eugene-and-big-number
+
+#include "common/modular/modular_arithmetic_proxy.h"
+#include "common/stl/base.h"
+
+using TModularProxy = ModularArithmeticProxy<false, false, uint64_t>;
+
+int main_eugene_and_big_number()
+{
+	unsigned T;
+	cin >> T;
+	for (unsigned it = 0; it < T; ++it)
+	{
+		uint64_t A, N, M, m = 9, l = 1;
+		cin >> A >> N >> M;
+        for (; m < A; ++l) m = 10 * m + 9;
+        TModularProxy mp(M * m);
+        cout << mp.MultSafe(mp.Sub(mp.PowUSafe(10, N * l), 1), A) / m << endl;
+	}
+	return 0;
+}
