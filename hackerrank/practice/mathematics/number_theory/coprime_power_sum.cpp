147c41a0a50cace623419c3ba6fe11f8cef95c6b
Loks- <alexeypoyarkov@gmail.com>
2018-10-18 09:06:16 -0700

    Add cache to solution for "Coprime Power Sum" problem.

--- hackerrank/practice/mathematics/number_theory/coprime_power_sum.cpp
+++ hackerrank/practice/mathematics/number_theory/coprime_power_sum.cpp
@@ -7,13 +7,14 @@
 #include "common/stl/base.h"
 
 #include <functional>
+#include <unordered_map>
 
 using TModular = Modular<>;
 using TPolynom = BaseNewtonPolynomial<TModular>;
 
 int main_coprime_power_sum()
 {
-    unsigned T, N, K;
+    unsigned T, N, K, maxn = 50;
     cin >> T;
     for (unsigned iT = 0; iT < T; ++iT)
     {
@@ -21,15 +22,22 @@ int main_coprime_power_sum()
         cin >> N >> K >> M;
         TPolynom p = GetSumOfPowers<TModular>(K);
         vector<uint64_t> vs = ReadVector<uint64_t>(N);
+        sort(vs.begin(), vs.end());
+        vs.push_back(M + 1);
         vector<TModular> vspk;
         for (uint64_t s : vs)
             vspk.push_back(TModular(s).PowU(K));
+        unordered_map<uint64_t, TModular> cache;
 
         std::function<TModular(uint64_t, unsigned)> SolveR = [&](uint64_t rM, unsigned i) -> TModular
         {
             if (rM == 0) return 0;
-            if (i >= N) return p.Apply(TModular(rM));
-            return SolveR(rM, i + 1) - SolveR(rM / vs[i], i + 1) * vspk[i];
+            uint64_t key = rM * (maxn + 1) + i;
+            auto it = cache.find(key);
+            if (it != cache.end()) return it->second;
+            TModular r = (vs[i] > rM) ? p.Apply(TModular(rM)) : SolveR(rM, i + 1) - SolveR(rM / vs[i], i + 1) * vspk[i];
+            cache[key] = r;
+            return r;
         };
 
         cout << SolveR(M, 0).Get() << endl;
