3aa3abb0625e290d4fcbe657b0a77cbce3da44bd
Loks- <alexeypoyarkov@gmail.com>
2018-10-20 00:38:05 -0700

    Fix code for MatrixRank.

--- common/linear_algebra/utils/rank.h
+++ common/linear_algebra/utils/rank.h
@@ -2,7 +2,6 @@
 
 #include "../matrix.h"
 #include <algorithm>
-#include <iostream>
 
 template<class TValue>
 unsigned MatrixRank(const Matrix<TValue>& matrix)
@@ -17,10 +16,8 @@ unsigned MatrixRank(const Matrix<TValue>& matrix)
             {
                 if (i != r)
                 {
-                    for (unsigned k = j; k < m.Columns(); ++k)
-                        std::swap(m(r, k), m(i, k));
-                    // TValue *pr = &(m(r, j)), *pr_end = pr + (m.Rows() - j), *pi = &(m(i, j));
-                    // for (; pr < pr_end; ++pi, ++pr) std::swap(*pi, *pr);
+                    TValue *pr = &(m(r, j)), *pr_end = pr + (m.Columns() - j), *pi = &(m(i, j));
+                    for (; pr < pr_end; ++pi, ++pr) std::swap(*pi, *pr);
                 }
                 break;
             }
@@ -30,11 +27,8 @@ unsigned MatrixRank(const Matrix<TValue>& matrix)
         {
             if (m(i, j) == 0) continue;
             TValue rm = m(r, j) / m(i, j);
-            for (unsigned k = j; k < m.Columns(); ++k)
-                m(i, k) -= rm * m(r, k);
-            // TValue *pr = &(m(r, j)), *pr_end = pr + (m.Rows() - j), *pi = &(m(i, j));
-            // std::cout << "\t" << i << "\t" << pr << "\t" << pr_end << "\t" << pi << std::endl;
-            // for (; pr < pr_end; ) *pi++ -= rm * *pr++;
+            TValue *pr = &(m(r, j)), *pr_end = pr + (m.Columns() - j), *pi = &(m(i, j));
+            for (; pr < pr_end; ) *pi++ -= rm * *pr++;
         }
         ++r;
     }
