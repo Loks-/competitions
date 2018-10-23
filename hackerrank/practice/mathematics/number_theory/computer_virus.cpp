dff0c0c88a160dfbd836ddd94c7b075e98b4ac5c
Loks- <alexeypoyarkov@gmail.com>
2018-10-22 18:14:08 -0700

    Add solution and tests for "Computer Virus" problem.

---
+++ hackerrank/practice/mathematics/number_theory/computer_virus.cpp
@@ -0,0 +1,28 @@
+// https://www.hackerrank.com/challenges/demidenko-computer-virus
+
+#include "common/modular/modular.h"
+#include "common/modular/factorial_modular.h"
+#include "common/stl/base.h"
+
+using TModular = Modular<>;
+
+int main_computer_virus()
+{
+    FactorialModular<TModular> f;
+	unsigned T, K;
+	cin >> T;
+	for (unsigned it = 0; it < T; ++it)
+	{
+        uint64_t N;
+        cin >> K >> N;
+        TModular s = 0, nf = 1, p2 = 1;
+        for (unsigned i = 0; i <= K; ++i)
+        {
+            s += p2 * f.BinomialCoefficient(K, i) * nf * f.GetI(i);
+            nf *= TModular(N - i);
+            p2 *= TModular(2);
+        }
+        cout << s.Get() << endl;
+	}
+	return 0;
+}
