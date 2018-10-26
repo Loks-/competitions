780a7eeaf56acf36e89ef07b7ab37f3b36956784
Loks- <alexeypoyarkov@gmail.com>
2018-10-26 05:28:01 +0000

    Update solution for "Sherlock and Divisors" using Factorization class.

--- hackerrank/practice/mathematics/fundamentals/sherlock_and_divisors.cpp
+++ hackerrank/practice/mathematics/fundamentals/sherlock_and_divisors.cpp
@@ -1,18 +1,18 @@
 // https://www.hackerrank.com/challenges/sherlock-and-divisors
 
+#include "common/factorization/factorization.h"
 #include "common/factorization/utils/divisors.h"
-#include "common/factorization/primes_list.h"
 #include "common/stl/base.h"
 
 int main_sherlock_and_divisors()
 {
+    Factorization ff;
 	unsigned T, N;
-	PrimesList primes_list(100000);
 	cin >> T;
 	for (unsigned it = 0; it < T; ++it)
 	{
 		cin >> N;
-		cout << ((N & 1) ? 0 : GetDivisorsCount(primes_list.Factorize(N / 2))) << endl;
+		cout << ((N & 1) ? 0 : GetDivisorsCount(ff.Factorize(N / 2))) << endl;
 	}
 	return 0;
 }
