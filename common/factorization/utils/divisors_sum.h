cb813221e9b2f3f8fe84146b97dd83663bbd2b29
Alexey Poyarkov <alexeypoyarkov@gmail.com>
2018-10-25 20:01:32 -0700

    Add solution and tests for "Nice Clique" problem.

---
+++ common/factorization/utils/divisors_sum.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include "../base.h"
+#include "../../numeric/utils/pow.h"
+
+inline uint64_t GetDivisorsSum(const TFactorization& factorization)
+{
+	uint64_t s = 1;
+	for (auto p : factorization)
+		s *= ((p.second == 1) ? p.first + 1 : (PowU(p.first, p.second + 1) - 1) / (p.first - 1));
+	return s;
+}
