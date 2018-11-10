ec461736728dc7dc6f21c0d88c7e15bf58a8d55e
Loks- <alexeypoyarkov@gmail.com>
2018-11-10 19:22:43 +0000

    Add solution and tests for "Lazy Sorting" problem.

---
+++ hackerrank/practice/mathematics/probability/lazy_sorting.cpp
@@ -0,0 +1,30 @@
+// https://www.hackerrank.com/challenges/lazy-sorting
+
+#include "common/numeric/factorial.h"
+#include "common/stl/base.h"
+
+int main_lazy_sorting()
+{
+    Factorial f;
+	unsigned N, x, lx = 0;
+	cin >> N;
+	vector<unsigned> v(101, 0);
+	bool sorted = true;
+	for (unsigned it = 0; it < N; ++it)
+	{
+		cin >> x;
+		if (x < lx) sorted = false;
+		lx = x;
+		++v[x];
+	}
+	if (sorted)
+	{
+		cout << "0.000000" << endl;
+		return 0;
+	}
+	uint64_t s = f(N);
+	for (unsigned i : v)
+		s /= f(i);
+	cout << s << ".000000" << endl;
+	return 0;
+}
