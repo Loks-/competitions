75322969e0db169623aa6cd330bcd5bac154bb06
Loks- <alexeypoyarkov@gmail.com>
2018-11-11 22:17:47 +0000

    Add PLEMertens class.

---
+++ common/factorization/primes_list_extended/mertens.h
@@ -0,0 +1,39 @@
+#pragma once
+
+#include "mobius.h"
+#include "../primes_list.h"
+#include "../../assert.h"
+#include <vector>
+
+template<class TPrimesList>
+class PLEMertens : public PLEMobius<TPrimesList>
+{
+public:
+    using TBase = PLEMobius<TPrimesList>;
+    using TSelf = PLEMertens<TPrimesList>;
+
+protected:
+    std::vector<int> mertens;
+
+public:
+    PLEMobius(uint64_t size) : TPrimesList(size) {}
+
+    void MertensPrecalc()
+    {
+        if (TBase::mobius.size() == 0) TBase::MobiusPrecalc();
+        assert(TBase::mobius.size() == TPrimesList::table_size + 1);
+        mertens.resize(TPrimesList::table_size + 1);
+        mertens[0] = 0;
+        for (unsigned i = 1; i <= TPrimesList::table_size; ++i)
+            mertens[i] = mertens[i-1] + TBase::mobius[i];
+    }
+    
+    int Mertens(uint64_t n) const
+    {
+        if (mertens.size() == 0) MertensPrecalc();
+        assert(n < mertens.size());
+        return mertens[i];
+    }
+};
+
+using TPLEMertens = PLEMertens<PrimesList>;
