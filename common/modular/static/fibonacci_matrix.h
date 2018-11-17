243d852111ce3cce23394e9763c12090892c2d5e
Alexey Poyarkov <alexeypoyarkov@gmail.com>
2018-11-16 18:27:04 -0800

    Add solution and tests for "Fibonacci Numbers Tree" problem.

--- common/modular/static/fibonacci_matrix.h
+++ common/modular/static/fibonacci_matrix.h
@@ -2,6 +2,7 @@
 
 #include "../../base.h"
 #include "../../linear_algebra/matrix_static_size.h"
+#include <utility>
 
 template<class TModular>
 class ModularFibonacciMatrix
@@ -9,7 +10,8 @@ class ModularFibonacciMatrix
 public:
 	using TMatrix = TSquareMatrixStaticSize<TModular, 2>;
 
-	static TModular GetU(uint64_t n, TModular f0, TModular f1)
+	// Return { F_{n-1}, F_n }.
+	static std::pair<TModular, TModular> GetPU(uint64_t n)
 	{
 		TMatrix transition;
 		transition(0, 0) = 0;
@@ -17,7 +19,18 @@ public:
 		transition(1, 0) = 1;
 		transition(1, 1) = 1;
 		TMatrix fn = transition.PowU(n);
-		return fn(0, 0) * f0 + fn(0, 1) * f1;
+		return { fn(0, 0), fn(0, 1) };
 	}
-	static TModular GetU(uint64_t n) { return GetU(n, 0, 1); }
+
+	static std::pair<TModular, TModular> GetPS(int64_t n)
+	{
+		if (n >= 0) return GetPU(uint64_t(n));
+		auto p = GetPU(uint64_t(-n) + 1);
+		return (n & 1) ? std::make_pair(-p.second, p.first) : std::make_pair(p.second, -p.first);
+	}
+
+	static TModular GetU(uint64_t n) { return GetPU(n).second; }
+	static TModular GetU(uint64_t n, TModular f0, TModular f1) { auto p = GetPU(n); return p.first * f0 + p.second * f1; }
+	static TModular GetS(int64_t n) { return GetPS(n).second; }
+	static TModular GetS(int64_t n, TModular f0, TModular f1) { auto p = GetPS(n); return p.first * f0 + p.second * f1; }
 };
