5f0b9778da667b9b2d2732da6633d4d0dee4d2cd
Loks- <alexeypoyarkov@gmail.com>
2018-11-17 21:53:30 +0000

    Add solution and tests for "Journey to Mars" problem.

---
+++ hackerrank/practice/mathematics/combinatorics/journey_to_mars.cpp
@@ -0,0 +1,25 @@
+// https://www.hackerrank.com/challenges/ajourney
+
+#include "common/modular/arithmetic.h"
+#include "common/numeric/utils/pow.h"
+#include "common/stl/base.h"
+
+using TModularA = TModularArithmetic_C32U;
+
+#include <cmath>
+
+int main_journey_to_mars()
+{
+    unsigned T, N, K;
+    cin >> T;
+    for (unsigned iT = 0; iT < T; ++iT)
+    {
+        cin >> N >> K;
+        unsigned P10K = PowU(10u, K);
+        long double P2l = (N - 1) * log10l(2.0L);
+        uint64_t l1 = uint64_t(P10K * exp10l(P2l - uint64_t(P2l))) / 10;
+        uint64_t l2 = TModularA::PowU(2, N - 1, P10K);
+        cout << l1 + l2 << endl;
+    }
+	return 0;
+}
