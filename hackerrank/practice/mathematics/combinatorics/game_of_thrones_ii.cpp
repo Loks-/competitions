2be523ae5d309f2a19db8c9213a771f4e001ec94
Loks- <alexeypoyarkov@gmail.com>
2018-11-17 21:22:35 +0000

    Add solution and tests for "Game Of Thrones - II" problem.

---
+++ hackerrank/practice/mathematics/combinatorics/game_of_thrones_ii.cpp
@@ -0,0 +1,32 @@
+// https://www.hackerrank.com/challenges/game-of-throne-ii
+
+#include "common/modular/static/factorial.h"
+#include "common/modular/static/modular_io.h"
+#include "common/stl/base.h"
+
+#include <string>
+
+using TModular = Modular<>;
+using TFactorial = ModularFactorial<TModular, false>;
+
+int main_game_of_thrones_ii()
+{
+    TFactorial f;
+    string s;
+    cin >> s;
+    sort(s.begin(), s.end());
+    unsigned n = unsigned(s.size());
+    s.push_back(' ');
+    unsigned ls = 0, so = 0;
+    TModular r = 1;
+    for (unsigned i = 1; i <= n; ++i)
+    {
+        if (s[i] == s[i-1]) continue;
+        unsigned l = i - ls;
+        ls = i;
+        so += (l & 1);
+        r *= f(l / 2);
+    }
+    cout << ((so > 1) ? TModular(0) : f(n / 2) / r) << endl;
+	return 0;
+}
