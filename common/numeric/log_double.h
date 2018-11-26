8f06729cd642bcf0ccde50a628bd53808129b347
Loks- <alexeypoyarkov@gmail.com>
2018-11-26 02:32:55 +0000

    Add first solution (TL) for "Assignment Problem" problem.

---
+++ common/numeric/log_double.h
@@ -0,0 +1,45 @@
+#pragma once
+
+#include "../base.h"
+#include <algorithm>
+#include <cmath>
+#include <limits>
+
+class LogDouble
+{
+protected:
+    double value;
+
+public:
+    static double safe_log(double x) { assert(x >= 0.); return (x == 0.) ? -std::numeric_limits<double>::infinity() : log(x); }
+    LogDouble() : value(-std::numeric_limits<double>::infinity()) {}
+    LogDouble(double x) { value = safe_log(x); }
+    
+    double Get() const { return exp(value); }
+    double GetLog() const { return value; }
+    void SetLog(double log_x) { value = log_x; }
+    static LogDouble MakeLog(double log_x) { LogDouble t; t.SetLog(log_x); return t; }
+
+    bool operator<(const LogDouble& r) const { return value < r.value; }
+    bool operator<=(const LogDouble& r) const { return !(r.value < value); }
+    
+    LogDouble& operator+=(const LogDouble& r) { *this = (*this + r); return *this; }
+    LogDouble& operator-=(const LogDouble& r) { *this = (*this - r); return *this; }
+    LogDouble& operator*=(const LogDouble& r) { value += r.value; return *this; }
+    LogDouble& operator/=(const LogDouble& r) { value -= r.value; return *this; }
+
+    LogDouble operator+(const LogDouble& r) const
+    {
+        double v1 = std::max(value, r.value); 
+        double v2 = std::min(value, r.value);
+        return MakeLog(v1 + log(1.0 + exp(v2 - v1)));
+    }
+    LogDouble operator-(const LogDouble& r) const
+    { 
+        if (value == r.value) return LogDouble();
+        assert(r.value < value);
+        return MakeLog(value + log(1.0 - exp(r.value - value)));
+    }
+    LogDouble operator*(const LogDouble& r) const { LogDouble t(*this); t *= r; return t; }
+    LogDouble operator/(const LogDouble& r) const { LogDouble t(*this); t /= r; return t; }
+};
--- hackerrank/practice/mathematics/probability/assignment_problem.cpp
+++ hackerrank/practice/mathematics/probability/assignment_problem.cpp
@@ -1,42 +1,52 @@
 // https://www.hackerrank.com/challenges/assignment
 
-#include "common/numeric/factorial_double.h"
-#include "common/statistics/stat_m2.h"
+#include "common/numeric/log_double.h"
+#include "common/statistics/stat_m1.h"
 #include "common/stl/base.h"
 
 #include <iomanip>
-#include <random>
 #include <unordered_map>
+#include <utility>
 
 int main_assignment_problem()
 {
+    unsigned maxm = 65, maxm2 = maxm * maxm; //251;
+    vector<LogDouble> v(maxm * maxm * maxm); // layers max_length m
+    for (unsigned l = 1; l < maxm; ++l)
+        v[1 * maxm2 + l * maxm + l] = 1.0;
+    for (unsigned m = 1; m + 1 < maxm; ++m)
+    {
+        for (unsigned l = 1; l <= m; ++l)
+        {
+            for (unsigned ml = 1; ml <= m; ++ml)
+            {
+                LogDouble c = v[l * maxm2 + ml * maxm + m];
+                if (c.GetLog() < -0.5) continue; // c == 0
+                for (unsigned k = 1; m + k < maxm; ++k)
+                    v[(l + 1) * maxm2 + max(k, ml) * maxm + (m + k)] += c;
+            }
+        }
+    }
+
 	cout << setprecision(3) << fixed;
-    FactorialDouble f;
-    unordered_map<unsigned, unsigned> mcnt;
-    minstd_rand random_engine;
 	unsigned T, M, N;
 	cin >> T;
 	for (unsigned iT = 0; iT < T; ++iT)
 	{
+        StatM1<LogDouble> s;
         cin >> M >> N;
-        uniform_int_distribution<unsigned> uid(0, N - 1);
-        StatM2<> s;
-        for (unsigned j = 0; (j < 1000) || (s.Variance() > 1e-6 * j); ++j)
+        LogDouble b = 1.0;
+        for (unsigned l = 1; l <= min(M, N); ++l)
         {
-            // cout << j << "\t" << s.Variance() << endl;
-            mcnt.clear();
-            for (unsigned k = 0; k < M; ++k)
-                mcnt[uid(random_engine)] += 1;
-            unsigned best = 1;
-            double wm = 1.0;
-            for (auto p : mcnt)
+            b *= double(N - l + 1) / double(l);
+            for (unsigned ml = 1; ml <= M; ++ml)
             {
-                best = max(best, p.second);
-                wm *= f.Get(p.second);
+                LogDouble w = v[l* maxm2 + ml *maxm + M];
+                if (w.GetLog() < -0.5) continue;
+                s.AddSample(double(ml), w * b);
             }
-            s.AddSample(best, wm);
         }
-        cout << s.Mean() << endl;
+        cout << s.Mean().Get() << endl;
 	}
 	return 0;
 }
