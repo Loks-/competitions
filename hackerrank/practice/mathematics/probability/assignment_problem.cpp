0aa5cb104a773f7b43561c67bbf9af853f216209
Loks- <alexeypoyarkov@gmail.com>
2018-11-26 09:05:16 -0800

    Small fix.

--- hackerrank/practice/mathematics/probability/assignment_problem.cpp
+++ hackerrank/practice/mathematics/probability/assignment_problem.cpp
@@ -16,19 +16,16 @@ int main_assignment_problem()
     vector<LogDouble> v(maxm * maxm * maxm); // layers max_length m
     for (unsigned l = 1; l < maxm; ++l)
         v[1 * maxm2 + l * maxm + l] = 1.0;
-    for (unsigned m = 1; m + 1 < maxm; ++m)
+    for (unsigned m = 1; m < maxm; ++m)
     {
-        unsigned k_end = maxm - m; // min(m + 1, maxm - m);
+        unsigned k_end = min(m + 1, maxm - m);
         for (unsigned l = 1; l <= m; ++l)
         {
             unsigned ml_end = m - l + 2;
             for (unsigned ml = (m - 1) / l + 1; ml < ml_end; ++ml)
             {
-                // if (l > 1 && 2 * ml > m)
-                // {
-                //     // cout << m << "\t" << l << "\t" << ml << "\t" << v[l * maxm2 + ml * maxm + m].Get() << "\t" << f.BinomialCoefficient(m - ml - 1, l - 2) * l << endl;
-                //     v[l * maxm2 + ml * maxm + m] = LogDouble::MakeLog(f.BinomialCoefficientLog(m - ml - 1, l - 2)) * l;
-                // }
+                if (l > 1 && 2 * ml > m)
+                    v[l * maxm2 + ml * maxm + m] = LogDouble::MakeLog(f.BinomialCoefficientLog(m - ml - 1, l - 2)) * l;
                 LogDouble c = v[l * maxm2 + ml * maxm + m];
                 for (unsigned k = 1; k < k_end; ++k)
                     v[(l + 1) * maxm2 + max(k, ml) * maxm + (m + k)] += c;
