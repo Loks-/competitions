8b804580acb2e37cdec88c6705a381b5640f2fe9
Loks- <alexeypoyarkov@gmail.com>
2018-11-26 08:18:05 +0000

    Small update.

--- hackerrank/practice/mathematics/probability/assignment_problem.cpp
+++ hackerrank/practice/mathematics/probability/assignment_problem.cpp
@@ -1,5 +1,6 @@
 // https://www.hackerrank.com/challenges/assignment
 
+#include "common/numeric/factorial_double.h"
 #include "common/numeric/log_double.h"
 #include "common/statistics/stat_m1.h"
 #include "common/stl/base.h"
@@ -10,20 +11,26 @@
 
 int main_assignment_problem()
 {
+    FactorialDouble f;
     unsigned maxm = 251, maxm2 = maxm * maxm; //251;
     vector<LogDouble> v(maxm * maxm * maxm); // layers max_length m
     for (unsigned l = 1; l < maxm; ++l)
         v[1 * maxm2 + l * maxm + l] = 1.0;
     for (unsigned m = 1; m + 1 < maxm; ++m)
     {
+        unsigned k_end = maxm - m; // min(m + 1, maxm - m);
         for (unsigned l = 1; l <= m; ++l)
         {
             unsigned ml_end = m - l + 2;
             for (unsigned ml = (m - 1) / l + 1; ml < ml_end; ++ml)
             {
-                LogDouble c = v[l * maxm2 + ml * maxm + m];                
-                // if (c.GetLog() < -0.5) { cout << m << "\t" << l << "\t" << ml << endl; continue; }// c == 0
-                for (unsigned k = 1; m + k < maxm; ++k)
+                // if (l > 1 && 2 * ml > m)
+                // {
+                //     // cout << m << "\t" << l << "\t" << ml << "\t" << v[l * maxm2 + ml * maxm + m].Get() << "\t" << f.BinomialCoefficient(m - ml - 1, l - 2) * l << endl;
+                //     v[l * maxm2 + ml * maxm + m] = LogDouble::MakeLog(f.BinomialCoefficientLog(m - ml - 1, l - 2)) * l;
+                // }
+                LogDouble c = v[l * maxm2 + ml * maxm + m];
+                for (unsigned k = 1; k < k_end; ++k)
                     v[(l + 1) * maxm2 + max(k, ml) * maxm + (m + k)] += c;
             }
         }
