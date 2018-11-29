6e20d080cb7e3579cbfc0188548528f5afdc62f8
Loks- <alexeypoyarkov@gmail.com>
2018-11-29 09:41:41 -0800

    Add solution and tests for "Tower 3-coloring" problem.

---
+++ hackerrank/practice/mathematics/combinatorics/tower_3_coloring.cpp
@@ -0,0 +1,13 @@
+// https://www.hackerrank.com/challenges/tower-3-coloring
+
+#include "common/modular/arithmetic.h"
+#include "common/stl/base.h"
+
+int main_tower_3_coloring()
+{
+    const uint64_t M = 1000000007;
+    unsigned n;
+    cin >> n;
+    cout << TModularArithmetic_P32U::PowU(3, TModularArithmetic_C32U::PowU(3, n, M - 1), M) << endl;
+	return 0;
+}
