12d6152f9376ddca1e32b70d36b96a33f754cff2
Loks- <alexeypoyarkov@gmail.com>
2018-12-01 21:39:53 +0000

    Speed up solution for "Grid Lines" problem.

--- hackerrank/practice/mathematics/combinatorics/grid_lines.cpp
+++ hackerrank/practice/mathematics/combinatorics/grid_lines.cpp
@@ -12,22 +12,28 @@ int main_grid_lines()
 {
     TFactorial f;
     
-    auto F = [&](unsigned n, unsigned m, unsigned k)
+    auto G = [&](uint64_t n, uint64_t m, uint64_t k, uint64_t dx, uint64_t dy, uint64_t l)
+    {
+        return (f.BinomialCoefficient(l, k - 1) * n * m - 
+            f.BinomialCoefficient(l + 1, k) * (n * dy + m * dx + dx * dy) * (k - 1) +
+            f.BinomialCoefficient(l + 2, k + 1) * dx * dy * k * (k - 1)) * 2;
+    };
+
+    auto F = [&](uint64_t n, uint64_t m, uint64_t k)
     {
         TModular s = f.BinomialCoefficient(m, k) * n + f.BinomialCoefficient(n, k) * m;
-        for (unsigned dx = 1; dx * (k - 1) < n; ++dx)
+        for (uint64_t dx = 1; dx * (k - 1) < n; ++dx)
         {
-            for (unsigned dy = 1; dy * (k - 1) < m; ++dy)
+            for (uint64_t dy = 1; dy * (k - 1) < m; ++dy)
             {
                 if (GCD(dx, dy) != 1) continue;
-                for (unsigned l = k - 1; (dx * l < n) && (dy * l < m); ++l)
-                    s += f.BinomialCoefficient(l - 1, k - 2) * (n - dx * l) * (m - dy * l) * 2;
+                s += G(n, m, k, dx, dy, min((n - 1) / dx, (m - 1) / dy)) - G(n, m, k, dx, dy, k - 2);
             }
         }
         return s;
     };
 
-    unsigned n, m, k;
+    uint64_t n, m, k;
     cin >> n >> m >> k;
     cout << F(n + 1, m + 1, k) - F(n - 1, m - 1, k) << endl;
 	return 0;
