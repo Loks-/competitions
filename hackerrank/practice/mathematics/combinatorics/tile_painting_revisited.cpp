5e051092a90af79769c976600c71ed00fdef3ca3
Loks- <alexeypoyarkov@gmail.com>
2018-12-01 17:37:59 +0000

    Add solution and tests for "Tile Painting: Revisited!" problem.

---
+++ hackerrank/practice/mathematics/combinatorics/tile_painting_revisited.cpp
@@ -0,0 +1,26 @@
+// https://www.hackerrank.com/challenges/tile-painting-revisited
+
+#include "common/modular/static/modular_io.h"
+#include "common/stl/base.h"
+
+using TModular = Modular<>;
+
+int main_tile_painting_revisited()
+{
+    unsigned T;
+    cin >> T;
+    for (unsigned iT = 0; iT < T; ++iT)
+    {
+        uint64_t N;
+        cin >> N;
+        TModular s = N;
+        for (uint64_t l = 1; l * l < N; ++l)
+        {
+            s += (N - l * l);
+            uint64_t k = (N - 1) / l;
+            s += TModular(k - l) * (2 * N - l * (k + l + 1));
+        }
+        cout << s << endl;
+    }
+	return 0;
+}
