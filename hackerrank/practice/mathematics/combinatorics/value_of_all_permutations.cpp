1365f7a623e551ffe37dbb35e1d4ccccb1c856f0
Loks- <alexeypoyarkov@gmail.com>
2018-12-10 02:57:15 +0000

    Finalize solution for "Value of all Permutations" problem.

--- hackerrank/practice/mathematics/combinatorics/value_of_all_permutations.cpp
+++ hackerrank/practice/mathematics/combinatorics/value_of_all_permutations.cpp
@@ -7,33 +7,33 @@
 
 int main_value_of_all_permutations()
 {
-    unsigned p, n, q, l, m;
+    unsigned p, n, q, l = 1, m;
     cin >> p >> n;
     TModularProxy_P32U mp(p);
     PModularFactorial<TModularProxy_P32U> f(p);
     vector<unsigned> v = ReadVector<unsigned>(n);
     sort(v.begin(), v.end());
-    uint64_t lfi = 1, s = 0, sj = 0;
-    vector<uint64_t> va(n);
-    for (unsigned i = 0; i < v.size(); i += l)
+    uint64_t total = f.Get(n);
+    vector<uint64_t> vs(n);
+    vs[0] = v[0];
+    for (unsigned i = 1; i < v.size(); ++i)
     {
-        s = 0;
-        for (l = 1; (i + l < v.size()) && (v[i + l] == v[i]);) ++l;
-        lfi = mp.Mult(lfi, f.GetI(l));
-        for (unsigned j = i; j < i + l; ++j)
-            sj += v[j];
-        sj %= p;
-        for (unsigned j = 1; j <= i + l; ++j)
-            s += mp.Mult(sj, mp.Mult(f.Get(i + l - 1), mp.Mult(f.GetI(i + l - j), f.Get(n - j))));
-        for (unsigned j = i; j < i + l; ++j)
-            va[j] = s % p;
+        vs[i] = vs[i - 1] + v[i];
+        if (v[i] != v[i-1])
+        {
+            total = mp.Mult(total, f.GetI(l));
+            l = 0;
+        }
+        ++l;
     }
+    total = mp.Mult(total, f.GetI(l));
     cin >> q;
     for (unsigned iq = 0; iq < q; ++iq)
     {
         cin >> m;
         auto it = upper_bound(v.begin(), v.end(), m);
-        cout << ((m < v[0]) ? 0ull : mp.Mult(va[it - v.begin() - 1], lfi)) << endl;
+        unsigned l = (it - v.begin()) - 1;
+        cout << ((m < v[0]) ? 0ull : mp.Div(mp.MultSafe(vs[l], total), n - l)) << endl;
     }
 	return 0;
 }
