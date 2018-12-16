5dda698ee5911a5285357235998589b3d4faec2a
Loks- <alexeypoyarkov@gmail.com>
2018-12-16 19:27:41 +0000

    Add solution (TL) and tests for "Parity Party" problem.

---
+++ hackerrank/practice/mathematics/combinatorics/parity_party.cpp
@@ -0,0 +1,33 @@
+// https://www.hackerrank.com/challenges/alien-flowers
+
+#include "common/modular/static/factorial.h"
+#include "common/modular/static/modular_io.h"
+#include "common/stl/base.h"
+
+using TModular = TModular_P32<7340033>;
+using TFactorial = ModularFactorial<TModular>;
+
+// TODO:
+//   FFT solution
+int main_parity_party()
+{
+	TFactorial f;
+	unsigned n, a, b, c, s;
+	cin >> n >> a >> b >> c;
+    s = a + b + c;
+	TModular res = 0, one = 1;
+    for (unsigned i = 0; i <= a + b; ++i)
+    {
+        unsigned k0 = (i <= b) ? 0u : i - b;
+        unsigned k1 = (a <= i) ? a : i;
+        TModular r = 0, m1pk = (k0 & 1) ? -one : one;
+        for (unsigned k = k0; k <= k1; ++k)
+        {
+            r += f.BinomialCoefficient(a, k) * f.BinomialCoefficient(b, i - k) * m1pk;
+            m1pk = -m1pk;
+        }
+        res += r * (TModular(s) - TModular(2 * i)).PowU(n);
+    }
+	cout << res / TModular(2).PowU(a + b) << endl;
+	return 0;
+}
