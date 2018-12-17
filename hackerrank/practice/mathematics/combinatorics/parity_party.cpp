3c2dd329ab17ba30a155c635b22024f50b2a3024
Loks- <alexeypoyarkov@gmail.com>
2018-12-17 16:21:23 +0000

    Use ModularFFT in the solution for "Parity Party" problem.

--- hackerrank/practice/mathematics/combinatorics/parity_party.cpp
+++ hackerrank/practice/mathematics/combinatorics/parity_party.cpp
@@ -1,33 +1,29 @@
 // https://www.hackerrank.com/challenges/parity-party
 
-#include "common/modular/static/factorial.h"
+#include "common/modular/static/fft.h"
 #include "common/modular/static/modular_io.h"
 #include "common/stl/base.h"
 
 using TModular = TModular_P32<7340033>;
-using TFactorial = ModularFactorial<TModular>;
+using TFFT = ModularFFT<TModular>;
 
-// TODO:
-//   FFT solution
 int main_parity_party()
 {
-	TFactorial f;
-	unsigned n, a, b, c, s;
+    TModular one = 1;
+    TFFT fft;
+	unsigned n, a, b, c, s, l;
 	cin >> n >> a >> b >> c;
     s = a + b + c;
-	TModular res = 0, one = 1;
+    for (l = 1; l <= a + b; l *= 2);
+    vector<TModular> va_fft = fft.FFT(l, {one, -one});
+    vector<TModular> vb_fft = fft.FFT(l, {one, one});
+    vector<TModular> vf_fft(l);
+    for (unsigned i = 0; i < l; ++i)
+        vf_fft[i] = va_fft[i].PowU(a) * vb_fft[i].PowU(b);
+    vector<TModular> vf = fft.FFTI(l, vf_fft);
+	TModular res = 0;
     for (unsigned i = 0; i <= a + b; ++i)
-    {
-        unsigned k0 = (i <= b) ? 0u : i - b;
-        unsigned k1 = (a <= i) ? a : i;
-        TModular r = 0, m1pk = (k0 & 1) ? -one : one;
-        for (unsigned k = k0; k <= k1; ++k)
-        {
-            r += f.BinomialCoefficient(a, k) * f.BinomialCoefficient(b, i - k) * m1pk;
-            m1pk = -m1pk;
-        }
-        res += r * (TModular(s) - TModular(2 * i)).PowU(n);
-    }
+        res += vf[i] * (TModular(s) - TModular(2 * i)).PowU(n);
 	cout << res / TModular(2).PowU(a + b) << endl;
 	return 0;
 }
