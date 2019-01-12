e949c9e951a302e1a366d236811a7c6ff90039e4
Loks- <alexeypoyarkov@gmail.com>
2019-01-12 21:33:02 +0000

    Add solution and tests for "A Very Big Sum" problem.

---
+++ hackerrank/practice/algorithms/warmup/a_very_big_sum.cpp
@@ -0,0 +1,14 @@
+// https://www.hackerrank.com/challenges/a-very-big-sum
+
+#include "common/stl/base.h"
+#include "common/vector/read.h"
+
+#include <numeric>
+
+int main_a_very_big_sum() {
+  unsigned n;
+  cin >> n;
+  vector<uint64_t> v = ReadVector<uint64_t>(n);
+  cout << accumulate(v.begin(), v.end(), 0ull) << endl;
+  return 0;
+}
