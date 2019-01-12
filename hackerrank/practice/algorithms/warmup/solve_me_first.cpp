c3dd58128fb5caf321d4ccdb7e29a8644a8146f8
Loks- <alexeypoyarkov@gmail.com>
2019-01-12 21:16:05 +0000

    Add solution and tests for "Solve Me First" problem.

---
+++ hackerrank/practice/algorithms/warmup/solve_me_first.cpp
@@ -0,0 +1,10 @@
+// https://www.hackerrank.com/challenges/solve-me-first
+
+#include "common/stl/base.h"
+
+int main_solve_me_first() {
+  int a, b;
+  cin >> a >> b;
+  cout << a + b << endl;
+  return 0;
+}
