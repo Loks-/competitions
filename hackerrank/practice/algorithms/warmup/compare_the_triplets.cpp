1b60a1e8f2cac3e477964361a5c7df671f1946ca
Loks- <alexeypoyarkov@gmail.com>
2019-01-12 21:28:59 +0000

    Add solution and tests for "Compare the Triplets" problem.

---
+++ hackerrank/practice/algorithms/warmup/compare_the_triplets.cpp
@@ -0,0 +1,19 @@
+// https://www.hackerrank.com/challenges/compare-the-triplets
+
+#include "common/stl/base.h"
+#include "common/vector/read.h"
+
+#include <numeric>
+
+int main_compare_the_triplets() {
+  vector<unsigned> v1 = ReadVector<unsigned>(3), v2 = ReadVector<unsigned>(3);
+  unsigned a = 0, b = 0;
+  for (unsigned i = 0; i < 3; ++i) {
+    if (v1[i] < v2[i])
+      ++b;
+    else if (v1[i] > v2[i])
+      ++a;
+  }
+  cout << a << " " << b << endl;
+  return 0;
+}
