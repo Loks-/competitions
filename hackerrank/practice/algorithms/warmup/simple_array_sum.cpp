9438520b6bb7e9b8d8dacf8bacbadfd85e6b510f
Loks- <alexeypoyarkov@gmail.com>
2019-01-12 21:23:10 +0000

    Add solution and tests for "Simple Array Sum" problem.

---
+++ hackerrank/practice/algorithms/warmup/simple_array_sum.cpp
@@ -0,0 +1,14 @@
+// https://www.hackerrank.com/challenges/simple-array-sum
+
+#include "common/stl/base.h"
+#include "common/vector/read.h"
+
+#include <numeric>
+
+int main_simple_array_sum() {
+  unsigned n;
+  cin >> n;
+  vector<unsigned> v = ReadVector<unsigned>(n);
+  cout << accumulate(v.begin(), v.end(), 0u) << endl;
+  return 0;
+}
