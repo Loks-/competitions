a6f7a636c8cbec20a417bd9e7c2e6ff4a935e0ac
Loks- <alexeypoyarkov@gmail.com>
2019-01-12 22:52:11 +0000

    Add solution and tests for "Mini-Max Sum" problem.

---
+++ hackerrank/practice/algorithms/warmup/mini_max_sum.cpp
@@ -0,0 +1,14 @@
+// https://www.hackerrank.com/challenges/simple-array-sum
+
+#include "common/stl/base.h"
+#include "common/vector/read.h"
+
+#include <numeric>
+
+int main_mini_max_sum() {
+  vector<unsigned> v = ReadVector<unsigned>(5);
+  unsigned s = accumulate(v.begin(), v.end(), 0u);
+  cout << s - *max_element(v.begin(), v.end()) << " "
+       << s - *min_element(v.begin(), v.end()) << endl;
+  return 0;
+}
