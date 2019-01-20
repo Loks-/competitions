8fe0bd4e10774e009b17d219e54fe439b420a32a
Loks- <alexeypoyarkov@gmail.com>
2019-01-20 18:17:22 +0000

    Add solution and tests for "Calculate the Nth term" problem.

---
+++ hackerrank/practice/language/c/functions/calculate_the_nth_term.c
@@ -0,0 +1,24 @@
+// https://www.hackerrank.com/challenges/recursion-in-c
+
+#include <stdio.h>
+
+int find_nth_term(int n, int a, int b, int c) {
+  switch (n) {
+    case 1:
+      return a;
+    case 2:
+      return b;
+    case 3:
+      return c;
+    default:
+      return find_nth_term(n - 1, a, b, c) + find_nth_term(n - 2, a, b, c) +
+             find_nth_term(n - 3, a, b, c);
+  }
+}
+
+int main_calculate_the_nth_term() {
+  int n, a, b, c;
+  scanf("%d %d %d %d", &n, &a, &b, &c);
+  printf("%d\n", find_nth_term(n, a, b, c));
+  return 0;
+}
