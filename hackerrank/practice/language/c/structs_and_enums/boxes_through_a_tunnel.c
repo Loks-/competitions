edf10175a048401eb2e0dcd18d8f71edbdcbb885
Loks- <alexeypoyarkov@gmail.com>
2019-01-21 08:56:47 +0000

    Add static keyword.

--- hackerrank/practice/language/c/structs_and_enums/boxes_through_a_tunnel.c
+++ hackerrank/practice/language/c/structs_and_enums/boxes_through_a_tunnel.c
@@ -10,9 +10,9 @@ struct box {
 
 typedef struct box box;
 
-int get_volume(box b) { return b.length * b.width * b.height; }
+static int get_volume(box b) { return b.length * b.width * b.height; }
 
-int is_lower_than_max_height(box b) { return b.height < MAX_HEIGHT; }
+static int is_lower_than_max_height(box b) { return b.height < MAX_HEIGHT; }
 
 int main_boxes_through_a_tunnel() {
   int n;
