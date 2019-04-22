14cc5513d2f0eeb22988345c7eb53aac0c37e7cb
Loks- <alexeypoyarkov@gmail.com>
2019-04-22 05:45:05 +0000

    Use Heap instead of priority_queue.

--- google/code_jam/2018/ps/senate_evacuation.cpp
+++ google/code_jam/2018/ps/senate_evacuation.cpp
@@ -1,6 +1,5 @@
+#include "common/heap/heap.h"
 #include "common/stl/base.h"
-
-#include <queue>
 
 int main_senate_evacuation() {
   unsigned T;
@@ -8,29 +7,27 @@ int main_senate_evacuation() {
   for (unsigned it = 1; it <= T; ++it) {
     unsigned N, t, s = 0;
     cin >> N;
-    priority_queue<pair<unsigned, char>> pq;
+    HeapMaxOnTop<pair<unsigned, char>> pq;
     for (unsigned i = 0; i < N; ++i) {
       cin >> t;
       s += t;
-      if (t > 0) pq.push(make_pair(t, char(i)));
+      if (t > 0) pq.Add({t, char(i)});
     }
     cout << "Case #" << it << ": ";
     if (s & 1) {
-      auto p = pq.top();
-      pq.pop();
+      auto p = pq.GetTop();
       printf("%c ", p.second + 'A');
       p.first -= 1;
-      if (p.first > 0) pq.push(p);
+      if (p.first > 0) pq.Add(p);
     }
     for (;;) {
       for (unsigned i = 0; i < 2; ++i) {
-        auto p = pq.top();
-        pq.pop();
+        auto p = pq.GetTop();
         printf("%c", p.second + 'A');
         p.first -= 1;
-        if (p.first > 0) pq.push(p);
+        if (p.first > 0) pq.Add(p);
       }
-      if (pq.empty()) break;
+      if (pq.Empty()) break;
       cout << " ";
     }
     cout << endl;
