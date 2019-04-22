489b3f45e761cc7fd649d6f860ca08cb6ff36dc3
Loks- <alexeypoyarkov@gmail.com>
2019-04-22 05:21:05 +0000

    Use heap instead of priority_queue.

--- hackerrank/practice/data_structures/heap/jesse_and_cookies.cpp
+++ hackerrank/practice/data_structures/heap/jesse_and_cookies.cpp
@@ -1,28 +1,24 @@
 // https://www.hackerrank.com/challenges/jesse-and-cookies
 
+#include "common/heap/heap.h"
 #include "common/stl/base.h"
-
-#include <functional>
-#include <queue>
 
 int main_jesse_and_cookies() {
   unsigned N;
   int count = 0;
   uint64_t K, a1, a2;
-  priority_queue<uint64_t, vector<uint64_t>, greater<uint64_t>> q;
+  HeapMinOnTop<uint64_t> q;
   cin >> N >> K;
   for (unsigned i = 0; i < N; ++i) {
     cin >> a1;
-    q.push(a1);
+    q.Add(a1);
   }
-  for (; (q.size() > 1) && (q.top() < K);) {
-    a1 = q.top();
-    q.pop();
-    a2 = q.top();
-    q.pop();
-    q.push(a1 + 2 * a2);
+  for (; (q.Size() > 1) && (q.Top() < K);) {
+    a1 = q.GetTop();
+    a2 = q.GetTop();
+    q.Add(a1 + 2 * a2);
     ++count;
   }
-  cout << (((q.size() > 0) && (q.top() >= K)) ? count : -1) << endl;
+  cout << (((q.Size() > 0) && (q.Top() >= K)) ? count : -1) << endl;
   return 0;
 }
