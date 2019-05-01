f63e2fbd38856fa78abd4dd67b410d16ab0f6cea
Loks- <alexeypoyarkov@gmail.com>
2019-05-01 10:27:42 -0700

    Change default algo for DistanceFromSourcePositiveCost.

--- common/graph/graph_ei/distance_positive_cost.h
+++ common/graph/graph_ei/distance_positive_cost.h
@@ -2,8 +2,9 @@
 
 #include "common/graph/graph_ei.h"
 #include "common/graph/graph_ei/edge_cost_proxy.h"
-#include <queue>
-#include <utility>
+#include "common/heap/ukey_value_map.h"
+
+#include <functional>
 #include <vector>
 
 namespace graph {
@@ -11,25 +12,20 @@ template <class TGraph, class TEdgeCostFunction, class TEdgeCost>
 std::vector<TEdgeCost> DistanceFromSourcePositiveCost(
     const TGraph& graph, const TEdgeCostFunction& f, unsigned source,
     const TEdgeCost& max_cost) {
-  using TPair = std::pair<TEdgeCost, unsigned>;
-  std::vector<TEdgeCost> vd(graph.Size(), max_cost);
-  std::priority_queue<TPair, std::vector<TPair>, std::greater<TPair>> q;
-  for (q.push({TEdgeCost(), source}); !q.empty();) {
-    auto p = q.top();
-    q.pop();
-    unsigned u = p.second;
-    if (vd[u] == max_cost) {
-      vd[u] = p.first;
-      for (auto e : graph.EdgesEI(u)) {
-        unsigned v = e.to;
-        if (vd[v] == max_cost) {
-          TEdgeCost cost = p.first + f(e.info);
-          if (cost < max_cost) q.push({cost, v});
-        }
+  heap::UKeyValueMap<TEdgeCost, std::greater<TEdgeCost>> q(
+      std::vector<TEdgeCost>(graph.Size(), max_cost), true);
+  for (q.Set(source, TEdgeCost()); !q.Empty();) {
+    unsigned u = q.ExtractKey();
+    TEdgeCost ucost = q.Get(u);
+    for (auto e : graph.EdgesEI(u)) {
+      unsigned v = e.to;
+      TEdgeCost cost = ucost + f(e.info);
+      if (cost < q.Get(v)) {
+        q.Set(v, cost);
       }
     }
   }
-  return vd;
+  return q.GetValues();
 }
 }  // namespace graph
 
