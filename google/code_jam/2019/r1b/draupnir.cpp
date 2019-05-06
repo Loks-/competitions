3f74ed4f0c8b094a15298087bd5ce811ad343498
Loks- <alexeypoyarkov@gmail.com>
2019-05-06 09:52:15 -0700

    Add solution and tests for "Draupnir" problem.

---
+++ google/code_jam/2019/r1b/draupnir.cpp
@@ -0,0 +1,25 @@
+#include "common/stl/base.h"
+#include "common/vector/write.h"
+
+int main_draupnir() {
+  unsigned T, W;
+  cin >> T >> W;
+  vector<uint64_t> v(6);
+  uint64_t r;
+  for (unsigned it = 1; it <= T; ++it) {
+    cout << 210 << endl;
+    cin >> r;
+    v[5] = (r >> 35) & 127;
+    v[4] = (r >> 42) & 127;
+    v[3] = (r >> 52) & 127;
+    cout << 42 << endl;
+    cin >> r;
+    r -= (v[3] << 10) + (v[4] << 8) + (v[5] << 7);
+    v[2] = (r >> 14) & 127;
+    v[1] = (r >> 21) & 127;
+    v[0] = (r >> 42) & 127;
+    WriteVector(v);
+    cin >> r;
+  }
+  return 0;
+}
