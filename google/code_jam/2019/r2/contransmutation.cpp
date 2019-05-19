cf3b912808bcb4c423baa100929269f3d253130d
Loks- <alexeypoyarkov@gmail.com>
2019-05-19 13:48:58 -0700

    Add solution and tests for "Contransmutation" problem.

--- google/code_jam/2019/r2/contransmutation.cpp
+++ google/code_jam/2019/r2/contransmutation.cpp
@@ -1,4 +1,5 @@
 #include "common/graph/graph.h"
+#include "common/graph/graph/distance.h"
 #include "common/graph/graph/distance_to_sink.h"
 #include "common/modular/static/modular.h"
 #include "common/modular/static/modular_io.h"
@@ -15,7 +16,7 @@ int main_contransmutation() {
   for (unsigned it = 1; it <= T; ++it) {
     unsigned n;
     cin >> n;
-    DirectedGraph g(n);
+    DirectedGraph g(n + 1);
     for (unsigned i = 0; i < n; ++i) {
       unsigned r1, r2;
       cin >> r1 >> r2;
@@ -23,45 +24,57 @@ int main_contransmutation() {
       g.AddEdge(i, r2 - 1);
     }
     vector<TModular> vc = ReadVector<TModular>(n);
-    auto vd = DistanceToSink(g, 0);
-    vector<unsigned> edges(n);
+    for (unsigned i = 0; i < n; ++i) {
+      if (vc[i].Get()) g.AddEdge(n, i);
+    }
+    auto vd1 = DistanceFromSource(g, n);
+    auto vd2 = DistanceToSink(g, 0);
+    vector<bool> valid(n + 1, false);
+    for (unsigned i = 0; i < n; ++i)
+      valid[i] = (vd1[i] != -1u) && (vd2[i] != -1u);
+    if (!valid[0]) {
+      cout << "Case #" << it << ": " << 0 << endl;
+      continue;
+    }
+    vector<unsigned> edges_in(n, 0);
     queue<unsigned> q;
     for (unsigned i = 0; i < n; ++i) {
-      edges[i] = g.InvertedEdges(i).size();
-      if (edges[i] == 0) q.push(i);
+      if (!valid[i]) continue;
+      for (unsigned j : g.InvertedEdges(i)) {
+        if (valid[j]) ++edges_in[i];
+      }
+      if (!edges_in[i]) q.push(i);
     }
     for (; !q.empty(); q.pop()) {
-      unsigned j = q.front();
-      for (unsigned l = 0; l < g.Edges(j).size(); ++l) {
-        unsigned k = g.Edges(j)[l];
-        vc[k] += vc[j];
-        if (--edges[k] == 0) q.push(k);
+      unsigned i = q.front();
+      valid[i] = false;
+      if (i == 0) break;
+      for (unsigned j : g.Edges(i)) {
+        if (!valid[j]) continue;
+        vc[j] += vc[i];
+        if (--edges_in[j] == 0) q.push(j);
       }
     }
-    // TModular last_value = 0;
-    // unsigned last_change = 0;
-    // bool unbounded = false;
-    // for (unsigned i = 0; i < last_change + n + 1; ++i) {
-    //   for (unsigned j = 1; j < n; ++j) {
-    //     TModular k = vc[j];
-    //     vc[g.Edges(j)[0]] += vc[j];
-    //     vc[g.Edges(j)[1]] += vc[j];
-    //     vc[j] -= k;
-    //   }
-    //   if (vc[0] != last_value) {
-    //     last_change = i;
-    //     last_value = vc[0];
-    //   }
-    //   if (i > n * (n + 1) + 100) {
-    //     unbounded = true;
-    //     break;
-    //   }
-    // }
-    // if (unbounded)
-    //   cout << "Case #" << it << ": "
-    //        << "UNBOUNDED" << endl;
-    // else
-    //   cout << "Case #" << it << ": " << vc[0].Get() << endl;
+    if (!valid[0]) {
+      cout << "Case #" << it << ": " << vc[0].Get() << endl;
+      continue;
+    }
+    bool unbounded = false;
+    TModular s = 0;
+    for (unsigned i = 0; i < n; ++i) {
+      if (!valid[i]) continue;
+      s += vc[i];
+      unsigned e = 0;
+      for (unsigned j : g.Edges(i)) {
+        if (valid[j]) ++e;
+      }
+      if (e > 1) unbounded = true;
+    }
+    if (unbounded)
+      cout << "Case #" << it << ": "
+           << "UNBOUNDED" << endl;
+    else
+      cout << "Case #" << it << ": " << s << endl;
   }
   return 0;
 }
