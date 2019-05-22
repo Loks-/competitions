66ac0e344936a811346dad01e1af0e52ec85fab4
Loks- <alexeypoyarkov@gmail.com>
2019-05-21 22:42:30 -0700

    Simplify code.

--- common/numeric/utils/smallest_denominator_in_interval.h
+++ common/numeric/utils/smallest_denominator_in_interval.h
@@ -3,6 +3,7 @@
 #include "common/base.h"
 #include "common/numeric/continued_fraction/continued_fraction.h"
 #include "common/numeric/fraction.h"
+#include <algorithm>
 #include <vector>
 
 inline TIFraction SmallestDenominatorInInterval(const TIFraction& l,
@@ -10,35 +11,30 @@ inline TIFraction SmallestDenominatorInInterval(const TIFraction& l,
   assert(l < r);
   auto vl = ContinuedFraction(l).GetVector(),
        vr = ContinuedFraction(r).GetVector();
+  unsigned i = 0, i_end = std::min(vl.size(), vr.size());
   std::vector<int64_t> vm;
-  for (unsigned i = 0;; ++i) {
-    if (i >= vl.size()) {
-      vm.push_back(vr[i] + 1);
-      break;
-    } else if (i >= vr.size()) {
-      vm.push_back(vl[i] + 1);
-      break;
-    } else if (vl[i] != vr[i]) {
-      if (vl[i] > vr[i]) vl.swap(vr);
-      if ((vr.size() == i + 1) && (vr[i] == vl[i] + 1)) {
-        vm.push_back(vl[i]);
-        if (vl.size() == i + 1) {
-          vm.push_back(2);
-        } else if (vl[i + 1] == 1) {
-          vm.push_back(1);
-          vm.push_back(vl[i + 2] + 1);
-        } else if ((vl.size() == i + 2) && (vl[i + 1] == 2)) {
-          vm.push_back(1);
-          vm.push_back(2);
-        } else {
-          vm.push_back(2);
-        }
-      } else {
-        vm.push_back(vl[i] + 1);
-      }
-      break;
-    } else {  // vl[i] == vr[i]
+  for (; (i < i_end) && (vl[i] == vr[i]); ++i) vm.push_back(vl[i]);
+  if (i >= vl.size()) {
+    vm.push_back(vr[i] + 1);
+  } else if (i >= vr.size()) {
+    vm.push_back(vl[i] + 1);
+  } else {
+    if (vl[i] > vr[i]) vl.swap(vr);
+    if ((vr.size() == i + 1) && (vr[i] == vl[i] + 1)) {
       vm.push_back(vl[i]);
+      if (vl.size() == i + 1) {
+        vm.push_back(2);
+      } else if (vl[i + 1] == 1) {
+        vm.push_back(1);
+        vm.push_back(vl[i + 2] + 1);
+      } else if ((vl.size() == i + 2) && (vl[i + 1] == 2)) {
+        vm.push_back(1);
+        vm.push_back(2);
+      } else {
+        vm.push_back(2);
+      }
+    } else {
+      vm.push_back(vl[i] + 1);
     }
   }
   return ContinuedFraction(vm).ToFraction();
