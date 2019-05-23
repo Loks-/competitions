fdb6fb995fc7a4e0e6fcc61f85493931351363e7
Loks- <alexeypoyarkov@gmail.com>
2019-05-22 22:48:36 -0700

    Add pairing heap to TestGraphEIDistancePositiveCost tester.

--- tester/tester_graph_ei_distance_positive_cost.cpp
+++ tester/tester_graph_ei_distance_positive_cost.cpp
@@ -7,10 +7,12 @@
 #include "common/heap/dheap_ukey_pos_map.h"
 #include "common/heap/dheap_ukey_value_map.h"
 #include "common/heap/fibonacci_ukey_value_map.h"
+#include "common/heap/pairing_ukey_value_map.h"
 #include "common/timer.h"
 
 #include "tester/graph_ei_distance_positive_cost.h"
 
+#include <functional>
 #include <iostream>
 #include <string>
 #include <unordered_set>
@@ -26,6 +28,9 @@ template <class TData>
 using TDHeap8 = heap::DHeap<8u, TData>;
 template <class TData>
 using TDHeap16 = heap::DHeap<16u, TData>;
+template <bool multipass, bool auxiliary>
+using TPairing = heap::PairingUKeyValueMap<uint64_t, std::less<uint64_t>,
+                                           multipass, auxiliary>;
 
 TesterGraphEIDistancePositiveCost::TesterGraphEIDistancePositiveCost(
     unsigned graph_size, unsigned edges_per_node)
@@ -100,6 +105,10 @@ bool TesterGraphEIDistancePositiveCost::TestAll() {
   hs.insert(TestKVM<heap::DHeapUKeyValueMap<16, uint64_t>>("DM16"));
   hs.insert(TestKVM<heap::BinomialUKeyValueMap<uint64_t>>("BNML"));
   hs.insert(TestKVM<heap::FibonacciUKeyValueMap<uint64_t>>("FBNC"));
+  hs.insert(TestKVM<TPairing<0, 0>>("PR00"));
+  hs.insert(TestKVM<TPairing<1, 0>>("PR01"));
+  hs.insert(TestKVM<TPairing<0, 1>>("PR10"));
+  hs.insert(TestKVM<TPairing<1, 1>>("PR11"));
   return hs.size() == 1;
 }
 
