920da71e638f6065c282392b7d5ded84544c2f3c
Loks- <alexeypoyarkov@gmail.com>
2019-05-24 20:53:23 -0700

    Add solution for "Floyd : City of Blinding Lights" using Floyd–Warshall algorithm.

---
+++ common/graph/graph_ei/distance_all_pairs.h
@@ -0,0 +1,40 @@
+#pragma once
+
+#include "common/graph/graph_ei.h"
+#include "common/graph/graph_ei/edge_cost_proxy.h"
+#include <algorithm>
+#include <vector>
+
+namespace graph {
+// Based on Floyd–Warshall algorithm.
+template <class TGraph, class TEdgeCostFunction, class TEdgeCost>
+std::vector<std::vector<TEdgeCost>> DistanceAllPairs(
+    const TGraph& graph, const TEdgeCostFunction& f,
+    const TEdgeCost& max_cost) {
+  unsigned N = graph.Size();
+  std::vector<std::vector<TEdgeCost>> vd(N,
+                                         std::vector<TEdgeCost>(N, max_cost));
+  for (unsigned u = 0; u < N; ++u) {
+    for (auto e : graph.EdgesEI(u))
+      vd[u][e.to] = std::min(vd[u][e.to], f(e.info));
+    vd[u][u] = TEdgeCost();
+  }
+  for (unsigned k = 0; k < N; ++k) {
+    for (unsigned i = 0; i < N; ++i) {
+      if (!(vd[i][k] < max_cost)) continue;
+      for (unsigned j = 0; j < N; ++j) {
+        if ((vd[k][j] < max_cost) && (vd[i][k] + vd[k][j] < vd[i][j]))
+          vd[i][j] = vd[i][k] + vd[k][j];
+      }
+    }
+  }
+  return vd;
+}
+}  // namespace graph
+
+template <class TEdgeInfo, bool directed_edges>
+std::vector<std::vector<TEdgeInfo>> DistanceAllPairs(
+    const graph::GraphEI<TEdgeInfo, directed_edges>& g, TEdgeInfo max_cost) {
+  return graph::DistanceAllPairs(g, graph::EdgeCostProxy<TEdgeInfo>(),
+                                 max_cost);
+}
---
+++ hackerrank/practice/algorithms/graph/floyd_city_of_blinding_lights__floyd.cpp
@@ -0,0 +1,30 @@
+// https://www.hackerrank.com/challenges/floyd-city-of-blinding-lights
+
+#include "common/graph/graph_ei.h"
+#include "common/graph/graph_ei/distance_all_pairs.h"
+#include "common/stl/base.h"
+
+int main_floyd_city_of_blinding_lights__floyd() {
+  unsigned N, M, Q, j, k, r;
+  cin >> N >> M;
+  DirectedGraphEI<unsigned> g(N);
+  // This problem contains edge duplicates
+  // g.ReadEdges(M);
+  vector<vector<unsigned>> vt(N, vector<unsigned>(N, 0));
+  for (unsigned i = 0; i < M; ++i) {
+    cin >> j >> k >> r;
+    vt[j - 1][k - 1] = r;
+  }
+  for (unsigned i = 0; i < N; ++i) {
+    for (unsigned j = 0; j < N; ++j) {
+      if (vt[i][j]) g.AddEdge(i, j, vt[i][j]);
+    }
+  }
+  vector<vector<unsigned>> vd = DistanceAllPairs(g, -1u);
+  cin >> Q;
+  for (unsigned i = 0; i < Q; ++i) {
+    cin >> j >> k;
+    cout << int(vd[j - 1][k - 1]) << endl;
+  }
+  return 0;
+}
