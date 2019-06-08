d2687edb38ac76ca464e9ed545e41a4804615437
Loks- <alexeypoyarkov@gmail.com>
2019-06-07 18:14:00 -0700

    Use std::function instead of real recursion function.

--- common/factorization/utils/divisors.h
+++ common/factorization/utils/divisors.h
@@ -1,25 +1,21 @@
 #pragma once
 
 #include "common/factorization/base.h"
-
-namespace factorization {
-namespace hidden {
-inline void GetDivisorsI(const TFactorization& factorization, unsigned fpi,
-                         uint64_t current, std::vector<uint64_t>& output) {
-  if (fpi < factorization.size()) {
-    for (unsigned i = 0; i <= factorization[fpi].power; ++i) {
-      GetDivisorsI(factorization, fpi + 1, current, output);
-      current *= factorization[fpi].prime;
-    }
-  } else {
-    output.push_back(current);
-  }
-}
-}  // namespace hidden
-}  // namespace factorization
+#include <functional>
 
 inline std::vector<uint64_t> GetDivisors(const TFactorization& factorization) {
   std::vector<uint64_t> v;
-  factorization::hidden::GetDivisorsI(factorization, 0, 1, v);
+  std::function<void(unsigned, uint64_t)> AddR = [&](unsigned fpi,
+                                                     uint64_t current) -> void {
+    if (fpi < factorization.size()) {
+      for (unsigned i = 0; i <= factorization[fpi].power; ++i) {
+        AddR(fpi + 1, current);
+        current *= factorization[fpi].prime;
+      }
+    } else {
+      v.push_back(current);
+    }
+  };
+  AddR(0, 1);
   return v;
 }
