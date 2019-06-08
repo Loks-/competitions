8a7e7dbf468f2b46be84924cff42e277c4d8a8c4
Loks- <alexeypoyarkov@gmail.com>
2019-06-08 12:32:56 -0700

    Fix solution.

--- google/code_jam/2019/r3/zilionim.cpp
+++ google/code_jam/2019/r3/zilionim.cpp
@@ -7,7 +7,8 @@ int main_zilionim() {
   UnsignedSet us(100);
   vector<pair<uint64_t, unsigned>> vp;
   vp.push_back({0, 0});
-  for (vp.push_back({B - 1, 0}); vp.back().first < N;) {
+  vp.push_back({B - 1, 0});
+  for (; vp.back().first < N;) {
     uint64_t K = vp.back().first + 1, KE = K + B;
     unsigned i = 0, j = vp.size() - 1;
     for (; vp[i].first + B <= K; ++i) {
@@ -15,19 +16,18 @@ int main_zilionim() {
       for (; (j > 0) && (vp[j - 1].first >= r);) --j;
       KE = min(KE, vp[i].first + vp[j].first + B);
       us.Insert(vp[i].second ^ vp[j].second);
-      if (vp[i].first + B == K) break;
-      --r;
-      for (; (j > 0) && (vp[j - 1].first >= r);) --j;
-      KE = min(KE, vp[i + 1].first + vp[j].first + B);
-      us.Insert(vp[i + 1].second ^ vp[j].second);
     }
     for (i = 0; us.HasKey(i);) ++i;
     us.Clear();
-    if (vp.back().second == i)
+    // cerr << K << "\t" << KE << "\t" << i << endl;
+    if (vp.back().second == i) {
       vp.back().first = KE;
-    else
+    } else {
+      vp.push_back({K, i});
       vp.push_back({KE, i});
+    }
   }
+  cerr << vp.size() << endl;
 
   unsigned T, W;
   cin >> T >> W;
@@ -36,7 +36,8 @@ int main_zilionim() {
     vector<pair<uint64_t, uint64_t>> v;
     v.push_back({1, N + 1});
     int64_t M;
-    for (cin >> M; M > 0; cin >> M) {
+
+    auto Move = [&]() {
       bool found = false;
       for (unsigned i = 0; i < v.size(); ++i) {
         if ((v[i].first <= M) && (v[i].second >= M + B)) {
@@ -47,53 +48,42 @@ int main_zilionim() {
         }
       }
       assert(found);
-      unsigned e = 0;
-      for (auto p : v) {
-        auto it =
-            lower_bound(vp.begin(), vp.end(),
-                        make_pair<uint64_t, unsigned>(p.second - p.first, 0));
-        e ^= it->second;
-      }
+    };
+    auto Eval1 = [&](uint64_t l) {
+      return lower_bound(vp.begin(), vp.end(), make_pair(l, 0u))->second;
+    };
+    auto Eval = [&] {
+      unsigned k = 0;
+      for (auto p : v) k ^= Eval1(p.second - p.first);
+      return k;
+    };
+
+    for (cin >> M; M > 0; cin >> M) {
+      Move();
+      unsigned e = Eval();
       M = 0;
       if (e) {
         for (auto p : v) {
-          auto it =
-              lower_bound(vp.begin(), vp.end(),
-                          make_pair<uint64_t, unsigned>(p.second - p.first, 0));
-          if ((it->second ^ e) < it->second) {
-            unsigned ite = it->second ^ e;
-            uint64_t K = p.second - p.first;
+          unsigned e1 = Eval1(p.second - p.first);
+          if ((e1 ^ e) < e1) {
+            unsigned er = e1 ^ e;
+            uint64_t l = p.second - p.first;
             unsigned i = 0, j = vp.size() - 1;
-            found = false;
-            for (; vp[i].first + B <= K; ++i) {
-              uint64_t r = K - vp[i].first - B;
-              for (; (j > 0) && (vp[j - 1].first >= r);) --j;
-              if (vp[i].second ^ vp[j].second == ite) {
+            bool found = false;
+            for (; vp[i].first + B <= l; ++i) {
+              if ((vp[i].second ^ Eval1(l - vp[i].first - B)) == er) {
                 found = true;
                 M = p.first + vp[i].first;
                 break;
               }
-              if (vp[i].first + B == K) break;
-              --r;
-              for (; (j > 0) && (vp[j - 1].first >= r);) --j;
-              if (vp[i + 1].second ^ vp[j].second == ite) {
-                found = true;
-                M = p.first + vp[i].first + 1;
-                break;
-              }
             }
-            // if (!found) {
-            cerr << K << "\t" << it->second << "\t" << e << "\t" << ite << endl;
-            // }
             assert(found);
-            if (found) break;
+            break;
           }
         }
         win_move_found = true;
-      }
-      // assert(!win_move_found || e);
-      assert((e == 0) || (M > 0));
-      if (M == 0) {
+      } else {
+        assert(!win_move_found);
         // random_shuffle(v.begin(), v.end());
         for (auto p : v) {
           if (p.second - p.first >= B) {
@@ -103,24 +93,8 @@ int main_zilionim() {
         }
       }
       cout << M << endl;
-      found = false;
-      for (unsigned i = 0; i < v.size(); ++i) {
-        if ((v[i].first <= M) && (v[i].second >= M + B)) {
-          found = true;
-          v.push_back({M + B, v[i].second});
-          v[i].second = M;
-          break;
-        }
-      }
-      assert(found);
-      unsigned enew = 0;
-      for (auto p : v) {
-        auto it =
-            lower_bound(vp.begin(), vp.end(),
-                        make_pair<uint64_t, unsigned>(p.second - p.first, 0));
-        enew ^= it->second;
-      }
-      cerr << "My move " << e << " -> " << enew << endl;
+      Move();
+      cerr << "My move " << e << " -> " << Eval() << endl;
     }
   }
   return 0;
