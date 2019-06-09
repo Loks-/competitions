da7354d8f11adc415cb78cfabb5bdf92945a7dda
Loks- <alexeypoyarkov@gmail.com>
2019-06-09 10:12:28 -0700

    Add solution for "Sherlock and Planes" using geometry classes.

---
+++ hackerrank/practice/mathematics/geometry/sherlock_and_planes.cpp
@@ -0,0 +1,17 @@
+// https://www.hackerrank.com/challenges/sherlock-and-planes
+
+#include "common/geometry/d3/point_io.h"
+#include "common/geometry/d3/utils/plane_from_points.h"
+#include "common/stl/base.h"
+#include "common/vector/read.h"
+
+int main_sherlock_and_planes() {
+  unsigned T;
+  cin >> T;
+  for (unsigned it = 0; it < T; ++it) {
+    auto v = ReadVector<I3Point>(4);
+    auto p = PlaneFromPoints(v[0], v[1], v[2]);
+    cout << ((p(v[3]) == 0) ? "YES" : "NO") << endl;
+  }
+  return 0;
+}
