79964da78660d5ea97a8dd330a4a1f9244130885
Loks- <alexeypoyarkov@gmail.com>
2019-06-30 17:20:52 -0700

    Add MapCore class.

---
+++ icfppc/2019/src/base/map_core.cpp
@@ -0,0 +1,24 @@
+#include "base/map_core.h"
+
+#include "base/point.h"
+#include <cassert>
+
+namespace base {
+void MapCore::InitSize(int _xsize, int _ysize) {
+  xsize = _xsize;
+  ysize = _ysize;
+}
+
+unsigned MapCore::Index(const Point& p) const {
+  assert(Inside(p));
+  return p.x * ysize + p.y;
+}
+
+Point MapCore::GetPoint(unsigned index) const {
+  return {int(index) / ysize, int(index) % ysize};
+}
+
+bool MapCore::Inside(const Point& p) const {
+  return (0 <= p.x) && (p.x < xsize) && (0 <= p.y) && (p.y < ysize);
+}
+}  // namespace base
---
+++ icfppc/2019/src/base/map_core.h
@@ -0,0 +1,19 @@
+#pragma once
+
+#include "base/point.h"
+
+namespace base {
+class MapCore {
+ protected:
+  int xsize;
+  int ysize;
+
+ protected:
+  void InitSize(int _xsize, int _ysize);
+
+ public:
+  unsigned Index(const Point& p) const;
+  Point GetPoint(unsigned index) const;
+  bool Inside(const Point& p) const;
+};
+}  // namespace base
