7c9aac95209fd6a7f392ca4d7f0926d19054fbc3
Loks- <alexeypoyarkov@gmail.com>
2019-07-09 09:28:19 -0700

    Make Single solver thread safe.

--- icfppc/2019/src/solvers/single.h
+++ icfppc/2019/src/solvers/single.h
@@ -1,19 +1,29 @@
 #pragma once
 
-#include "solvers/worker/base.h"
+#include "base/encode.h"
+#include "base/ext/world.h"
 #include "common/solvers/base.h"
+#include "common/solvers/psolver.h"
 #include <string>
 
 namespace solvers {
+template <class TWorker>
 class Single : public Base {
  protected:
-  worker::Base& worker;
+  TWorker worker;
 
  public:
-  Single(worker::Base& _worker);
-  PSolver Clone() const override;
-  std::string Name() const override;
+  Single() {}
+  Single(TWorker& _worker) { worker = _worker; }
+  PSolver Clone() const override { return std::make_shared<Single>(*this); }
+  std::string Name() const override { return "single_" + worker.Name(); }
 
-  std::string Solve(const std::string& problem) override;
+  std::string Solve(const std::string& problem) override {
+    base::ext::World world;
+    world.Init(problem);
+    auto alist = worker.Solve(world);
+    return base::Encode(alist);
+  }
+  // bool SkipCacheRead() const override { return true; }
 };
 }  // namespace solvers
