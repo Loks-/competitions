d50bfae1198ba7c7ddf3437df2c66daa434f2ecc
Loks- <alexeypoyarkov@gmail.com>
2019-12-06 10:49:10 -0800

    Fix overflow.

--- tester/prime_generation.cpp
+++ tester/prime_generation.cpp
@@ -39,13 +39,17 @@ std::vector<uint64_t> GetPrimes_PrimesList(uint64_t maxn) {
 std::vector<uint64_t> GetPrimes_EratosthenesBit(uint64_t maxn) {
   std::vector<uint64_t> vp;
   std::vector<bool> vb(maxn + 1, true);
-  for (uint64_t i = 2; i <= maxn; ++i) {
+  uint64_t sqrtn = USqrt(maxn), i = 2;
+  for (; i <= sqrtn; ++i) {
     if (vb[i]) {
       vp.push_back(i);
       for (uint64_t j = i * i; j <= maxn; j += i) {
         vb[j] = false;
       }
     }
+  }
+  for (; i <= maxn; ++i) {
+    if (vb[i]) vp.push_back(i);
   }
   return vp;
 }
@@ -82,13 +86,17 @@ std::vector<uint64_t> GetPrimes_EratosthenesOdd(uint64_t maxn) {
   if (maxn < 2) return {};
   std::vector<uint64_t> vp{2};
   std::vector<bool> vb(maxn + 1, true);
-  for (uint64_t i = 3; i <= maxn; i += 2) {
+  uint64_t sqrtn = USqrt(maxn), i = 3;
+  for (; i <= sqrtn; i += 2) {
     if (vb[i]) {
       vp.push_back(i);
       for (uint64_t j = i * i; j <= maxn; j += 2 * i) {
         vb[j] = false;
       }
     }
+  }
+  for (; i <= maxn; i += 2) {
+    if (vb[i]) vp.push_back(i);
   }
   return vp;
 }
@@ -98,7 +106,8 @@ std::vector<uint64_t> GetPrimes_EratosthenesOddMemoryReduced(uint64_t maxn) {
   std::vector<uint64_t> vp{2};
   uint64_t hn = (maxn - 1) / 2;
   std::vector<bool> vb(hn + 1, true);
-  for (uint64_t hi = 1; hi <= hn; ++hi) {
+  uint64_t sqrtn = USqrt(maxn), hs = (sqrtn - 1) / 2, hi = 1;
+  for (; hi <= hs; ++hi) {
     if (vb[hi]) {
       uint64_t i = 2 * hi + 1;
       vp.push_back(i);
@@ -106,6 +115,9 @@ std::vector<uint64_t> GetPrimes_EratosthenesOddMemoryReduced(uint64_t maxn) {
         vb[hj] = false;
       }
     }
+  }
+  for (; hi <= hn; ++hi) {
+    if (vb[hi]) vp.push_back(2 * hi + 1);
   }
   return vp;
 }
