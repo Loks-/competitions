aacd75c75fcf1c05d72ead6ae5839c9f6dc68ea2
Loks- <alexeypoyarkov@gmail.com>
2019-12-11 10:11:17 -0800

    Update solution for problem 668.

---
+++ common/factorization/primes_range.h
@@ -0,0 +1,32 @@
+#pragma once
+
+#include "common/base.h"
+#include "common/factorization/primes_generator.h"
+
+namespace factorization {
+class PrimesRange {
+ public:
+  class Iterator {
+   protected:
+    uint64_t maxn;
+    PrimesGenerator* p;
+
+   public:
+    Iterator(uint64_t _maxn) : maxn(_maxn), p(nullptr) {}
+    Iterator(PrimesGenerator* _p) : maxn(0), p(_p) {}
+
+    uint64_t operator*() const { return p->Get(); }
+    void operator++() { p->Next(); }
+    bool operator!=(const Iterator& e) const { return p->Get() < e.maxn; }
+  };
+
+ protected:
+  uint64_t maxn;
+  PrimesGenerator pg;
+
+ public:
+  PrimesRange(uint64_t _maxn) : maxn(_maxn) {}
+  Iterator begin() { return Iterator(&pg); }
+  Iterator end() { return Iterator(maxn); }
+};
+}  // namespace factorization
--- project_euler/problems/668.cpp
+++ project_euler/problems/668.cpp
@@ -1,31 +1,9 @@
-#include "common/factorization/primality_test.h"
+#include "common/factorization/primes_range.h"
 #include "common/stl/base.h"
-#include "common/thread_pool.h"
-
-#include <numeric>
 
 int main_668() {
-  uint64_t n = 10000000000;
-  const unsigned threads = 8;
-  vector<uint64_t> vs(threads);
-  {
-    ThreadPool tp(threads);
-    for (unsigned it = 0; it < threads; ++it) {
-      auto task = std::make_shared<std::packaged_task<void()>>([&, it]() {
-        uint64_t begin = ((it * n) / threads) | 1;
-        uint64_t end = ((it + 1) * n) / threads;
-        for (uint64_t i = begin; i < end; i += 2) {
-          if (IsPrime(i)) {
-            uint64_t ndi = n / i;
-            vs[it] += std::min(ndi, i);
-          }
-        }
-      });
-      tp.enqueueTask(std::move(task));
-    }
-  }
-  uint64_t s = 2;
-  s = accumulate(vs.begin(), vs.end(), s);
-  std::cout << n - s << std::endl;
+  uint64_t n = 10000000000, s = n;
+  for (uint64_t p : factorization::PrimesRange(n)) s -= std::min(p, n / p);
+  std::cout << s << std::endl;
   return 0;
 }
