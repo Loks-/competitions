5530f03dcd8ebc840b303bf65eda6d45603c7e33
Loks- <alexeypoyarkov@gmail.com>
2019-12-16 10:03:10 -0800

    Add special code for partition function.

--- common/generating_function/functions/partition.h
+++ common/generating_function/functions/partition.h
@@ -1,8 +1,7 @@
 #pragma once
 
-#include "common/generating_function/functions/geometric.h"
 #include "common/generating_function/generating_function.h"
-#include "common/generating_function/operators/partition.h"
+#include "common/numeric/utils/usqrt.h"
 
 // https://oeis.org/A000041
 // https://en.wikipedia.org/wiki/Partition_function_(number_theory)
@@ -10,8 +9,39 @@
 namespace gf {
 namespace functions {
 template <class TValue>
+class Partition : public GeneratingFunction<TValue> {
+ protected:
+  std::vector<TValue> va;
+
+ public:
+  Partition() : va({1}) {}
+
+  void Adjust(uint64_t n) override {
+    for (uint64_t i = va.size(); i <= n; ++i) {
+      TValue s = TValue(0);
+      uint64_t k1, k2;
+      TValue n1 = -TValue(1), m = n1;
+      for (uint64_t k = 1;; ++k) {
+        k1 = k * (3 * k - 1) / 2;
+        k2 = k * (3 * k + 1) / 2;
+        m *= n1;
+        if (k2 > i) break;
+        s += m * (va[i - k2] + va[i - k1]);
+      }
+      if (k1 <= i) s += m * va[i - k1];
+      va.push_back(s);
+    }
+  }
+
+  TValue Get(uint64_t n) override {
+    Adjust(n);
+    return va[n];
+  }
+};
+
+template <class TValue>
 static PGeneratingFunction<TValue> CreatePartition() {
-  return gf::operators::CreatePartition(CreatePGeometricOne<TValue>());
+  return std::make_shared<Partition<TValue>>();
 }
 }  // namespace functions
 }  // namespace gf
--- tester/tester_generating_function.cpp
+++ tester/tester_generating_function.cpp
@@ -1,7 +1,19 @@
+#include "common/generating_function/functions/geometric.h"
 #include "common/generating_function/functions/partition.h"
+#include "common/generating_function/operators/partition.h"
 #include "common/modular.h"
 
-bool TestGenetatingFunctionPartition() {
+bool TestGenetatingFunctionOperatorsPartition() {
+  uint64_t n = 100, expected = 190569292;
+  auto f_uint64_t = gf::operators::CreatePartition(
+      gf::functions::CreatePGeometricOne<uint64_t>());
+  auto f_modular = gf::operators::CreatePartition(
+      gf::functions::CreatePGeometricOne<TModularD>());
+  return (f_uint64_t->Get(n) == expected) &&
+         (f_modular->Get(n) == TModularD(expected));
+}
+
+bool TestGenetatingFunctionFunctionsPartition() {
   uint64_t n = 100, expected = 190569292;
   auto f_uint64_t = gf::functions::CreatePartition<uint64_t>();
   auto f_modular = gf::functions::CreatePartition<TModularD>();
@@ -9,4 +21,9 @@ bool TestGenetatingFunctionPartition() {
          (f_modular->Get(n) == TModularD(expected));
 }
 
-bool TestGeneratingFunction() { return TestGenetatingFunctionPartition(); }
+bool TestGeneratingFunction() {
+  bool all_ok = true;
+  all_ok = all_ok && TestGenetatingFunctionOperatorsPartition();
+  all_ok = all_ok && TestGenetatingFunctionFunctionsPartition();
+  return all_ok;
+}
