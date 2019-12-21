695110b4b2352fa6775d0e128fb78c8ab9c781f2
Loks- <alexeypoyarkov@gmail.com>
2019-12-20 22:00:45 -0800

    Add solution for problem 684.

---
+++ project_euler/problems/684.cpp
@@ -0,0 +1,20 @@
+#include "common/stl/base.h"
+#include "common/modular_io.h"
+#include "common/numeric/fibonacci.h"
+
+int main_684() {
+  unsigned N = 90;
+
+  auto S = [](uint64_t n) {
+    uint64_t i = (n % 9) + 1;
+    return TModularD(10).PowU(n / 9) * ((i * (i + 1)) / 2 + 5) - (n + 6);
+  };
+
+  TModularD r;
+  numeric::Fibonacci f;
+  for (unsigned i = 2; i <= N; ++i) {
+    r += S(f(i));
+  }
+  std::cout << r << std::endl;
+  return 0;
+}
