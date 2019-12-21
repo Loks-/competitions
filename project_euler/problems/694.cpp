d5b78f3a277730e47608ad3fade5d429a1d07e93
Loks- <alexeypoyarkov@gmail.com>
2019-12-21 11:42:23 -0800

    Simplify code.

--- project_euler/problems/694.cpp
+++ project_euler/problems/694.cpp
@@ -1,34 +1,25 @@
-#include "common/factorization/primes_list.h"
+#include "common/factorization/primes_generator.h"
 #include "common/stl/base.h"
 
 #include <functional>
 
 int main_694() {
-  uint64_t N = 1000000, M = 1000000000000000000ull;
-  factorization::PrimesList primes_list(N);
-  uint64_t s = M;
+  uint64_t N = 2000000, M = 1000000000000000000ull;
+  auto vp = GeneratePrimes(N);
+  uint64_t s = 0;
 
-  std::function<void(uint64_t k, const TFactorization& f, unsigned fi)> Add =
-      [&](uint64_t k, const TFactorization& f, unsigned fi) -> void {
-    if (fi == f.size()) {
+  std::function<void(uint64_t, unsigned)> Add = [&](uint64_t k,
+                                                    unsigned i) -> void {
+    uint64_t p = vp[i], l = p * p * p;
+    if (k > M / l) {
       s += M / k;
     } else {
-      for (; k <= M; k *= f[fi].prime) {
-        Add(k, f, fi + 1);
-        if (k > M / f[fi].prime) break;  // Avoid overflow
-      }
+      Add(k, i + 1);
+      for (; k <= M / l; k *= p) Add(k * l, i + 1);
     }
   };
 
-  for (uint64_t i = 2; i <= N; ++i) {
-    auto f = primes_list.FactorizeTable(i);
-    bool has_power_2plus = false;
-    for (auto p : f) {
-      if (p.power > 1) has_power_2plus = true;
-    }
-    if (has_power_2plus) continue;
-    Add(i * i * i, f, 0);
-  }
+  Add(1, 0);
   cout << s << endl;
   return 0;
 }
