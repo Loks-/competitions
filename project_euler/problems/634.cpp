5e6021ce782c982f4f691d46785f7c3d67133771
Loks- <alexeypoyarkov@gmail.com>
2019-12-21 18:11:15 -0800

    Clean solution.

--- project_euler/problems/634.cpp
+++ project_euler/problems/634.cpp
@@ -4,9 +4,7 @@
 #include <functional>
 
 int main_634() {
-  // uint64_t N = 3000000, M = 9000000000000000000ull;
   uint64_t N = 3000000000, M = 9000000000000000000ull;
-  // uint64_t N = 2000, M = 3000000;
   auto vp = GeneratePrimes(N);
   cout << "Primes created. Size = " << vp.size() << endl;
   uint64_t s = 0;
@@ -46,71 +44,6 @@ int main_634() {
     }
   };
 
-  std::function<void(uint64_t, unsigned)> Add11 = [&](uint64_t k,
-                                                      unsigned i) -> void {
-    if (k == 0) return;
-    s += 1;
-    for (; i < vp.size(); ++i) {
-      uint64_t p = vp[i], kp = k / (p * p);
-      if (kp == 0) break;
-      for (; kp; kp /= p) Add11(kp, i + 1);
-    }
-  };
-
-  std::function<void(uint64_t, unsigned)> Add01 = [&](uint64_t k,
-                                                      unsigned i) -> void {
-    for (; i < vp.size(); ++i) {
-      uint64_t p = vp[i], kp = k / (p * p);
-      if (kp == 0) break;
-      Add11(kp, i + 1);
-      kp /= p;
-      Add01(kp, i + 1);
-      for (kp /= p; kp; kp /= p) Add11(kp, i + 1);
-    }
-  };
-
-  std::function<void(uint64_t, unsigned)> Add10 = [&](uint64_t k,
-                                                      unsigned i) -> void {
-    for (; i < vp.size(); ++i) {
-      uint64_t p = vp[i], kp = k / (p * p);
-      if (kp == 0) break;
-      Add10(kp, i + 1);
-      kp /= p;
-      Add11(kp, i + 1);
-      kp /= p;
-      Add10(kp, i + 1);
-      for (kp /= p; kp; kp /= p) Add11(kp, i + 1);
-    }
-  };
-
-  std::function<void(uint64_t, unsigned)> AddXX = [&](uint64_t k,
-                                                      unsigned i) -> void {
-    for (; i < vp.size(); ++i) {
-      uint64_t p = vp[i], kp = k / (p * p);
-      if (kp == 0) break;
-      for (; kp; kp /= p) Add11(kp, i + 1);
-    }
-  };
-
-  std::function<void(uint64_t, unsigned)> Add00 = [&](uint64_t k,
-                                                      unsigned i) -> void {
-    for (; i < vp.size(); ++i) {
-      uint64_t p = vp[i], kp = k / (p * p);
-      if (kp == 0) break;
-      Add10(kp, i + 1);
-      kp /= p;
-      Add01(kp, i + 1);
-      kp /= p;
-      Add10(kp, i + 1);
-      kp /= p;
-      Add11(kp, i + 1);
-      kp /= p;
-      AddXX(kp, i + 1);
-      for (kp /= p; kp; kp /= p) Add11(kp, i + 1);
-    }
-  };
-
-  // Add00(M, 0);
   Add(M, 0, 3);
   cout << s << endl;
   return 0;
