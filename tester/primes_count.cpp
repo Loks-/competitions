2ce1bc64c23721d8fb1ca46cbb75cf070eb9779b
Loks- <alexeypoyarkov@gmail.com>
2020-01-04 23:37:45 -0800

    Add comment.

--- tester/primes_count.cpp
+++ tester/primes_count.cpp
@@ -92,6 +92,7 @@ uint64_t PrimesCount_LucyHedgehog(uint64_t n) {
   return Count(n, primes.size());
 }
 
+// Add cache for Count(k, i) where p_i^2 > k.
 uint64_t PrimesCount_LucyHedgehog2(uint64_t n) {
   uint64_t nsqrt = USqrt(n);
   auto primes = GeneratePrimes(nsqrt);
