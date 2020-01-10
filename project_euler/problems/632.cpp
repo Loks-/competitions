5c8570cf6e250abf3b65e30ff583d6a4dc5e378a
Loks- <alexeypoyarkov@gmail.com>
2020-01-10 14:59:33 -0800

    Spped up code.

--- project_euler/problems/632.cpp
+++ project_euler/problems/632.cpp
@@ -1,5 +1,4 @@
 #include "common/factorization/table/mobius.h"
-#include "common/factorization/table/utils/factorize.h"
 #include "common/factorization/table/utils/square_free_count.h"
 #include "common/modular_io.h"
 #include "common/numeric/utils/pow.h"
@@ -9,9 +8,13 @@
 int main_632() {
   uint64_t N = PowU<uint64_t>(10, 16), NSqrt = USqrt(N);
   factorization::table::Mobius mobius(NSqrt);
+  vector<unsigned> vk(NSqrt + 1, 0);
+  for (uint64_t p : mobius.GetPrimes()) {
+    for (uint64_t l = p; l <= NSqrt; l += p) ++vk[l];
+  }
   vector<uint64_t> vsk;
   for (uint64_t a = 1; a <= NSqrt; ++a) {
-    size_t k = Factorize(mobius, a).size();
+    size_t k = vk[a];
     if (vsk.size() <= k) vsk.resize(k + 1);
     vsk[k] += SquareFreeCount(mobius, N / (a * a));
   }
