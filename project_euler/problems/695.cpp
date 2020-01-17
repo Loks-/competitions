19c2447a3ef1991cb9171448bc4ae0528bfcab23
Loks- <alexeypoyarkov@gmail.com>
2020-01-16 20:30:07 -0800

    Fix f3_211 and f2_211.

--- project_euler/problems/695.cpp
+++ project_euler/problems/695.cpp
@@ -21,8 +21,8 @@ int main_695() {
   // Region 2.1: (x1 - x0) * (y1 - y0) <= x0 * y0
   // Region 2.1.1: 1 <= x0 + y0
   auto f4_211 = s2p;
-  auto f3_211 = Integration(f4_211, 3, 1. - y0, one);
-  auto f2_211 = Integration(f3_211, 2, 1. - x0, one);
+  auto f3_211 = Integration(f4_211, 3, y0, one);
+  auto f2_211 = Integration(f3_211, 2, x0, one);
   cout << f2_211 << endl;
   // Segment 3: x0 <= x1 <= 1, 0 <= y1 <= y0
   auto s3p = fc * (1. - x1) * (1. - y0);
