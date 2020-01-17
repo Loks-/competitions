b8561165cd8b334d323e4bad9de3b8e5dcdcf97c
Loks- <alexeypoyarkov@gmail.com>
2020-01-17 00:44:07 -0800

    Add LnAbsC class.

--- common/calculus/ext_polynomial/term_bases/type.h
+++ common/calculus/ext_polynomial/term_bases/type.h
@@ -3,7 +3,7 @@
 namespace calculus {
 namespace ext_polynomial {
 namespace term_bases {
-enum class Type { ONE, LN_ABS };
+enum class Type { ONE, LN_ABS, LN_ABS_C };
 }  // namespace term_bases
 }  // namespace ext_polynomial
 }  // namespace calculus
