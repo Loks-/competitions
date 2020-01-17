efc4073974d7a49ed10990dbcd14ca4efa0403a8
Loks- <alexeypoyarkov@gmail.com>
2020-01-17 00:46:37 -0800

    Code for future use.

--- project_euler/problems/695.cpp
+++ project_euler/problems/695.cpp
@@ -24,7 +24,7 @@ int main_695() {
   //   Substitute2 x0 = 1 - x0s, y0 = 1 - y0s
   //   After S2: 0 <= x1s <= x0s, 0 <= y1s <= y0s
   auto s2ps2 = Substitution(Substitution(s2ps1, 0, one - x0), 1, one - y0);
-  cout << s2ps2 << endl;
+  cout << "s2ps2 = " << s2ps2 << endl;
   //   Region 2.1: (x1 - x0) * (y1 - y0) <= x0 * y0
   //     After S1: x1s * y1s <= x0 * y0
   //     After S2: x1s * y1s <= (1 - x0s) * (1 - y0s)
@@ -33,17 +33,29 @@ int main_695() {
   auto f4_211 = s2ps2;
   auto f3_211 = Integration(f4_211, 3, zero, y0);
   auto f2_211 = Integration(f3_211, 2, zero, x0);
-  cout << f2_211 << endl;
+  cout << "f2_211 = " << f2_211 << endl;
+  auto f1_211 = Integration(f2_211, 1, zero, one - x0);
+  cout << "f1_211 = " << f1_211 << endl;
   //     Region 2.1.2: x0 + y0 <= 1
   //       After S2: 1 <= x0s + y0s
   auto f4_212 = s2ps2;
   auto f3_212c = Integration(f4_212, 3, (one - x0) * (one - y0) / x1, y0);
-  cout << f3_212c << endl;
+  cout << "f3_212c = " << f3_212c << endl;
   // a = (one - x0) * (one - y0) / y0, b = x0
   auto f3_212c_ii = Integration(f3_212c, 2);
-  cout << f3_212c_ii << endl;
+  cout << "f3_212c_ii = " << f3_212c_ii << endl;
   auto f2_212c_b = SubstitutionIndex(f3_212c_ii, 2, 0);
-  cout << f2_212c_b << endl;
+  cout << "f2_212c_b = " << f2_212c_b << endl;
+  // Temporary, incorrect substitution
+  auto f2_212c_a = SubstitutionIndex(f3_212c_ii, 2, 1);
+  cout << "f2_212c_a = " << f2_212c_a << endl;
+  auto f2_212c = f2_212c_b - f2_212c_a;
+  auto f2_212 = f2_211 - f2_212c;
+  cout << "f2_212 = " << f2_212 << endl;
+  auto f1_212 = Integration(f2_212, 1, one - x0, one);
+  auto f1_2 = f1_211 + f1_212;
+  auto f0_2 = Integration(f1_2, 0, zero, one);
+  cout << "f0_2 = " << f0_2 << endl;
   // cout << Project(f3_212c_ii, 0, 1.) << endl;
   // Segment 3: x0 <= x1 <= 1, 0 <= y1 <= y0
   auto s3p = fc * (1. - x1) * (1. - y0);
