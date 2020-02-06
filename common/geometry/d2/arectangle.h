3147975b0a3d15dd52dd697ea084e5b2af5be89e
Loks <alexeypoyarkov@gmail.com>
2020-02-06 14:11:22 -0800

    Rectangle -> ARectangle.

--- common/geometry/d2/arectangle.h
+++ common/geometry/d2/arectangle.h
@@ -5,13 +5,14 @@
 
 namespace geometry {
 namespace d2 {
+// Rectangle with edges parallel to coordinate axis
 template <class T>
-class Rectagnle {
+class ARectagnle {
  public:
   Point<T> p1, p2;
 
-  Rectagnle() : p1(), p2() {}
-  Rectagnle(const Point<T>& _p1, const Point<T>& _p2) : p1(_p1), p2(_p2) {}
+  ARectagnle() : p1(), p2() {}
+  ARectagnle(const Point<T>& _p1, const Point<T>& _p2) : p1(_p1), p2(_p2) {}
 
   bool StrictInside(const Point<T>& p) {
     return (p1.x < p.x) && (p.x < p2.x) && (p1.y < p.y) && (p.y < p2.y);
@@ -20,5 +21,5 @@ class Rectagnle {
 }  // namespace d2
 }  // namespace geometry
 
-using D2Rectagnle = geometry::d2::Rectagnle<double>;
-using I2Rectagnle = geometry::d2::Rectagnle<int64_t>;
+using D2ARectagnle = geometry::d2::ARectagnle<double>;
+using I2ARectagnle = geometry::d2::ARectagnle<int64_t>;
