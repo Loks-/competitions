66e3fd693cc19f591fe23da467e81623a88b8e87
Loks- <alexeypoyarkov@gmail.com>
2020-04-17 00:11:35 -0700

    Refactor AddEach class.

---
+++ common/binary_search_tree/action/add_each.h
@@ -0,0 +1,35 @@
+#pragma once
+
+#include "common/binary_search_tree/action/none.h"
+
+namespace bst {
+namespace action {
+template <class TData>
+class AddEach : public None {
+ public:
+  using TBase = None;
+  using TSelf = AddEach<TData>;
+  static const bool is_none = false;
+
+  TData add_value;
+
+  AddEach() : add_value() {}
+  bool IsEmpty() const { return add_value == TData(); }
+  void Clear() { add_value = TData(); };
+
+  template <class TNode>
+  void Add(TNode* node, const TData& value) {
+    add_value += value;
+  }
+
+  template <class TNode>
+  void Apply(TNode* node) {
+    if (IsEmpty()) return;
+    node->data += add_value;
+    if (node->l) node->l->AddAction(add_value);
+    if (node->r) node->r->AddAction(add_value);
+    add_value = TData();
+  }
+};
+}  // namespace action
+}  // namespace bst
--- common/binary_search_tree/action/add_each__sum.h
+++ common/binary_search_tree/action/add_each__sum.h
@@ -1,36 +1,20 @@
 #pragma once
 
-#include "common/binary_search_tree/action/none.h"
+#include "common/binary_search_tree/action/add_each.h"
 
 namespace bst {
 namespace action {
 // Support info::Sum
 template <class TData>
-class AddEachSum : public None {
+class AddEachSum : public AddEach<TData> {
  public:
-  using TBase = None;
+  using TBase = AddEach<TData>;
   using TSelf = AddEachSum<TData>;
-  static const bool is_none = false;
-
-  TData add_value;
-
-  AddEachSum() : add_value(0) {}
-  bool IsEmpty() const { return add_value == 0; }
-  void Clear() { add_value = 0; };
 
   template <class TNode>
   void Add(TNode* node, const TData& value) {
     node->info.sum += node->info.size * value;
-    add_value += value;
-  }
-
-  template <class TNode>
-  void Apply(TNode* node) {
-    if (IsEmpty()) return;
-    node->data += add_value;
-    if (node->l) node->l->AddAction(add_value);
-    if (node->r) node->r->AddAction(add_value);
-    add_value = 0;
+    TBase::Add(node, value);
   }
 };
 }  // namespace action
