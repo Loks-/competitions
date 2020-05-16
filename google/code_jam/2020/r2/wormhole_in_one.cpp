0a9f0cd81cade6f650b1bceb3bb5bc09102ad6d6
Loks- <alexeypoyarkov@gmail.com>
2020-05-16 11:29:48 -0700

    Simplify solution.

--- google/code_jam/2020/r2/wormhole_in_one.cpp
+++ google/code_jam/2020/r2/wormhole_in_one.cpp
@@ -1,6 +1,5 @@
 #include "common/geometry/d2/base.h"
 #include "common/geometry/d2/iangle.h"
-#include "common/geometry/d2/line_pv.h"
 #include "common/geometry/d2/point_io.h"
 #include "common/geometry/d2/stl_hash/iangle.h"
 #include "common/stl/base.h"
@@ -15,22 +14,16 @@ int main_wormhole_in_one() {
   for (unsigned it = 1; it <= T; ++it) {
     cin >> N;
     auto v = ReadVector<I2Point>(N);
-    unordered_map<I2Angle, unordered_map<int64_t, unordered_set<unsigned>>> m;
+    unordered_map<I2Angle, unordered_set<unsigned>> m;
     for (unsigned i = 0; i < N; ++i) {
       for (unsigned j = 0; j < N; ++j) {
         if (i == j) continue;
-        auto a = I2Angle(v[j] - v[i]);
-        auto c = I2LinePV(v[i], a.ToVector())({});
-        m[a][c].insert({i, j});
+        m[I2Angle(v[j] - v[i])].insert({i, j});
       }
     }
-    unsigned best = 1;
-    for (auto& p : m) {
-      unsigned s = 0;
-      for (auto& pp : p.second) s += pp.second.size();
-      best = max(best, 2 + s - (s & 1));
-    }
-    cout << "Case #" << it << ": " << min(best, N) << endl;
+    unsigned best = 0;
+    for (auto& p : m) best = max(best, unsigned(p.second.size()));
+    cout << "Case #" << it << ": " << min(best - (best & 1) + 2, N) << endl;
   }
   return 0;
 }
