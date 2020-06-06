7e564f93303824352d550caf48442c04be7df433
Loks- <alexeypoyarkov@gmail.com>
2020-06-06 16:10:25 -0700

    Add solution and tester (commented out) for "Pen Testing" problem.

---
+++ common/vector/mask.h
@@ -0,0 +1,17 @@
+#pragma once
+
+#include "common/base.h"
+
+#include <vector>
+
+inline std::vector<unsigned> MaskToVector(uint64_t mask) {
+  std::vector<unsigned> v;
+  for (unsigned i = 0; mask; ++i) {
+    uint64_t b = (1ull << i);
+    if (mask & b) {
+      v.push_back(i);
+      mask &= ~b;
+    }
+  }
+  return v;
+}
