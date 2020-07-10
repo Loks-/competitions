6142c9bc16c9a3ef2dffec9338efe6621ef37625
Loks- <alexeypoyarkov@gmail.com>
2020-07-09 23:05:23 -0700

    Add ConnectedComponentsCount.

---
+++ common/graph/graph/connected_components_count.h
@@ -0,0 +1,30 @@
+#pragma once
+
+#include "common/graph/graph.h"
+
+#include <stack>
+#include <vector>
+
+// Time: O(V + E)
+inline unsigned ConnectedComponentsCount(const UndirectedGraph& graph) {
+  unsigned n = graph.Size(), r = 0;
+  std::vector<unsigned> visited(n, 0);
+  std::stack<unsigned> s;
+  for (unsigned i = 0; i < n; ++i) {
+    if (visited[i] == 0) {
+      visited[i] = 1;
+      ++r;
+      for (s.push(i); !s.empty();) {
+        unsigned j = s.top();
+        s.pop();
+        for (unsigned c : graph.Edges(j)) {
+          if (visited[c] == 0) {
+            visited[c] = 1;
+            s.push(c);
+          }
+        }
+      }
+    }
+  }
+  return r;
+}
