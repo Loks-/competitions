6c7e5f2e88f02e41866140e0175357e4586310d6
Loks- <alexeypoyarkov@gmail.com>
2020-07-22 01:16:57 -0700

    Add modes to main.

--- icfppc/2020/src/main.cpp
+++ icfppc/2020/src/main.cpp
@@ -6,14 +6,25 @@
 #include <string>
 
 int main(int argc, char* argv[]) {
+  if (argc < 2) {
+    cout << "Mode required. Supported modes are: [galaxy, galaxy_test]."
+         << std::endl;
+    return -1;
+  }
   LoadGalaxy();
-  TestGalaxy();
-  if (argc >= 2) {
+  string mode = argv[1];
+  if (mode == "galaxy") {
+    assert(argc > 2);
     MessageDecoder md;
-    Expression e = md.DecodeExpression(argv[1]);
+    Expression e = md.DecodeExpression(argv[2]);
     e.Evaluate();
     e.Print();
     cout << endl;
+  } else if (mode == "galaxy_test") {
+    return TestGalaxy();
+  } else {
+    cout << "Unknown mode." << endl;
+    return -1;
   }
   return 0;
 }
