aabf93ff44245a592eb0e8a497062fbe3bc55f44
Loks- <alexeypoyarkov@gmail.com>
2020-07-23 18:07:27 -0700

    Add protocol exmpales to main.

--- icfppc/2020/src/main.cpp
+++ icfppc/2020/src/main.cpp
@@ -5,6 +5,31 @@
 
 #include <string>
 
+// Protocol commands examples:
+//   Initial sequence:
+//     1. "ap ap ap interact galaxy ( ) ap ap vec 0 0"
+//     2. "ap ap ap interact galaxy ( 0 , ( 0 ) , 0 , ( ) ) ap ap vec 0 0"
+//     3. "ap ap ap interact galaxy ( 0 , ( 1 ) , 0 , ( ) ) ap ap vec 0 0"
+//     4. "ap ap ap interact galaxy ( 0 , ( 2 ) , 0 , ( ) ) ap ap vec 0 0"
+//     5. "ap ap ap interact galaxy ( 0 , ( 3 ) , 0 , ( ) ) ap ap vec 0 0"
+//     6. "ap ap ap interact galaxy ( 1 , ( 1 ) , 0 , ( ) ) ap ap vec 0 0"
+//     7. "ap ap ap interact galaxy ( 1 , ( 2 ) , 0 , ( ) ) ap ap vec 0 0"
+//     8. "ap ap ap interact galaxy ( 1 , ( 3 ) , 0 , ( ) ) ap ap vec 0 0"
+//     9. "ap ap ap interact galaxy ( 1 , ( 4 ) , 0 , ( ) ) ap ap vec 8 4"
+//    10. "ap ap ap interact galaxy ( 1 , ( 5 ) , 0 , ( ) ) ap ap vec 2 -8"
+//    11. "ap ap ap interact galaxy ( 1 , ( 6 ) , 0 , ( ) ) ap ap vec 3 6"
+//    12. "ap ap ap interact galaxy ( 1 , ( 7 ) , 0 , ( ) ) ap ap vec 0 -14"
+//    13. "ap ap ap interact galaxy ( 1 , ( 8 ) , 0 , ( ) ) ap ap vec -4 10"
+//    14. "ap ap ap interact galaxy ( 1 , ( 9 ) , 0 , ( ) ) ap ap vec 9 -3"
+//    15. "ap ap ap interact galaxy ( 1 , ( 10 ) , 0 , ( ) ) ap ap vec -4 10"
+//    16. "ap ap ap interact galaxy ( 1 , ( 11 ) , 0 , ( ) ) ap ap vec 1 4"
+//   Galaxy: (most pixels are fine as vector)
+//     "ap ap ap interact galaxy ( 2 , ( 1 , -1 ) , 0 , ( ) ) ap ap vec 1000
+//     1000"
+//   Tutorial:
+//     "ap ap ap interact galaxy ( 5 , ( 2 , 1 , nil , nil , nil , nil , nil ,
+//     44309 ) , 9 , ( 103652820 , 192496425430 ) ) ap ap vec 1000 1000"
+
 int main(int argc, char* argv[]) {
   if (argc < 2) {
     cout << "Mode required. Supported modes are: [galaxy, galaxy_test]."
