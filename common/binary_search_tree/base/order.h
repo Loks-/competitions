e9b019ed23bb7b57b0898a1f1dfe16c9a6a2bdbc
Loks- <alexeypoyarkov@gmail.com>
2021-03-26 15:03:45 -0700

    Add solution and tests for Reversort problem.

---
+++ common/binary_search_tree/base/order.h
@@ -0,0 +1,25 @@
+#pragma once
+
+#include "common/base.h"
+#include "common/binary_search_tree/action/apply_root_to_node.h"
+
+namespace bst {
+namespace base {
+template <class TNode>
+size_t Order(TNode* node) {
+  static_assert(TNode::use_parent, "use_parent should be true");
+  static_assert(TNode::TInfo::has_size, "info should contain size");
+  assert(node);
+  bst::action::ApplyRootToNode(node);
+  size_t order = 0;
+  if (node->l) order += node->l->info.size;
+  for (; node->p; node = node->p) {
+    if (node->p->r == node) {
+      if (node->p->l) order += node->p->l->info.size;
+      ++order;
+    }
+  }
+  return order;
+}
+}  // namespace base
+}  // namespace bst
---
+++ google/code_jam/2021/qr/reversort.cpp
@@ -0,0 +1,33 @@
+#include "common/binary_search_tree/action/reverse.h"
+#include "common/binary_search_tree/base/order.h"
+#include "common/binary_search_tree/treap.h"
+#include "common/binary_search_tree/utils/add_action_to_segment.h"
+#include "common/stl/base.h"
+#include "common/template.h"
+#include "common/vector/read.h"
+
+using TTree =
+    bst::Treap<false, true, TEmpty, bst::info::Size, bst::action::Reverse>;
+using TNode = TTree::TNode;
+
+int main_reversort() {
+  unsigned T, N;
+  cin >> T;
+  for (unsigned it = 0; it < T; ++it) {
+    cin >> N;
+    TTree tree(N);
+    for (unsigned i = 0; i < N; ++i) tree.New();
+    auto vu = ReadVector<unsigned>(N);
+    std::vector<TNode*> vn;
+    for (unsigned u : vu) vn.push_back(tree.NodeByRawIndex(u - 1));
+    auto r = tree.BuildTree(vn);
+    uint64_t c = 0;
+    for (unsigned i = 0; i < N - 1; ++i) {
+      auto position = bst::base::Order(tree.NodeByRawIndex(i));
+      c += position - i + 1;
+      r = bst::AddActionToSegment<TTree>(r, i, position + 1, true);
+    }
+    cout << "Case #" << it + 1 << ": " << c << endl;
+  }
+  return 0;
+}
