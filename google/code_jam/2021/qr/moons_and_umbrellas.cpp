5e4bbcd1566d6ba18bdb80b4e046f362f946f40e
Loks- <alexeypoyarkov@gmail.com>
2021-03-29 21:58:22 -0700

    Simplify solution.

--- google/code_jam/2021/qr/moons_and_umbrellas.cpp
+++ google/code_jam/2021/qr/moons_and_umbrellas.cpp
@@ -6,80 +6,28 @@ int main_moons_and_umbrellas() {
   unsigned T;
   cin >> T;
   for (unsigned it = 0; it < T; ++it) {
-    int cj, jc, cjc, tc = 0;
-    cin >> cj >> jc;
-    cjc = cj + jc;
-
-    auto SolveBB = [&](int n) {
-      assert(n > 1);
-      if ((cj < 0) || (jc < 0)) {
-        if (cjc < 0) {
-          int c = cjc * ((n - 2) / 2);
-          if ((n & 1)) c += min(cj, jc);
-          return c;
-        } else if (n > 2) {
-          return min(cj, jc);
-        }
-      }
-      return 0;
-    };
-
-    auto SolveBC = [&](int n) {
-      int c = 0;
-      if (cjc < 0) {
-        c = cjc * ((n - 1) / 2);
-        if ((n & 1) == 0) c += min(jc, 0);
-      }
-      return min(((n > 1) ? jc : 0), c);
-    };
-
-    auto SolveBJ = [&](int n) {
-      int c = 0;
-      if (cjc < 0) {
-        c = cjc * ((n - 1) / 2);
-        if ((n & 1) == 0) c += min(cj, 0);
-      }
-      return min(((n > 1) ? cj : 0), c);
-    };
-
-    auto SolveCB = [&](int n) { return SolveBJ(n); };
-    auto SolveJB = [&](int n) { return SolveBC(n); };
-    auto SolveCC = [&](int n) { return (n / 2) * min(cjc, 0); };
-    auto SolveCJ = [&](int n) { return cj + SolveCC(n - 1); };
-    auto SolveJC = [&](int n) { return jc + SolveCC(n - 1); };
-    auto SolveJJ = [&](int n) { return SolveCC(n); };
-
+    int cj, jc, tc = 0, tj = 0, l = 2000000;
     string s;
+    cin >> cj >> jc;
     cin >> s;
-    s = "B" + s;
-    unsigned p0 = 0, p1 = 1;
-    for (; p1 < s.size(); ++p1) {
-      if (s[p1] == 'C') {
-        if (s[p0] == 'C')
-          tc += SolveCC(p1 - p0);
-        else if (s[p0] == 'J')
-          tc += SolveJC(p1 - p0);
-        else
-          tc += SolveBC(p1 - p0);
-        p0 = p1;
-      } else if (s[p1] == 'J') {
-        if (s[p0] == 'C')
-          tc += SolveCJ(p1 - p0);
-        else if (s[p0] == 'J')
-          tc += SolveJJ(p1 - p0);
-        else
-          tc += SolveBJ(p1 - p0);
-        p0 = p1;
+    if (s[0] == 'C')
+      tj = l;
+    else if (s[0] == 'J')
+      tc = l;
+    for (unsigned i = 1; i < s.size(); ++i) {
+      if (s[i] == 'C') {
+        tc = min(tc, tj + jc);
+        tj = l;
+      } else if (s[i] == 'J') {
+        tj = min(tj, tc + cj);
+        tc = l;
+      } else {  // s[i] == '?'
+        int tc_ = tc;
+        tc = min(tc, tj + jc);
+        tj = min(tj, tc_ + cj);
       }
     }
-    if (s[p0] == 'C')
-      tc += SolveCB(p1 - p0);
-    else if (s[p0] == 'J')
-      tc += SolveJB(p1 - p0);
-    else
-      tc += SolveBB(p1 - p0);
-
-    cout << "Case #" << it + 1 << ": " << tc << endl;
+    cout << "Case #" << it + 1 << ": " << min(tc, tj) << endl;
   }
   return 0;
 }
