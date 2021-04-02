3936e2eae797b3297dde1b6e0671b1a787ea4ad5
Loks- <alexeypoyarkov@gmail.com>
2021-04-02 00:11:56 -0700

    Add proxy functions.

---
+++ common/graph/tree_ei/tree_path_maxima.h
@@ -0,0 +1,21 @@
+#pragma once
+
+#include "common/graph/graph_ei/edge_cost_proxy.h"
+#include "common/graph/tree_ei.h"
+#include "common/graph/tree_ei/tpm/tpm_pbst_fbt.h"
+
+#include <utility>
+#include <vector>
+
+namespace graph {
+// Cost on edges
+template <class TEdgeInfo, class TEdgeCostFunction>
+inline std::vector<typename TEdgeCostFunction::TEdgeCost> TreePathMaxima(const TreeEI<TEdgeInfo>& tree, const TEdgeCostFunction& f, const std::vector<std::pair<unsigned, unsigned>>& paths) {
+  return tpm::TPM_PBST_FBT(tree, f, paths);
+}
+
+template <class TEdgeInfo>
+inline std::vector<TEdgeInfo> TreePathMaxima(const TreeEI<TEdgeInfo>& tree, const std::vector<std::pair<unsigned, unsigned>>& paths) {
+  return TreePathMaxima(tree, EdgeCostProxy<TEdgeInfo>{}, param_type);    
+}
+}  // namespace graph
