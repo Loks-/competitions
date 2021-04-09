bccc172ca6bccf874412ce3036ec24715433f297
Loks- <alexeypoyarkov@gmail.com>
2021-04-09 12:41:10 -0700

    Add solution for RMQ with linear block search.

---
+++ common/vector/rmq/lbs.h
@@ -0,0 +1,93 @@
+#pragma once
+
+#include "common/base.h"
+#include "common/vector/rmq/position_value.h"
+#include "common/vector/rmq/ppt.h"
+
+#include <algorithm>
+#include <cmath>
+#include <vector>
+
+namespace nvector {
+namespace rmq {
+// Similar to PPT but use linear search on small blocks without preprocessing.
+// Block size ~ log(N).
+// O(N) memory, O(N) preprocessing time, O(log N) request time
+template <class TTValue>
+class LBS {
+ public:
+  using TValue = TTValue;
+  using TPositionValue = PositionValue<TValue>;
+
+ protected:
+  size_t n, k, l;
+  std::vector<size_t> vlpos;
+  std::vector<TValue> vv;
+  PPT<TValue> rmq;
+
+ public:
+  LBS() {}
+  LBS(const std::vector<TValue>& v, double block_size_scale = 0.5) {
+    Build(v, block_size_scale);
+  }
+
+  void Build(const std::vector<TValue>& v, double block_size_scale = 0.5) {
+    n = v.size();
+    k = std::max<size_t>(size_t(block_size_scale * log(double(n))), 1);
+    if (k == 1) {
+      rmq.Build(v);
+      return;
+    }
+    vv = v;
+    l = n / k;
+    vlpos.resize(l);
+    std::vector<TValue> vlmin(l);
+    for (size_t i = 0; i < l; ++i) {
+      size_t pos = i * k;
+      TValue value = v[i * k];
+      for (size_t j = 1; j < k; ++j) {
+        if (v[i * k + j] < value) {
+          pos = i * k + j;
+          value = v[i * k + j];
+        }
+      }
+      vlpos[i] = pos;
+      vlmin[i] = value;
+    }
+    rmq.Build(vlmin);
+  }
+
+  TPositionValue Minimum(size_t b, size_t e) const {
+    if (k == 1) return rmq.Minimum(b, e);
+    assert(b < e);
+    size_t ib = b / k, ie = e / k;
+    if (ib + 1 < ie) {
+      auto pv = rmq.Minimum(ib + 1, ie);
+      pv.pos = vlpos[pv.pos];
+      for (size_t i = b; i < (ib + 1) * k; ++i) {
+        if (vv[i] < pv.value) {
+          pv.pos = i;
+          pv.value = vv[i];
+        }
+      }
+      for (size_t i = ie * k; i < e; ++i) {
+        if (vv[i] < pv.value) {
+          pv.pos = i;
+          pv.value = vv[i];
+        }
+      }
+      return pv;
+    } else {
+      TPositionValue pv{b, vv[b]};
+      for (size_t i = b + 1; i < e; ++i) {
+        if (vv[i] < pv.value) {
+          pv.pos = i;
+          pv.value = vv[i];
+        }
+      }
+      return pv;
+    }
+  }
+};
+}  // namespace rmq
+}  // namespace nvector
