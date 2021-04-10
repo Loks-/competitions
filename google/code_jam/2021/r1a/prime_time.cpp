41ccf97c9507cc8d1a1cb8e0e65be93eb0aa1c28
Loks- <alexeypoyarkov@gmail.com>
2021-04-09 21:12:04 -0700

    Fix big test.

--- google/code_jam/2021/r1a/prime_time.cpp
+++ google/code_jam/2021/r1a/prime_time.cpp
@@ -10,8 +10,10 @@ int main_prime_time() {
   for (unsigned it = 1; it <= T; ++it) {
     cin >> M;
     auto v = nvector::Read<pair<uint64_t, uint64_t>>(M);
-    uint64_t s = 0, best = 0;
+    uint64_t s = 0, best = 0, s0 = 0;
     for (auto p : v) s += p.first * p.second;
+    for (uint64_t st = s; st; st /= v.back().first) s0 += v.back().first;
+    uint64_t s1 = (s0 >= s) ? 1 : (s - s0);
 
     std::function<void(uint64_t, uint64_t, uint64_t)> Solve = [&](
         uint64_t cs, uint64_t cm, uint64_t ci) -> void {
@@ -19,6 +21,7 @@ int main_prime_time() {
       if (ci >= M) return;
       uint64_t p = v[ci].first;
       if (cm > (s - cs - p) / p) return;
+      if (s / cm == s1 / cm) return;
       for (unsigned j = 0; j <= v[ci].second; ++j) {
         Solve(cs, cm, ci + 1);
         if (cm > s / p) break;
