39e32673e38912f599b4e49d57c9dd3e67cf2f74
Loks- <alexeypoyarkov@gmail.com>
2021-04-12 11:53:44 -0700

    Speed up GetBinomialCoefficientL to avoid long divisions.

--- common/numeric/long/utils/binomial_coefficient.h
+++ common/numeric/long/utils/binomial_coefficient.h
@@ -3,7 +3,10 @@
 #include "common/numeric/long/unsigned.h"
 #include "common/numeric/long/utils/factorial.h"
 
-inline LongUnsigned GetBinomialCoefficientL(unsigned n, unsigned k) {
+inline LongUnsigned GetBinomialCoefficientL(uint32_t n, uint32_t k) {
   if (k > n) return LongUnsigned(0);
-  return GetFactorialL(n) / GetFactorialL(k) / GetFactorialL(n - k);
+  if (k > n - k) k = n - k;
+  LongUnsigned l(1u);
+  for (uint32_t i = 1; i <= k; ++i) l = (l * (n - k + i)) / i;
+  return l;
 }
