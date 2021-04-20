9307197e08b171b5255be4b1b14fbaeb75e106f0
Loks- <alexeypoyarkov@gmail.com>
2021-04-20 13:36:15 -0700

    Add GCDSafe.

--- common/numeric/utils/gcd.h
+++ common/numeric/utils/gcd.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include "common/numeric/utils/abs.h"
+
 template <class T>
 inline T GCD(T x, T y) {
   for (; y;) {
@@ -8,4 +10,9 @@ inline T GCD(T x, T y) {
     y = z;
   }
   return x;
+}
+
+template <class T>
+inline T GCDSafe(T x, T y) {
+  return GCD<T>(Abs(x), Abs(y));
 }
--- hackerrank/practice/mathematics/fundamentals/sumar_and_the_floating_rocks.cpp
+++ hackerrank/practice/mathematics/fundamentals/sumar_and_the_floating_rocks.cpp
@@ -9,7 +9,7 @@ int main_sumar_and_the_floating_rocks() {
   for (unsigned it = 0; it < T; ++it) {
     int x1, y1, x2, y2;
     cin >> x1 >> y1 >> x2 >> y2;
-    cout << GCD(abs(x1 - x2), abs(y1 - y2)) - 1 << endl;
+    cout << GCDSafe(x1 - x2, y1 - y2) - 1 << endl;
   }
   return 0;
 }
