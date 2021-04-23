5380af4ea0355d83940d1a8814dda6da6041d404
Loks- <alexeypoyarkov@gmail.com>
2021-04-23 11:27:20 -0700

    Simplify code.

--- hackerrank/practice/mathematics/geometry/a_circle_and_a_square.cpp
+++ hackerrank/practice/mathematics/geometry/a_circle_and_a_square.cpp
@@ -2,6 +2,7 @@
 
 #include "common/geometry/d2/circle_int_io.h"
 #include "common/geometry/d2/point_io.h"
+#include "common/geometry/d2/utils/scale_point.h"
 #include "common/geometry/d2/utils/square_from_diagonal.h"
 #include "common/stl/base.h"
 
@@ -10,13 +11,11 @@ int main_a_circle_and_a_square() {
   I2Circle c;
   I2Point pa, pc;
   cin >> w >> h >> c >> pa >> pc;
-  I2Point pa2 = (I2Vector(pa) * 2).ToPoint(),
-          pc2 = (I2Vector(pc) * 2).ToPoint();
-  auto s = SquareFromDiagonal(pa2, pc2);
+  auto s = SquareFromDiagonal(ScaleXY(pa, 2), ScaleXY(pc, 2));
   for (int y = 0; y < h; ++y) {
     for (int x = 0; x < w; ++x) {
-      I2Point p(x, y), p2(2 * x, 2 * y);
-      cout << ((c.Inside(p) || s.Inside(p2)) ? "#" : ".");
+      I2Point p(x, y);
+      cout << ((c.Inside(p) || s.Inside(ScaleXY(p, 2))) ? "#" : ".");
     }
     cout << endl;
   }
