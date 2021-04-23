005f96917df9cf0c9671ab2d5cfa65b7c8521d95
Loks- <alexeypoyarkov@gmail.com>
2021-04-23 11:27:48 -0700

    Add weighted point class.

---
+++ common/geometry/d2/wpoint.h
@@ -0,0 +1,23 @@
+#pragma once
+
+#include "common/base.h"
+#include "common/geometry/d2/point.h"
+
+namespace geometry {
+namespace d2 {
+template <class T1, class T2 = T1>
+class WPoint : public Point<T1> {
+ public:
+  using TBase = Point<T1>;
+  using TSelf = WPoint<T1, T2>;
+
+  T2 w;
+
+  WPoint() : w(T2(1)) {}
+  WPoint(const T1& _x, const T1& _y, const T2& _w) : TBase(_x, _y), w(_w) {}
+};
+}  // namespace d2
+}  // namespace geometry
+
+using D2WPoint = geometry::d2::WPoint<double>;
+using I2WPoint = geometry::d2::WPoint<int64_t>;
