8898aaf8e7aa9f8b71c5ab04a0f742f5dee1a468
Loks- <alexeypoyarkov@gmail.com>
2021-04-23 09:45:55 -0700

    Rename function.

--- common/geometry/d3/utils/intersection_line_plane.h
+++ common/geometry/d3/utils/intersection_line_plane.h
@@ -4,8 +4,8 @@
 #include "common/geometry/d3/plane.h"
 
 template <class T>
-inline T Intersect(const geometry::d3::Line<T>& l,
-                   const geometry::d3::Plane<T>& p) {
+inline T Intersection(const geometry::d3::Line<T>& l,
+                      const geometry::d3::Plane<T>& p) {
   T x0 = p(l(0)), x1 = p(l(1));
   return x0 / (x0 - x1);
 }
