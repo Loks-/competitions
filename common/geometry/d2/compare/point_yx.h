ef9268b4cf61ef9d15090b7b046ed6e819087f40
Loks- <alexeypoyarkov@gmail.com>
2021-04-23 08:18:12 -0700

    Simplify code.

---
+++ common/geometry/d2/compare/point_yx.h
@@ -0,0 +1,9 @@
+#pragma once
+
+#include "common/geometry/d2/point.h"
+
+template <class T>
+inline bool CompareYX(const geometry::d2::Point<T>& p1,
+                      const geometry::d2::Point<T>& p2) {
+  return (p1.y == p2.y) ? (p1.x < p2.x) : (p1.y < p2.y);
+}
