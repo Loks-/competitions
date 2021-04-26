4488423c65500346223b3ad37341e3c7f2ddcf5f
Loks- <alexeypoyarkov@gmail.com>
2021-04-25 21:43:31 -0700

    Simplify solution.

--- google/code_jam/2021/r1b/subtransmutation.cpp
+++ google/code_jam/2021/r1b/subtransmutation.cpp
@@ -1,8 +1,6 @@
-#include "common/binary_search.h"
 #include "common/numeric/utils/gcd.h"
 #include "common/stl/base.h"
 #include "common/vector/read.h"
-#include "common/vector/write.h"
 
 #include <string>
 
@@ -13,23 +11,19 @@ int main_subtransmutation() {
     cin >> N >> A >> B;
     auto v = nvector::Read<unsigned>(N);
     auto g = GCD(A, B), g1 = (N - 1) % g;
-    A /= g;
-    B /= g;
-    std::vector<unsigned> v2((N - 1) / g + 1, 0), vt;
+    std::vector<unsigned> vt;
     bool ok = true;
     for (unsigned i = 0; i < N; ++i) {
       if (!v[i]) continue;
       if ((i % g) != g1) ok = false;
-      v2[i / g] = v[i];
     }
-    // nvector::Write(v2);
 
     auto F = [&](unsigned n) {
       vt.clear();
       vt.resize(++n, 0);
       vt.back() = 1;
       for (unsigned i = n; i-- > 0;) {
-        auto v2i = (i < v2.size() ? v2[i] : 0);
+        auto v2i = (i < v.size() ? v[i] : 0);
         if (vt[i] < v2i) return false;
         if (i >= B) vt[i - B] += vt[i] - v2i;
         if (i >= A) vt[i - A] += vt[i] - v2i;
@@ -38,17 +32,15 @@ int main_subtransmutation() {
     };
 
     if (ok) {
-      // k = LowerBoundB(unsigned(v2.size()), F);
-      // for (unsigned i = max<unsigned>(v2.size() + A, k) - A; i < k; ++i) {
-      for (unsigned i = v2.size();; ++i) {
+      for (unsigned i = N;; ++i) {
         if (F(i)) {
           k = i;
           break;
         }
       }
     }
-    cout << "Case #" << it << ": "
-         << (ok ? to_string(k * g + g1 + 1) : "IMPOSSIBLE") << endl;
+    cout << "Case #" << it << ": " << (ok ? to_string(k + 1) : "IMPOSSIBLE")
+         << endl;
   }
   return 0;
 }
