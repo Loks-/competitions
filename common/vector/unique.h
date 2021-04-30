d17fc92f1230ca429ef14232e3ba85446cabe0cf
Loks- <alexeypoyarkov@gmail.com>
2021-04-30 10:02:04 -0700

    Add nvector::Unique.

---
+++ common/vector/unique.h
@@ -0,0 +1,17 @@
+#pragma once
+
+#include <algorithm>
+#include <vector>
+
+namespace nvector {
+template <class T>
+inline void Unique(std::vector<T>& v) {
+  v.erase(std::unique(v.begin(), v.end()), v.end());
+}
+
+template <class T>
+inline void UniqueS(std::vector<T>& v) {
+  std::sort(v.begin(), v.end());
+  Unique(v);
+}
+}  // namespace nvector
