9099b63e5f2997ffc43266348d1406c926fdcd77
Loks- <alexeypoyarkov@gmail.com>
2021-05-13 00:04:16 -0700

    Add new algorithm for testng in submit.

--- codingame/contests/2105/submit.cpp
+++ codingame/contests/2105/submit.cpp
@@ -2,13 +2,14 @@
 #include "fstrategy_complete.h"
 #include "fstrategy_wait_and_complete.h"
 #include "game.h"
-#include "strategy_wsgc_uts2.h"
 // #include "strategy_mcts3.h"
+#include "strategy_wsgc_mcts.h"
+// #include "strategy_wsgc_uts2.h"
 
 int main_submit() {
-  auto s = StrategyWSGCUTS2<FStrategyWaitAndComplete>::Make();
-  // auto s = StrategyMCTS3<FStrategyComplete,
-  // FStrategyWaitAndComplete>::Make();
+  // auto s = StrategyMCTS3<FStrategyWaitAndComplete>::Make();
+  auto s = StrategyWSGCMCTS<FStrategyWaitAndComplete>::Make();
+  // auto s = StrategyWSGCUTS2<FStrategyWaitAndComplete>::Make();
   s->SetP(1);
   Game g;
   g.ReadCells();
