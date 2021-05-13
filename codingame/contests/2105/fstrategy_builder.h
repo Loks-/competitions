db7ef51a985f4b2a527f115a44e340a0b30a4cfc
Loks- <alexeypoyarkov@gmail.com>
2021-05-13 10:20:47 -0700

    Tune FStrategyBuilder for better performance at the end.

--- codingame/contests/2105/fstrategy_builder.h
+++ codingame/contests/2105/fstrategy_builder.h
@@ -4,6 +4,8 @@
 #include "game.h"
 #include "settings.h"
 
+#include <algorithm>
+
 template <unsigned target_trees = 8, unsigned max_seeds = 2>
 class FStrategyBuilder {
  public:
@@ -13,30 +15,37 @@ class FStrategyBuilder {
     unsigned days_left = TotalDays() - game.pos.day - 1,
              ntrees = p.ntrees[1] + p.ntrees[2] + p.ntrees[3];
     // Complete
-    if ((ntrees >= target_trees) && (p.ntrees[3] > 0) &&
-        (p.sun >= CompleteCost())) {
+    if ((ntrees >= std::min(target_trees, days_left + 1u)) &&
+        (p.ntrees[3] > days_left) && (p.sun >= CompleteCost())) {
       for (unsigned i = 0; i < CellsSize(); ++i) {
         auto index = game.pos.cell_to_tree[i];
         if (index == 255) continue;
         auto& t = game.pos.trees[index];
-        if ((t.player == player) && (t.size == 3) && !t.used)
-          return Action(COMPLETE, t.cell);
+        if ((t.player == player) && (t.size == 3) && !t.used) {
+          if ((game.pos.nutrients > 0) || (days_left == 0)) {
+            if (game.pos.nutrients +
+                    CompletePointsFromRichness(game.cells[t.cell].richness) >=
+                2 + days_left)
+              return Action(COMPLETE, t.cell);
+          }
+          break;
+        }
       }
     }
     // Grow
-    if (days_left > 0) {
-      for (unsigned tsize = 3; tsize-- > 0;) {
-        if ((p.ntrees[tsize] > 0) &&
-            (p.sun >= GrowBaseCost(tsize + 1) + p.ntrees[tsize + 1])) {
-          for (auto& t : game.pos.trees) {
-            if ((t.player == player) && (t.size == tsize) && !t.used)
-              return Action(GROW, t.cell);
-          }
+    for (unsigned tsize = 3; tsize-- > 0;) {
+      if ((p.ntrees[tsize] > 0) &&
+          (p.sun >= GrowBaseCost(tsize + 1) + p.ntrees[tsize + 1]) &&
+          (tsize + days_left >= 3)) {
+        for (auto& t : game.pos.trees) {
+          if ((t.player == player) && (t.size == tsize) && !t.used)
+            return Action(GROW, t.cell);
         }
       }
     }
     // Seed
-    if ((p.ntrees[0] < max_seeds) && (p.sun >= SeedBaseCost() + p.ntrees[0])) {
+    if ((p.ntrees[0] < max_seeds) && (p.sun >= SeedBaseCost() + p.ntrees[0]) &&
+        ((p.ntrees[0] == 0) || (days_left >= 4))) {
       unsigned best_to = CellsSize(), best_from = CellsSize();
       for (auto& t : game.pos.trees) {
         if ((t.player == player) && !t.used) {
--- codingame/contests/2105/main.cpp
+++ codingame/contests/2105/main.cpp
@@ -1,6 +1,5 @@
 #include "fstrategy_builder.h"
 #include "fstrategy_complete.h"
-#include "fstrategy_random.h"
 #include "fstrategy_wait_and_complete.h"
 #include "runner.h"
 #include "strategy.h"
@@ -19,15 +18,15 @@ int main(int nargs, char **pargs) {
   Runner r;
   // Runner r(2230846503);
   // r.SetStrategy0(Strategy::Make());
-  r.SetStrategy0(StrategyProxy<FStrategyBuilder<8, 2>>::Make());
+  r.SetStrategy0(StrategyProxy<FStrategyBuilder<8, 2>>::Make("Builder 8/2"));
   // r.SetStrategy0(StrategyMCTS2::Make());
   // r.SetStrategy0(StrategyWSGCUTS::Make());
   // r.SetStrategy0(StrategyWSGCUTS2<FStrategyWaitAndComplete>::Make());
   // r.SetStrategy0(StrategyWSGCMCTS<FStrategyWaitAndComplete>::Make());
   // r.SetStrategy1(Strategy::Make());
-  // r.SetStrategy1(StrategyProxy<FStrategyRandom>::Make("Random"));
+  r.SetStrategy1(StrategyProxy<FStrategyBuilder<10, 2>>::Make("Builder 10/2"));
   // r.SetStrategy1(StrategyMCTS2::Make());
-  r.SetStrategy1(StrategyMCTS3<FStrategyWaitAndComplete>::Make());
+  // r.SetStrategy1(StrategyMCTS3<FStrategyWaitAndComplete>::Make());
   // r.SetStrategy1(
   //     StrategyMCTS3<FStrategyComplete, FStrategyWaitAndComplete>::Make());
   // r.SetStrategy1(StrategyWSGCUTS::Make());
