35c6a8d7a20f95acecdf96d52dc9cf7cf0fb2505
Loks- <alexeypoyarkov@gmail.com>
2021-05-17 20:47:08 -0700

    Add solution for "Horse-Racing Duals" problem.

---
+++ codingame/practice/cp0/horse_racing_duals.cpp
@@ -0,0 +1,15 @@
+// https://www.codingame.com/training/easy/horse-racing-duals
+
+#include "common/stl/base.h"
+#include "common/vector/read.h"
+
+#include <numeric>
+
+int main_horse_racing_duals() {
+  unsigned N;
+  cin >> N;
+  auto v = nvector::Read<unsigned>(N);
+  sort(v.begin(), v.end());
+  adjacent_difference(v.begin(), v.end(), v.begin());
+  cout << *min_element(v.begin() + 1, v.end()) << endl;
+}
