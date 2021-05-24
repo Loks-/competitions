c836952bc2d33c38a449c0ab5dd3fa4a9758cb64
Loks- <alexeypoyarkov@gmail.com>
2021-05-24 10:12:04 -0700

    New implementation for Goldberg-Radzik algorithm.

--- common/graph/graph_ei/distance/spfa/goldberg_radzik.h
+++ common/graph/graph_ei/distance/spfa/goldberg_radzik.h
@@ -19,26 +19,45 @@ inline std::vector<TEdgeCost> GoldbergRadzik(const TGraph& graph,
   std::vector<TEdgeCost> v(gsize, max_cost);
   v[source] = TEdgeCost();
   std::stack<unsigned> sa, sb, st;
-  // 0 - untouched, 2 - in a, 3 - in b, 4 - in b + checked
+  // 0 - untouched, 2 - in a, 3 - in b, 4 - recursive call
   std::vector<unsigned> m(gsize, 0);
   sb.push(source);
   m[source] = 3;
-  for (unsigned i = 0; !sb.empty(); ++i) {
+  for (; !sb.empty();) {
     for (; !sb.empty();) {
       unsigned u = sb.top();
-      if (m[u] == 3) {
-        m[u] = 4;
-        auto ucost = v[u];
-        for (const auto& e : graph.EdgesEI(u)) {
-          if (m[e.to] != 3) continue;
-          if (ucost + f(e.info) <= v[e.to]) sb.push(e.to);
-        }
-      } else {
-        sb.pop();
-        if (m[u] == 4) {
-          m[u] = 2;
+      auto ucost = v[u];
+      switch (m[u]) {
+        case 0:
+        case 2:
+          sb.pop();
+          break;
+        case 3:
+          m[u] = 0;
+          for (const auto& e : graph.EdgesEI(u)) {
+            if (ucost + f(e.info) < v[e.to]) {
+              m[u] = 4;  // Need to add to a.
+              break;
+            }
+          }
+          break;
+        case 4:
+          m[u] = 5;
+          for (const auto& e : graph.EdgesEI(u)) {
+            if (m[e.to] == 2) continue;  // Already in a.
+            if (m[e.to] == 5) continue;  // Already in processing
+            if (ucost + f(e.info) <= v[e.to]) {
+              // if (m[e.to] == 5) return {};  // Negative loop
+              m[e.to] = 4;
+              sb.push(e.to);
+            }
+          }
+          break;
+        case 5:
+          sb.pop();
           sa.push(u);
-        }
+          m[u] = 2;
+          break;
       }
     }
     for (; !sa.empty();) {
