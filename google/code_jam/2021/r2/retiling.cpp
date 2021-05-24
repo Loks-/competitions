36e868466aed8726f91ae7f22b5e15a8a9efe4f4
Loks- <alexeypoyarkov@gmail.com>
2021-05-23 20:43:31 -0700

    Simlify code.

--- google/code_jam/2021/r2/retiling.cpp
+++ google/code_jam/2021/r2/retiling.cpp
@@ -15,27 +15,22 @@ int main_retiling() {
     auto vs = nvector::Read<string>(R);
     auto ve = nvector::Read<string>(R);
     std::vector<int> vr;
-    for (unsigned ic = 0; ic < 2; ++ic) {
-      char c = (ic == 0) ? 'M' : 'G';
-      vector<I2Point> vps, vpe;
-      for (unsigned i = 0; i < R; ++i) {
-        for (unsigned j = 0; j < C; ++j) {
-          if (vs[i][j] == c) vps.push_back({i, j});
-          if (ve[i][j] == c) vpe.push_back({i, j});
-        }
+      char c = 'M';
+    vector<I2Point> vps, vpe;
+    for (unsigned i = 0; i < R; ++i) {
+      for (unsigned j = 0; j < C; ++j) {
+        if (vs[i][j] == c) vps.push_back({i, j});
+        if (ve[i][j] == c) vpe.push_back({i, j});
       }
-      if (vps.size() > vpe.size()) vps.swap(vpe);
-      unsigned n = vps.size(), m = vpe.size();
-      la::Matrix<int64_t> a(n, m);
-      for (unsigned i = 0; i < n; ++i) {
-        for (unsigned j = 0; j < m; ++j) 
-          a(i, j) = std::min<int64_t>(2 * F, S * DistanceL1(vps[i], vpe[j]));
-      }
-      auto r = alg::Assignment(a, 1 << 30);
-      // cerr << "A size = " << vps.size() << " " << vpe.size() << " " << r  << endl;
-      vr.push_back(r + F * (m - n));
     }
-    cout << "Case #" << it << ": " << min(vr[0], vr[1]) << endl;
+    if (vps.size() > vpe.size()) vps.swap(vpe);
+    unsigned n = vps.size(), m = vpe.size();
+    la::Matrix<int64_t> a(n, m);
+    for (unsigned i = 0; i < n; ++i) {
+      for (unsigned j = 0; j < m; ++j) 
+        a(i, j) = std::min<int64_t>(2 * F, S * DistanceL1(vps[i], vpe[j]));
+    }
+    cout << "Case #" << it << ": " << alg::Assignment(a, 2 * F + 1) + F * (m - n) << endl;
   }
   return 0;
 }
