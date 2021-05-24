80c188918ba787860d3445c6c387142c19133e1a
Loks- <alexeypoyarkov@gmail.com>
2021-05-24 10:09:03 -0700

    Rename current implementation to GoldbergRadzikLazy.

---
+++ common/graph/graph_ei/distance/spfa/goldberg_radzik_lazy.h
@@ -0,0 +1,63 @@
+#pragma once
+
+#include "common/graph/graph_ei.h"
+
+#include <stack>
+#include <vector>
+
+namespace graph {
+namespace distance {
+namespace spfa {
+// Goldberg-Radzik algorithm with lazy copy from b to a.
+template <class TGraph, class TEdgeCostFunction, class TEdgeCost>
+inline std::vector<TEdgeCost> GoldbergRadzikLazy(const TGraph& graph,
+                                                 const TEdgeCostFunction& f,
+                                                 unsigned source,
+                                                 const TEdgeCost& max_cost) {
+  unsigned gsize = graph.Size();
+  std::vector<TEdgeCost> v(gsize, max_cost);
+  v[source] = TEdgeCost();
+  std::stack<unsigned> sa, sb, st;
+  // 0 - untouched, 2 - in a, 3 - in b, 4 - in b + checked
+  std::vector<unsigned> m(gsize, 0);
+  sb.push(source);
+  m[source] = 3;
+  for (; !sb.empty();) {
+    for (; !sb.empty();) {
+      unsigned u = sb.top();
+      if (m[u] == 3) {
+        m[u] = 4;
+        auto ucost = v[u];
+        for (const auto& e : graph.EdgesEI(u)) {
+          if (m[e.to] != 3) continue;
+          if (ucost + f(e.info) <= v[e.to]) sb.push(e.to);
+        }
+      } else {
+        sb.pop();
+        if (m[u] == 4) {
+          m[u] = 2;
+          sa.push(u);
+        }
+      }
+    }
+    for (; !sa.empty();) {
+      unsigned u = sa.top();
+      sa.pop();
+      auto ucost = v[u];
+      for (const auto& e : graph.EdgesEI(u)) {
+        if (ucost + f(e.info) < v[e.to]) {
+          v[e.to] = ucost + f(e.info);
+          if (m[e.to] == 0) {
+            sb.push(e.to);
+            m[e.to] = 3;
+          }
+        }
+      }
+      m[u] = 0;
+    }
+  }
+  return v;
+}
+}  // namespace spfa
+}  // namespace distance
+}  // namespace graph
