6da847871ce9f2d25600db88a04a269f1d56d850
Loks- <alexeypoyarkov@gmail.com>
2021-05-25 00:16:12 -0700

    Simplify solution.

--- google/code_jam/2021/r2/retiling__mcf.cpp
+++ google/code_jam/2021/r2/retiling__mcf.cpp
@@ -16,30 +16,25 @@ int main_retiling__mcf() {
     auto vs = nvector::Read<string>(R);
     auto ve = nvector::Read<string>(R);
     std::vector<int> vr;
-    for (unsigned ic = 0; ic < 2; ++ic) {
-      char c = (ic == 0) ? 'M' : 'G';
-      vector<I2Point> vps, vpe;
-      for (unsigned i = 0; i < R; ++i) {
-        for (unsigned j = 0; j < C; ++j) {
-          if (vs[i][j] == c) vps.push_back({i, j});
-          if (ve[i][j] == c) vpe.push_back({i, j});
-        }
+    char c = 'M';
+    vector<I2Point> vps, vpe;
+    for (unsigned i = 0; i < R; ++i) {
+      for (unsigned j = 0; j < C; ++j) {
+        if (vs[i][j] == c) vps.push_back({i, j});
+        if (ve[i][j] == c) vpe.push_back({i, j});
       }
-      if (vps.size() > vpe.size()) vps.swap(vpe);
-      unsigned n = vps.size(), m = vpe.size(), source = n + m, sink = n + m + 1;
-      FlowGraph<graph::flow::Edge<int, int>> g(n + m + 2, source, sink);
-      for (unsigned i = 0; i < n; ++i) {
-        g.AddDataEdge(0, source, i, 1);
-        for (unsigned j = 0; j < m; ++j)
-          g.AddDataEdge(min<int>(2 * F, S * DistanceL1(vps[i], vpe[j])), i,
-                        j + n, 1);
-      }
-      for (unsigned j = 0; j < m; ++j) g.AddDataEdge(0, j + n, sink, 1);
-      auto r = MinCostFlow(g, n) + F * (m - n);
-      assert(g.Flow() == int(n));
-      vr.push_back(r);
     }
-    cout << "Case #" << it << ": " << min(vr[0], vr[1]) << endl;
+    if (vps.size() > vpe.size()) vps.swap(vpe);
+    unsigned n = vps.size(), m = vpe.size(), source = n + m, sink = n + m + 1;
+    FlowGraph<graph::flow::Edge<int, int>> g(n + m + 2, source, sink);
+    for (unsigned i = 0; i < n; ++i) {
+      g.AddDataEdge(0, source, i, 1);
+      for (unsigned j = 0; j < m; ++j)
+        g.AddDataEdge(min<int>(2 * F, S * DistanceL1(vps[i], vpe[j])), i, j + n,
+                      1);
+    }
+    for (unsigned j = 0; j < m; ++j) g.AddDataEdge(0, j + n, sink, 1);
+    cout << "Case #" << it << ": " << MinCostFlow(g, n) + F * (m - n) << endl;
   }
   return 0;
 }
