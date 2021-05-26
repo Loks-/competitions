1c7a52dccd19f915de250128a92ffd86297a64c9
Loks- <alexeypoyarkov@gmail.com>
2021-05-26 16:44:30 -0700

    Add DistanceAllPairsPositiveCost.

---
+++ common/graph/graph_ei/distance_all_pairs_positive_cost.h
@@ -0,0 +1,28 @@
+#pragma once
+
+#include "common/graph/graph_ei.h"
+#include "common/graph/graph_ei/distance/dijkstra.h"
+#include "common/graph/graph_ei/edge_cost_proxy.h"
+
+#include <vector>
+
+namespace graph {
+template <class TGraph, class TEdgeCostFunction, class TEdgeCost>
+inline std::vector<std::vector<TEdgeCost>> DistanceAllPairsPositiveCost(
+    const TGraph& graph, const TEdgeCostFunction& f,
+    const TEdgeCost& max_cost) {
+  unsigned N = graph.Size();
+  std::vector<std::vector<TEdgeCost>> vd;
+  vd.reserve(N);
+  for (unsigned i =0 ; i < N; ++i)
+    vd.emplace_back(distance::Dijkstra(graph, f, i, max_cost));
+  return vd;
+}
+}  // namespace graph
+
+template <class TEdgeInfo, bool directed_edges>
+inline std::vector<std::vector<TEdgeInfo>> DistanceAllPairsPositiveCost(
+    const graph::GraphEI<TEdgeInfo, directed_edges>& g, TEdgeInfo max_cost) {
+  return graph::DistanceAllPairsPositiveCost(g, graph::EdgeCostProxy<TEdgeInfo>(),
+                                 max_cost);
+}
