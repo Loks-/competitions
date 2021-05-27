10213f95902904e78ce04f6594457b16e8d35c62
Loks- <alexeypoyarkov@gmail.com>
2021-05-26 21:40:45 -0700

    Simplify algorithm.

--- common/graph/graph_ei/negative_cycle/spfa.h
+++ common/graph/graph_ei/negative_cycle/spfa.h
@@ -14,19 +14,14 @@ template <class TGraph, class TEdgeCostFunction, class TEdgeCost>
 inline std::vector<unsigned> SPFA(const TGraph& graph,
                                   const TEdgeCostFunction& f, unsigned source,
                                   const TEdgeCost& max_cost) {
-  unsigned gsize = graph.Size();
+  unsigned gsize = graph.Size(), u0 = CNone;
   std::vector<TEdgeCost> v(gsize, max_cost);
-  std::vector<unsigned> p(gsize, CNone);
+  std::vector<unsigned> p(gsize, CNone), l(gsize, 0);
   v[source] = TEdgeCost();
   std::queue<unsigned> q;
   std::vector<unsigned> inq(gsize, 0);
   q.push(source);
-  for (unsigned i = 0, ie = 0, r = 0; !q.empty(); ++i) {
-    if (i == ie) {
-      ie = i + q.size();
-      ++r;
-      if (r > gsize) break;
-    }
+  for (; !q.empty();) {
     unsigned u = q.front();
     q.pop();
     inq[u] = 0;
@@ -35,6 +30,11 @@ inline std::vector<unsigned> SPFA(const TGraph& graph,
       if (ucost + f(e.info) < v[e.to]) {
         v[e.to] = ucost + f(e.info);
         p[e.to] = u;
+        l[e.to] = l[u] + 1;
+        if (l[e.to] >= gsize) {
+          u0 = e.to;
+          break;
+        }
         if (!inq[e.to]) {
           inq[e.to] = 1;
           q.push(e.to);
@@ -42,8 +42,7 @@ inline std::vector<unsigned> SPFA(const TGraph& graph,
       }
     }
   }
-  if (q.empty()) return {};
-  auto u0 = q.front();
+  if (u0 == CNone) return {};
   std::vector<unsigned> r(1, u0);
   for (auto u = p[u0]; u != u0; u = p[u]) r.push_back(u);
   std::reverse(r.begin(), r.end());
