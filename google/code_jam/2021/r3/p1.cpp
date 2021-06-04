b7e4a17663ea2db906016b8916db341b5006b8f6
Loks- <alexeypoyarkov@gmail.com>
2021-06-04 14:31:49 -0700

    Initial files for R3.

---
+++ google/code_jam/2021/r3/p1.cpp
@@ -0,0 +1,10 @@
+#include "common/stl/full.h"
+
+int main_p1() {
+  unsigned T;
+  cin >> T;
+  for (unsigned it = 1; it <= T; ++it) {
+    cout << "Case #" << it << ": " << endl;
+  }
+  return 0;
+}
