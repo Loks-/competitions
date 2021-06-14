b0f3554e809796f326e6cb2432783db7b309a5bc
Loks- <alexeypoyarkov@gmail.com>
2021-06-14 11:50:42 -0700

    Use rolling bucket queue with double linked list in Dial algorithm.

--- common/graph/graph_ei/distance/dial.h
+++ common/graph/graph_ei/distance/dial.h
@@ -1,6 +1,6 @@
 #pragma once
 
-#include "common/heap/monotone/base/rolling_bucket_queue.h"
+#include "common/heap/monotone/ukvm/rolling_bucket_queue_dll.h"
 
 #include <vector>
 
@@ -9,33 +9,22 @@ namespace distance {
 // Dial's algorithm with Monotone Bucket Heap.
 // https://en.wikipedia.org/wiki/Dijkstra%27s_algorithm
 // All edges cost are not greater than max_edge_cost (C).
-// Time: O(V + E + D), Memory O(E + C).
+// Time: O(V + E + D), Memory O(V + C).
 // D = O(EC) is max distance
 template <class TGraph, class TEdgeCostFunction, class TEdgeCost>
 inline std::vector<TEdgeCost> Dial(const TGraph& graph,
                                    const TEdgeCostFunction& f, unsigned source,
                                    const TEdgeCost& max_cost,
                                    const TEdgeCost& max_edge_cost) {
-  std::vector<TEdgeCost> vd(graph.Size(), max_cost);
-  std::vector<unsigned> visited(graph.Size(), 0);
-  heap::monotone::base::RollingBucketQueue<unsigned> q(max_edge_cost + 1);
-  vd[source] = TEdgeCost();
-  for (q.Add(TEdgeCost(), source); !q.Empty();) {
-    auto p = q.Extract();
-    unsigned u = p.value;
-    if (!visited[u]) {
-      visited[u] = true;
-      for (auto e : graph.EdgesEI(u)) {
-        unsigned v = e.to;
-        TEdgeCost cost = p.priority + f(e.info);
-        if (cost < vd[v]) {
-          vd[v] = cost;
-          q.Add(cost, v);
-        }
-      }
-    }
+  heap::monotone::ukvm::RollingBucketQueueDLL q(
+      std::vector<TEdgeCost>(graph.Size(), max_cost), true, max_edge_cost + 1);
+  for (q.AddNewKey(source, TEdgeCost()); !q.Empty();) {
+    unsigned u = q.ExtractKey();
+    TEdgeCost ucost = q.Get(u);
+    for (auto e : graph.EdgesEI(u))
+      q.DecreaseValueIfLess(e.to, ucost + f(e.info));
   }
-  return vd;
+  return q.GetValues();
 }
 }  // namespace distance
 }  // namespace graph
