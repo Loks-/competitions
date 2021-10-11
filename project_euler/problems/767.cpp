5ef88793e84a095c38a7170e4c6f9287a554d7d9
Loks- <alexeypoyarkov@gmail.com>
2021-10-11 12:00:58 -0700

    Simplify code.

--- project_euler/problems/767.cpp
+++ project_euler/problems/767.cpp
@@ -6,18 +6,17 @@
 // Time: O(K^2 * log(H + K * N))
 // Memory: O(K)
 int main_767() {
+  // uint64_t H = 16, K = 3, N = 9;
   uint64_t H = 16, K = PowU(10ull, 5), N = PowU(10ull, 16);
   assert((N % K) == 0);
-  TModularD m2 = 2, s = 0;
+  TModularD m2 = 2, m2a = m2.PowU(N / K) - m2, s = 0;
   modular::mstatic::Factorial<TModularD> f;
   vector<TModularD> va(K + 1);
   for (unsigned l = 0; l <= K; ++l) {
     if ((l & (l - 1)) == 0) cout << "\tl = " << l << endl;
     for (unsigned i = 0; i <= l; ++i)
       va[l] += f.BinomialCoefficient(l, i).PowU(H);
-    for (unsigned i = 0; i < l; ++i)
-      va[l] -= va[i] * f.BinomialCoefficient(l, i) * m2.PowU(l - i);
-    s += va[l] * f.BinomialCoefficient(K, l) * m2.PowU((K - l) * (N / K));
+    s += va[l] * f.BinomialCoefficient(K, l) * m2a.PowU(K - l);
   }
   cout << s << endl;
   return 0;
