e4f9d1a5ba270ec13496d640e184fd384432d73e
Loks- <alexeypoyarkov@gmail.com>
2021-10-25 09:40:01 -0700

    Add bits rotate functions.

---
+++ common/numeric/bits/rotate.h
@@ -0,0 +1,28 @@
+#pragma once
+
+#include "common/base.h"
+
+namespace numeric {
+inline uint64_t RotateBitsR(uint64_t x, unsigned shift) {
+  shift &= 63;
+  if (shift) x = ((((1ull << shift) - 1) & x) << (64 - shift)) | (x >> shift);
+  return x;
+}
+
+inline uint64_t RotateBitsL(uint64_t x, unsigned shift) {
+  return RotateBitsR(x, 64 - shift & 63);
+}
+
+inline uint64_t RotateBitsR(uint64_t x, unsigned shift, unsigned length) {
+  shift %= length;
+  uint64_t tail = x & ((1ull << length) - 1);
+  if (shift)
+    x = ((((1ull << shift) - 1) & tail) << (length - shift)) | (tail >> shift) |
+        (x ^ tail);
+  return x;
+}
+
+inline uint64_t RotateBitsL(uint64_t x, unsigned shift, unsigned length) {
+  return RotateBitsL(x, length - (shift % length), length);
+}
+}  // namespace numeric
