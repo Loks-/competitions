6563a4a7187cf90c33f6f80a489bc7eb15cad918
Loks- <alexeypoyarkov@gmail.com>
2021-12-10 13:03:27 -0800

    Refactor solution for problem 2020 10.

--- advent_of_code/2021/src/2110a.cpp
+++ advent_of_code/2021/src/2110a.cpp
@@ -3,35 +3,28 @@
 
 #include <unordered_map>
 
-int main_2110b() {
+int main_2110a() {
   string ss = "([{<)]}>";
   unordered_map<char, int> um;
   for (int i = 0; i < 8; ++i) um[ss[i]] = i;
 
-  vector<int64_t> v;
+  vector<int64_t> v{3, 57, 1197, 25137};
   auto vs = nvector::ReadLines();
+  int64_t r = 0;
   for (auto& s : vs) {
     string snew;
-    bool skip = false;
     for (char c : s) {
       int ci = um[c];
       if (ci < 4) {
         snew += c;
       } else if (snew.empty() || (snew.back() != ss[ci - 4])) {
-        skip = true;
+        r += v[ci - 4];
         break;
       } else {
         snew.pop_back();
       }
     }
-    if (!skip) {
-      reverse(snew.begin(), snew.end());
-      int64_t x = 0;
-      for (char c : snew) x = 5 * x + um[c] + 1;
-      v.push_back(x);
-    }
   }
-  sort(v.begin(), v.end());
-  cout << v[v.size() / 2] << endl;
+  cout << r << endl;
   return 0;
 }
