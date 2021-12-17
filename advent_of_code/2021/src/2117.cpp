6d1e8048115d92e8a8b8f6d0a0d3a8708f9f18de
Loks- <alexeypoyarkov@gmail.com>
2021-12-17 08:32:32 -0800

    Simplify code.

--- advent_of_code/2021/src/2117.cpp
+++ advent_of_code/2021/src/2117.cpp
@@ -4,9 +4,8 @@
 #include "common/stl/pair.h"
 #include "common/string/utils/split.h"
 #include "common/vector/read_lines.h"
-#include "common/vector/unique.h"
 
-int main_2117b() {
+int main_2117() {
   auto s = nvector::ReadLines()[0];
   auto ss = Split(s, ' ');
   auto sx = ss[2].substr(2, ss[2].size() - 3), sy = ss[3].substr(2);
@@ -14,14 +13,14 @@ int main_2117b() {
   int64_t x1 = stoi(ssx[0]), x2 = stoi(ssx[2]), y1 = stoi(ssy[0]),
           y2 = stoi(ssy[2]);
   I2ARectangle box({x1, y1}, {x2, y2});
-  vector<pair<int64_t, int64_t>> vp;
   int64_t maxy = y1 - 1;
+  unsigned r = 0;
   for (int64_t dx = 0; dx <= x2; ++dx) {
     for (int64_t dy = y1; dy <= -y1; ++dy) {
       I2Vector v(dx, dy);
       for (I2Point p(dx, dy); (p.x <= x2) && (p.y >= y1); p += v) {
         if (box.Inside(p)) {
-          vp.push_back({dx, dy});
+          ++r;
           maxy = max(maxy, dy);
           break;
         }
@@ -31,7 +30,6 @@ int main_2117b() {
     }
   }
   cout << y1 * (y1 + 1) / 2 << endl;
-  nvector::UniqueS(vp);
-  cout << vp.size() << endl;
+  cout << r << endl;
   return 0;
 }
