cb60c5088c434cc98f32147e9be0dcacec1536e4
Loks- <alexeypoyarkov@gmail.com>
2021-12-19 21:53:35 -0800

    Add solution and input for problem 20 2021.

--- common/vector/sum.h
+++ common/vector/sum.h
@@ -8,4 +8,11 @@ template <class T>
 inline T Sum(const std::vector<T>& v) {
   return std::accumulate(v.begin(), v.end(), T());
 }
+
+template <class T>
+inline T SumVV(const std::vector<std::vector<T>>& v) {
+  T s = 0;
+  for (auto& iv : v) s += Sum(iv);
+  return s;
+}
 }  // namespace nvector
