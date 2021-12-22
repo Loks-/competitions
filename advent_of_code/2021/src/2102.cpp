53143ac16aafae7f598cdd513822db6954c31ac2
Loks- <alexeypoyarkov@gmail.com>
2021-12-22 13:59:28 -0800

    Merge ab for 2102 and add tests.

--- advent_of_code/2021/src/2102.cpp
+++ advent_of_code/2021/src/2102.cpp
@@ -2,7 +2,7 @@
 #include "common/stl/pair_io.h"
 #include "common/vector/read_all.h"
 
-int main_2102b() {
+int main_2102() {
   int64_t x = 0, y = 0, a = 0;
   for (auto& p : nvector::ReadAll<pair<string, int>>()) {
     if (p.first == "forward") {
@@ -12,6 +12,7 @@ int main_2102b() {
     if (p.first == "down") a += p.second;
     if (p.first == "up") a -= p.second;
   }
+  cout << x * a << endl;
   cout << x * y << endl;
   return 0;
 }
