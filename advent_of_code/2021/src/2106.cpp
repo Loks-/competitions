b95e22e93f0e9368dbe9ece87d8108dd470a0ba0
Loks- <alexeypoyarkov@gmail.com>
2021-12-24 01:09:34 -0800

    Replace input parsing.

--- advent_of_code/2021/src/2106.cpp
+++ advent_of_code/2021/src/2106.cpp
@@ -1,14 +1,12 @@
 #include "common/stl/base.h"
-#include "common/string/utils/split.h"
+#include "common/vector/read_from_line.h"
 #include "common/vector/rotate.h"
 #include "common/vector/sum.h"
 
 int main_2106() {
   unsigned ma = 80, mb = 256;
   vector<uint64_t> vc(9, 0);
-  string s;
-  cin >> s;
-  for (auto ss : Split(s, ',')) vc[stoi(ss)] += 1;
+  for (auto x : nvector::ReadFromLine<unsigned>(',')) vc[x] += 1;
   for (unsigned im = 1; im <= mb; ++im) {
     nvector::RotateLeft(vc, 1);
     vc[6] += vc[8];
