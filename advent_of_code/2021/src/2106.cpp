e94c12f0f0e7ae0f5ded72fedc6951c62c93c0a0
Loks- <alexeypoyarkov@gmail.com>
2021-12-24 01:06:10 -0800

    Merge 2106 ab to one.

--- advent_of_code/2021/src/2106.cpp
+++ advent_of_code/2021/src/2106.cpp
@@ -3,16 +3,16 @@
 #include "common/vector/rotate.h"
 #include "common/vector/sum.h"
 
-int main_2106b() {
-  unsigned m = 256;
+int main_2106() {
+  unsigned ma = 80, mb = 256;
   vector<uint64_t> vc(9, 0);
   string s;
   cin >> s;
   for (auto ss : Split(s, ',')) vc[stoi(ss)] += 1;
-  for (unsigned im = 0; im < m; ++im) {
+  for (unsigned im = 1; im <= mb; ++im) {
     nvector::RotateLeft(vc, 1);
     vc[6] += vc[8];
+    if ((im == ma) || (im == mb)) cout << nvector::Sum(vc) << endl;
   }
-  cout << nvector::Sum(vc) << endl;
   return 0;
 }
