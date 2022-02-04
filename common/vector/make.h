2cd16a203001fe0fc4b4c9b306658361c2129793
Loks- <alexeypoyarkov@gmail.com>
2022-02-03 17:03:56 -0800

    Add DVector alias.

--- common/vector/make.h
+++ common/vector/make.h
@@ -3,19 +3,38 @@
 #include <vector>
 
 namespace nvector {
+namespace hidden {
+template <class T, unsigned d>
+class HDVector;
+
 template <class T>
-inline auto Make(unsigned d1, const T& value) {
-  return std::vector<T>(d1, value);
+class HDVector<T, 0u> {
+ public:
+  using TType = T;
+};
+
+template <class T, unsigned d>
+class HDVector {
+ public:
+  using TType = std::vector<typename HDVector<T, d - 1>::TType>;
+};
+}  // namespace hidden
+
+template <class T, unsigned d>
+using DVector = typename hidden::HDVector<T, d>::TType;
+
+template <class T>
+inline auto Make(unsigned d0, const T& value) {
+  return DVector<T, 1>(d0, value);
 }
 
 template <class T>
-inline auto Make(unsigned d1, unsigned d2, const T& value) {
-  return std::vector<decltype(Make<T>(d2, value))>(d1, Make<T>(d2, value));
+inline auto Make(unsigned d0, unsigned d1, const T& value) {
+  return DVector<T, 2>(d0, Make<T>(d1, value));
 }
 
 template <class T>
-inline auto Make(unsigned d1, unsigned d2, unsigned d3, const T& value) {
-  return std::vector<decltype(Make<T>(d2, d3, value))>(d1,
-                                                       Make<T>(d2, d3, value));
+inline auto Make(unsigned d0, unsigned d1, unsigned d2, const T& value) {
+  return DVector<T, 3>(d0, Make<T>(d1, d2, value));
 }
 }  // namespace nvector
