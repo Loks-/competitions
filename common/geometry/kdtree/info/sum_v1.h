c3e2d9e3cf6d71ffa14f65ca888e5a75bfe9d81c
Loks- <alexeypoyarkov@gmail.com>
2022-02-04 14:27:04 -0800

    Rename class.

--- common/geometry/kdtree/info/sum_v1.h
+++ common/geometry/kdtree/info/sum_v1.h
@@ -3,33 +3,34 @@
 namespace geometry {
 namespace kdtree {
 namespace info {
+// Assume that all leafs with non-zero value have volume 1.
 template <class TSumType, class TInfo>
-class Sum : public TInfo {
+class SumV1 : public TInfo {
  public:
   using TBase = TInfo;
-  using TSelf = Sum<TSumType, TInfo>;
+  using TSelf = SumV1<TSumType, TInfo>;
 
   static const bool is_none = false;
   static const bool use_data = true;
 
-  TSumType sum;
+  TSumType sum_v1;
 
-  Sum() : sum() {}
+  SumV1() : sum_v1() {}
 
   void Merge(const TSelf& r) {
     TBase::Merge(r);
-    sum += r.sum;
+    sum_v1 += r.sum_v1;
   }
 
   template <class TNode>
   void Update(const TNode* node) {
     TBase::Update(node);
     if (node->IsLeaf()) {
-      sum = node->ldata;
+      sum_v1 = node->ldata;
     } else {
-      sum = node->ldata_in_inode ? node->ldata : TSumType();
-      if (node->l) sum += node->l->info.sum;
-      if (node->r) sum += node->r->info.sum;
+      sum_v1 = node->ldata_in_inode ? node->ldata : TSumType();
+      if (node->l) sum_v1 += node->l->info.sum_v1;
+      if (node->r) sum_v1 += node->r->info.sum_v1;
     }
   }
 };
