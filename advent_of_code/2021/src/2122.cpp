5573675af81885a0c7a0ab259766a191ac622fa5
Loks- <alexeypoyarkov@gmail.com>
2022-03-25 15:39:08 -0700

    Switch solution to use kdtree::BaseTree.

--- advent_of_code/2021/src/2122.cpp
+++ advent_of_code/2021/src/2122.cpp
@@ -1,16 +1,27 @@
 #include "common/data_structures/coordinate_compression.h"
 #include "common/geometry/d3/axis/rectangle.h"
 #include "common/geometry/d3/base.h"
+#include "common/geometry/kdtree/d3/base_tree.h"
 #include "common/stl/base.h"
 #include "common/string/utils/split.h"
-#include "common/vector/make.h"
 #include "common/vector/read_lines.h"
 
-// Time: O(N^4)
+#include <queue>
+
+using TTree = geometry::kdtree::D3BaseTree<size_t, unsigned>;
+using TNode = TTree::TNode;
+using geometry::kdtree::base::DSet;
+
 int main_2122() {
   struct Input {
     unsigned v;
     I3Point p1, p2;
+  };
+
+  struct QNode {
+    TNode* p;
+    TTree::TPoint b;
+    TTree::TPoint e;
   };
 
   const int maxv = 50;
@@ -39,27 +50,26 @@ int main_2122() {
     }
     vector<ds::CoordinateCompression<int64_t>> vcc(3);
     for (unsigned i = 0; i < 3; ++i) vcc[i].InitUnsorted(vv3[i]);
-    auto vvv =
-        nvector::Make<unsigned>(vcc[0].Size(), vcc[1].Size(), vcc[2].Size(), 0);
+    TTree tree;
+    tree.Init({0, 0, 0}, {vcc[0].Size(), vcc[1].Size(), vcc[2].Size()});
     for (auto& s : vi) {
       auto x0 = vcc[0].GetNew(s.p1.x), x1 = vcc[0].GetNew(s.p2.x);
       auto y0 = vcc[1].GetNew(s.p1.y), y1 = vcc[1].GetNew(s.p2.y);
       auto z0 = vcc[2].GetNew(s.p1.z), z1 = vcc[2].GetNew(s.p2.z);
-      for (auto x = x0; x < x1; ++x) {
-        for (auto y = y0; y < y1; ++y) {
-          for (auto z = z0; z < z1; ++z) vvv[x][y][z] = s.v;
-        }
-      }
+      tree.Set({x0, y0, z0}, {x1, y1, z1}, s.v);
     }
     int64_t r = 0;
-    for (unsigned x = 1; x < vcc[0].Size(); ++x) {
-      auto dx = vcc[0].GetOld(x) - vcc[0].GetOld(x - 1);
-      for (unsigned y = 1; y < vcc[1].Size(); ++y) {
-        auto dy = vcc[1].GetOld(y) - vcc[1].GetOld(y - 1);
-        for (unsigned z = 1; z < vcc[2].Size(); ++z) {
-          auto dz = vcc[2].GetOld(z) - vcc[2].GetOld(z - 1);
-          r += vvv[x - 1][y - 1][z - 1] * dx * dy * dz;
-        }
+    queue<QNode> q;
+    for (q.push({tree.root, tree.sb, tree.se}); !q.empty(); q.pop()) {
+      auto p = q.front();
+      if (p.p->IsLeaf()) {
+        int64_t rp = p.p->ldata;
+        for (unsigned i = 0; i < 3; ++i)
+          rp *= vcc[i].GetOld(p.e[i]) - vcc[i].GetOld(p.b[i]);
+        r += rp;
+      } else {
+        q.push({p.p->l, p.b, DSet(p.p->split_dim, p.e, p.p->split_value)});
+        q.push({p.p->r, DSet(p.p->split_dim, p.b, p.p->split_value), p.e});
       }
     }
     cout << r << endl;
