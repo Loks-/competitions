d17bfdbe927b8c4c25fec295b95e915dd56dd39b
Loks- <alexeypoyarkov@gmail.com>
2022-03-26 16:46:51 -0700

    Add input for problems with multiply possible output.

---
+++ google/code_jam/2018/wf/two_tiling.cpp
@@ -0,0 +1,15 @@
+#include "common/stl/base.h"
+
+#include <string>
+
+int main_two_tiling() {
+  unsigned T;
+  cin >> T;
+  for (unsigned it = 1; it <= T; ++it) {
+    // string s;
+    // vector<string> vs;
+    // for (; vs.size() < 3;)
+    cout << "Case #" << it << ": " << 0 << endl;
+  }
+  return 0;
+}
