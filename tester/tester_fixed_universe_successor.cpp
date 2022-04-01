16e0ad0d250b66a4f6286b909dddc086e72af1fd
Loks- <alexeypoyarkov@gmail.com>
2022-04-01 15:24:49 -0700

    Add U=2^60 to tester.

--- tester/tester_fixed_universe_successor.cpp
+++ tester/tester_fixed_universe_successor.cpp
@@ -54,32 +54,38 @@ size_t TesterFixedUniverseSuccessor::TestBase(const std::string& name) const {
 }
 
 bool TesterFixedUniverseSuccessor::TestAll(bool small_test) const {
+  std::cout << "USize = " << usize << std::endl;
   std::unordered_set<size_t> hs;
   if (small_test) {
     hs.insert(TestBase<ds::fus::VectorSet>("VSet  "));
     hs.insert(TestBase<ds::fus::VectorMultiset>("VMSet "));
     hs.insert(TestBase<ds::fus::VectorPrecomputed>("VPreC "));
   }
-  hs.insert(TestBase<ds::fus::TwoLayersU64>("L2U64 "));
-  hs.insert(TestBase<ds::fus::SqrtDecomposition>("SqrtD "));
+  if (usize <= (1ull << 30)) {
+    hs.insert(TestBase<ds::fus::TwoLayersU64>("L2U64 "));
+    hs.insert(TestBase<ds::fus::SqrtDecomposition>("SqrtD "));
+    hs.insert(TestBase<ds::fus::ImplicitBinaryTree>("IBT   "));
+  }
   hs.insert(TestBase<ds::fus::BinarySearchTree>("BST   "));
-  hs.insert(TestBase<ds::fus::ImplicitBinaryTree>("IBT   "));
   hs.insert(TestBase<ds::fus::B64Trie>("B64T  "));
-  if (usize < (1ull << 27)) {
+  if (usize <= (1ull << 27)) {
     hs.insert(TestBase<ds::fus::VanEmdeBoasTreeFull>("VEBTF "));
   }
-  if (small_test) {
+  if (usize <= (1ull << 20)) {
     hs.insert(TestBase<ds::fus::VanEmdeBoasTreeFullStaticSize<20>>("VEBTFS"));
-  } else if (usize < (1ull << 27)) {
+  } else if (usize <= (1ull << 27)) {
     hs.insert(TestBase<ds::fus::VanEmdeBoasTreeFullStaticSize<27>>("VEBTFS"));
   }
   hs.insert(TestBase<ds::fus::VanEmdeBoasTreeCompact>("VEBTC "));
-  if (small_test) {
+  if (usize <= (1ull << 20)) {
     hs.insert(
         TestBase<ds::fus::VanEmdeBoasTreeCompactStaticSize<20>>("VEBTCS"));
-  } else if (usize < (1ull << 27)) {
+  } else if (usize <= (1ull << 27)) {
     hs.insert(
         TestBase<ds::fus::VanEmdeBoasTreeCompactStaticSize<27>>("VEBTCS"));
+  } else {
+    hs.insert(
+        TestBase<ds::fus::VanEmdeBoasTreeCompactStaticSize<64>>("VEBTCS"));
   }
   if (small_test) {
     hs.insert(TestBase<ds::fus::XFastTrie>("XFTrie"));
@@ -88,7 +94,13 @@ bool TesterFixedUniverseSuccessor::TestAll(bool small_test) const {
 }
 
 bool TestFixedUniverseSuccessor(bool time_test) {
-  TesterFixedUniverseSuccessor tfus(time_test ? 100000000 : 10000,
-                                    time_test ? 1000000 : 10);
-  return tfus.TestAll(!time_test);
+  if (time_test) {
+    TesterFixedUniverseSuccessor tfus1(100000000, 1000000);
+    TesterFixedUniverseSuccessor tfus2((1ull << 60), 1000000);
+    // return tfus2.TestAll(false);
+    return tfus1.TestAll(false) && tfus2.TestAll(false);
+  } else {
+    TesterFixedUniverseSuccessor tfus(10000, 1000);
+    return tfus.TestAll(true);
+  }
 }
