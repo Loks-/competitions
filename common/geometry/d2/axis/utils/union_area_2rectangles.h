ea073dfd548d0932b6a759f35fd8291105511ac9
Loks- <alexeypoyarkov@gmail.com>
2022-05-27 15:21:58 -0700

    Add missed inline.

--- common/geometry/d2/axis/utils/union_area_2rectangles.h
+++ common/geometry/d2/axis/utils/union_area_2rectangles.h
@@ -4,7 +4,7 @@
 #include "common/geometry/d2/axis/utils/intersection_area_2rectangles.h"
 
 template <class T>
-T UnionArea(const geometry::d2::axis::Rectangle<T>& r1,
-            const geometry::d2::axis::Rectangle<T>& r2) {
+inline T UnionArea(const geometry::d2::axis::Rectangle<T>& r1,
+                   const geometry::d2::axis::Rectangle<T>& r2) {
   return r1.Area() + r2.Area() - IntersectionArea(r1, r2);
 }
