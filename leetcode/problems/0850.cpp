77117fca995058ed2f586e3d6f2f8121c3be26a5
Loks- <alexeypoyarkov@gmail.com>
2022-05-27 15:40:41 -0700

    Add solution for problem 850.

---
+++ leetcode/problems/0850.cpp
@@ -0,0 +1,30 @@
+// https://leetcode.com/problems/rectangle-area-ii/
+
+#include "leetcode/test.h"
+
+#include "common/geometry/d2/axis/rectangle.h"
+#include "common/geometry/d2/axis/utils/union_area_rectangles.h"
+#include "common/stl/base.h"
+
+namespace {
+class Solution0850 {
+ public:
+  int rectangleArea(const vector<vector<int>>& rectangles) {
+    vector<I2ARectangle> vr;
+    for (auto& v : rectangles) vr.push_back({{v[0], v[1]}, {v[2], v[3]}});
+    return UnionArea(vr) % 1000000007;
+  }
+};
+}  // namespace
+
+int main_0850() {
+  auto Run = [](const vector<vector<int>>& v) {
+    Solution0850 s;
+    return s.rectangleArea(v);
+  };
+
+  vector<vector<vector<int>>> input{{{0, 0, 2, 2}, {1, 0, 2, 3}, {1, 0, 3, 1}},
+                                    {{0, 0, 1000000000, 1000000000}}};
+  vector<int> output{6, 49};
+  return TestAll(Run, input, output) ? 0 : -1;
+}
