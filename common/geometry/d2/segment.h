1b49249c4352c5f8380ce981d3c75840536c351e
Loks- <alexeypoyarkov@gmail.com>
2022-06-10 19:35:47 -0700

    Add Flip.

--- common/geometry/d2/segment.h
+++ common/geometry/d2/segment.h
@@ -7,13 +7,17 @@ namespace d2 {
 template <class T, bool closed>
 class Segment {
  public:
+  using TSelf = Segment<T, closed>;
+
   Point<T> p1, p2;
 
  public:
   Segment(const Point<T>& _p1, const Point<T>& _p2) : p1(_p1), p2(_p2) {}
 
   bool Empty() const { return p1 == p2; }
-  bool operator==(const Segment<T, closed>& r) const { return (p1 == r.p1) && (p2 == r.p2); }
+  bool operator==(const TSelf& r) const { return (p1 == r.p1) && (p2 == r.p2); }
+
+  TSelf Flip() const { return {p2, p1}; }
 };
 }  // namespace d2
 }  // namespace geometry
