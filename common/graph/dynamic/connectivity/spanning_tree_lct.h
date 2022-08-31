9d04cce1e98c101fa43dc6d380c71fe2ba0d1938
Loks- <alexeypoyarkov@gmail.com>
2022-08-31 09:30:37 -0700

    Simplify code.

--- common/graph/dynamic/connectivity/spanning_tree_lct.h
+++ common/graph/dynamic/connectivity/spanning_tree_lct.h
@@ -5,11 +5,9 @@
 #include "common/data_structures/unsigned_set.h"
 #include "common/graph/dynamic/graph.h"
 #include "common/graph/tree/link_cut_tree.h"
-#include "common/template.h"
 
 #include <algorithm>
 #include <stack>
-#include <unordered_set>
 
 namespace graph {
 namespace dynamic {
@@ -17,7 +15,7 @@ namespace connectivity {
 // Keep spannig tree based on LinkCutTree.
 class SpanningTreeLCT {
  public:
-  using TGraph = graph::dynamic::Graph<TEmpty>;
+  using TGraph = graph::dynamic::Graph<bool>;
   using TEdge = typename TGraph::TEdge;
   using TEdgeID = TEdge*;
   using TLCT = graph::LinkCutTree<TEmpty, bst::info::None>;
@@ -27,7 +25,6 @@ class SpanningTreeLCT {
   TGraph g;
   TLCT lct;
   unsigned ncomponents;
-  std::unordered_set<TEdge*> lct_edges;
   ds::UnsignedSet uset;
   std::stack<unsigned> s;
 
@@ -42,21 +39,19 @@ class SpanningTreeLCT {
   }
 
   TEdgeID InsertEdge(unsigned u1, unsigned u2) {
-    auto e = g.AddEdge(u1, u2);
+    auto e = g.AddEdge(u1, u2, false);
     auto n1 = Node(u1), n2 = Node(u2);
     if (!SameTree(n1, n2)) {
       --ncomponents;
       lct.SetRoot(n1);
       lct.Link(n1, n2);
-      lct_edges.insert(e);
+      e->data = true;
     }
     return e;
   }
 
   void RemoveEdge(TEdgeID edge) {
-    auto it = lct_edges.find(edge);
-    if (it != lct_edges.end()) {
-      lct_edges.erase(it);
+    if (edge->data) {
       auto u1 = edge->u1, u2 = edge->u2;
       auto n1 = Node(u1), n2 = Node(u2);
       g.DeleteEdge(edge);
@@ -80,7 +75,7 @@ class SpanningTreeLCT {
             auto nu = Node(u);
             lct.SetRoot(nu);
             lct.Link(nu, Node(v));
-            lct_edges.insert(e);
+            e->data = true;
             break;
           } else {
             uset.Insert(v);
