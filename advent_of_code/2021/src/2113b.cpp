8fe648559a44bb95c8c1cdadf06c39fbe81e0f01
Loks- <alexeypoyarkov@gmail.com>
2022-12-03 21:47:00 -0800

    Small cleaning

--- advent_of_code/2021/src/2113b.cpp
+++ advent_of_code/2021/src/2113b.cpp
@@ -3,6 +3,7 @@
 #include "common/stl/base.h"
 #include "common/string/utils/split.h"
 #include "common/vector/make.h"
+#include "common/vector/read_from_string.h"
 #include "common/vector/read_lines.h"
 
 int main_2113b() {
@@ -11,12 +12,12 @@ int main_2113b() {
   vector<I2Point> vp;
   for (i = 0; i < vs.size(); ++i) {
     if (vs[i].empty()) break;
-    auto ss = Split(vs[i], ',');
-    vp.push_back({stoi(ss[0]), stoi(ss[1])});
+    auto v = nvector::ReadFromString<int>(vs[i], ",");
+    vp.push_back({v[0], v[1]});
   }
   for (++i; i < vs.size(); ++i) {
-    auto ss = Split(vs[i], ' ');
-    int l = stoi(ss[2].substr(2));
+    auto ss = Split(vs[i], " =");
+    int l = stoi(ss[3]);
     for (auto& p : vp) {
       auto& z = ((ss[2][0] == 'x') ? p.x : p.y);
       if (z > l) z = 2 * l - z;
