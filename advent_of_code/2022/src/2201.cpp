3e9df425c145a41883705d3b965df3aa5754f3c2
Loks- <alexeypoyarkov@gmail.com>
2022-12-04 21:47:45 -0800

    Simplify solution for 2201.

--- advent_of_code/2022/src/2201.cpp
+++ advent_of_code/2022/src/2201.cpp
@@ -1,21 +1,17 @@
 #include "common/stl/base.h"
 #include "common/vector/read_lines.h"
+#include "common/vector/split_empty_string.h"
 
 #include <numeric>
 
 int main_2201() {
   auto vs = nvector::ReadLines();
-  int current = 0;
+  auto vvs = nvector::SplitEmptyString(vs);
   std::vector<int> v;
-  for (auto& s : vs) {
-    if (s.empty()) {
-      v.push_back(current);
-      current = 0;
-    } else {
-      current += stoi(s);
-    }
+  for (auto& vsi : vvs) {
+    v.push_back(0);
+    for (auto& s : vsi) v.back() += stoi(s);
   }
-  v.push_back(current);
   std::sort(v.begin(), v.end());
   for (auto l : {1u, 3u})
     cout << std::accumulate(v.rbegin(), v.rbegin() + l, 0ll) << endl;
---
+++ common/vector/split_empty_string.h
@@ -0,0 +1,19 @@
+#pragma once
+
+#include <string>
+#include <vector>
+
+namespace nvector {
+std::vector<std::vector<std::string>> SplitEmptyString(
+    const std::vector<std::string>& vs) {
+  std::vector<std::vector<std::string>> vvs(vs.empty() ? 0 : 1);
+  for (auto& s : vs) {
+    if (s.empty()) {
+      vvs.push_back({});
+    } else {
+      vvs.back().push_back(s);
+    }
+  }
+  return vvs;
+}
+}  // namespace nvector
