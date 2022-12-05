b92b2a19ae784e6ad3384630c7e62079cf17bd66
Loks- <alexeypoyarkov@gmail.com>
2022-12-04 21:35:41 -0800

    Add solution and tests for 2205.

---
+++ advent_of_code/2022/src/2205.cpp
@@ -0,0 +1,34 @@
+#include "common/stl/base.h"
+#include "common/string/utils/split.h"
+#include "common/vector/read.h"
+#include "common/vector/read_lines.h"
+
+int main_2205() {
+  auto vs = nvector::ReadLines();
+  unsigned l = 0;
+  for (; !vs[l].empty();) ++l;
+  auto vi = Split(vs[l - 1], " ");
+  for (bool ab : {false, true}) {
+    vector<vector<char>> vv(vi.size() + 1);
+    for (unsigned i = l - 1; i--;) {
+      auto s = vs[i];
+      s.resize(4 * vv.size(), ' ');
+      for (unsigned j = 1; j < vv.size(); ++j) {
+        if (s[4 * j - 3] != ' ') vv[j].push_back(s[4 * j - 3]);
+      }
+    }
+    for (unsigned i = l + 1; i < vs.size(); ++i) {
+      auto ss = Split(vs[i], " ");
+      unsigned n = stoi(ss[1]), f = stoi(ss[3]), t = stoi(ss[5]);
+      if ((vv[f].size() < n) || (f == t)) cout << "X" << endl;
+      if (ab)
+        vv[t].insert(vv[t].end(), vv[f].end() - n, vv[f].end());
+      else
+        vv[t].insert(vv[t].end(), vv[f].rbegin(), vv[f].rbegin() + n);
+      vv[f].resize(vv[f].size() - n);
+    }
+    for (unsigned i = 1; i < vv.size(); ++i) cout << vv[i].back();
+    cout << endl;
+  }
+  return 0;
+}
