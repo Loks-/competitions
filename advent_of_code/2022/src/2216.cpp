714727903fe99ec1c9f0b9c9de6d778c24c9e7c4
Loks- <alexeypoyarkov@gmail.com>
2022-12-16 12:04:45 -0800

    Drop time from recursion.

--- advent_of_code/2022/src/2216.cpp
+++ advent_of_code/2022/src/2216.cpp
@@ -14,7 +14,7 @@ int main_2216() {
     unsigned index;
     string name;
     vector<string> dir;
-    int64_t flow;
+    unsigned flow;
   };
 
   vector<Valve> vv;
@@ -31,9 +31,11 @@ int main_2216() {
   sort(vv.begin(), vv.end(), [](auto& l, auto& r) { return l.flow > r.flow; });
   unordered_map<string, unsigned> vm;
   unsigned nbits = 0;
+  unsigned flow_total = 0;
   for (unsigned i = 0; i < vv.size(); ++i) {
     vm[vv[i].name] = i;
     if (vv[i].flow) ++nbits;
+    flow_total += vv[i].flow;
   }
   DirectedGraph g(vv.size());
   for (unsigned i = 0; i < vv.size(); ++i) {
@@ -45,32 +47,46 @@ int main_2216() {
 
   unordered_map<uint64_t, unsigned> cache;
   std::function<unsigned(unsigned, unsigned, uint64_t)> Solve =
-      [&](unsigned time, unsigned index, uint64_t mask) {
+      [&](unsigned closed_flow, unsigned index, uint64_t mask) {
         if (mask == 0) return 0u;
-        auto key = (time << 16) + index + (mask << 32);
+        auto key = index + (mask << 32);
         auto it = cache.find(key);
         if (it != cache.end()) return it->second;
-        unsigned best = 0;
+        unsigned best = 1u << 30;
         for (uint64_t tmask = mask; tmask;) {
           auto i = numeric::FirstBit(tmask) - 1;
           auto b = (1ull << i);
           tmask &= ~b;
-          if (vvd[index][i] + 1 >= time) continue;
-          auto time2 = time - vvd[index][i] - 1;
-          unsigned current = vv[i].flow * time2 + Solve(time2, i, mask & ~b);
-          best = max(best, current);
+          auto dt = vvd[index][i] + 1;
+          unsigned current =
+              closed_flow * dt + Solve(closed_flow - vv[i].flow, i, mask & ~b);
+          best = min(best, current);
         }
         cache[key] = best;
         return best;
       };
 
   auto si = vm["AA"];
-  uint64_t mask = (1ull << nbits) - 1ull;
-  cout << Solve(30, si, (1ull << nbits) - 1ull) << endl;
-  unsigned r2 = 0;
-  for (uint64_t p1 = 0; p1 < (1ull << (nbits - 1)); ++p1) {
-    auto current = Solve(26, si, p1) + Solve(26, si, mask & ~p1);
-    r2 = max(r2, current);
+  vector<unsigned> vflow(1ull << nbits, 0), vs2(1ull << nbits, 0);
+  uint64_t mask = vflow.size() - 1;
+  unsigned r1 = 0, r2 = 0;
+  for (unsigned j = 1; j < vflow.size(); ++j) {
+    auto i = numeric::FirstBit(j) - 1;
+    vflow[j] = vflow[j ^ (1ull << i)] + vv[i].flow;
+    auto s = Solve(vflow[j], si, j);
+    r1 = max(r1, 30 * vflow[j] - s);
+    vs2[j] = 26 * vflow[j] - min(s, 26 * vflow[j]);
+  }
+  cout << r1 << endl;
+  auto vs3 = vs2;
+  for (unsigned j = 0; j < vflow.size(); ++j) {
+    for (uint64_t tj = j; tj;) {
+      auto i = numeric::FirstBit(tj) - 1;
+      auto b = (1ull << i);
+      tj &= ~b;
+      vs3[j] = max(vs3[j], vs3[j ^ b]);
+    }
+    r2 = max(r2, vs3[j] + vs2[mask ^ j]);
   }
   cout << r2 << endl;
   return 0;
