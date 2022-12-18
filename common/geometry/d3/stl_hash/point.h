9815120326b9d911874d20f89837ee4506e1c90f
Loks- <alexeypoyarkov@gmail.com>
2022-12-17 21:56:19 -0800

    Add solution and tests for 2218.

---
+++ common/geometry/d3/stl_hash/point.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#include "common/geometry/d3/point.h"
+#include "common/hash.h"
+
+namespace std {
+template <class T>
+struct hash<geometry::d3::Point<T>> {
+  size_t operator()(const geometry::d3::Point<T>& value) const {
+    return HashCombine(HashCombine(hash<T>{}(value.x), hash<T>{}(value.y)),
+                       hash<T>{}(value.z));
+  }
+};
+}  // namespace std
