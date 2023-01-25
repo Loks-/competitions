c3181ad527d9e529f0311c70fbe8708e66f92470
Loks- <alexeypoyarkov@gmail.com>
2023-01-24 18:34:26 -0800

    Avoid second call.

--- common/optimization/root/bisection.h
+++ common/optimization/root/bisection.h
@@ -8,15 +8,23 @@ template <class TFunction>
 inline double Bisection(TFunction& f, double l, double r, double eps) {
   auto fl = f(l), fr = f(r);
   if ((0 < fl) && (fr < 0)) {
-    return Bisection([&](double x) { return -f(x); }, l, r, eps);
-  }
-  assert((fl <= 0) && (0 <= fr));
-  for (; r - l > eps;) {
-    double m = (l + r) / 2;
-    if (f(m) <= 0) {
-      l = m;
-    } else {
-      r = m;
+    for (; r - l > eps;) {
+      double m = (l + r) / 2;
+      if (f(m) >= 0) {
+        l = m;
+      } else {
+        r = m;
+      }
+    }
+  } else {
+    assert((fl <= 0) && (0 <= fr));
+    for (; r - l > eps;) {
+      double m = (l + r) / 2;
+      if (f(m) <= 0) {
+        l = m;
+      } else {
+        r = m;
+      }
     }
   }
   return (l + r) / 2;
