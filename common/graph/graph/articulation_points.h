e8f735f148b3305e0b9fb44bcddc3a4513e5be8a
Loks- <alexeypoyarkov@gmail.com>
2023-05-21 13:23:28 -0700

    Point default algorithm to stack based.

--- common/graph/graph/articulation_points.h
+++ common/graph/graph/articulation_points.h
@@ -1,10 +1,10 @@
 #pragma once
 
 #include "common/graph/graph.h"
-#include "common/graph/graph/articulation_points/dfs_recursion.h"
+#include "common/graph/graph/articulation_points/dfs_stack.h"
 
 #include <vector>
 
 inline std::vector<unsigned> ArticulationPoints(const UndirectedGraph& g) {
-  return graph::ap::DFSRecursion(g);
+  return graph::ap::DFSStack(g);
 }
