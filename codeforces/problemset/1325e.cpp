28622efaf739005d39a1265e0d8d01964382bcdb
Loks- <alexeypoyarkov@gmail.com>
2024-07-21 07:39:29 -0700

    Move shortest cycle function to the common library and point solution to it.

--- codeforces/problemset/1325e.cpp
+++ codeforces/problemset/1325e.cpp
@@ -1,82 +1,36 @@
 #include "common/factorization/factorization.h"
-#include "common/heap/ukvm/dheap.h"
+#include "common/graph/graph_ei/shortest_cycle/vertex_exclusion.h"
 #include "common/stl/base.h"
 #include "common/vector/read.h"
 #include "common/vector/unique.h"
 
 #include <unordered_map>
 
-using THeap = heap::ukvm::DHeap<4, unsigned>;
+using TGraph = UndirectedGraphEI<unsigned>;
 
 int main_1325e() {
   unsigned N;
   cin >> N;
   auto v = nvector::Read<unsigned>(N);
   vector<vector<unsigned>> vvp;
-  vector<unsigned> vua;
   vector<unsigned> vp(1, 1u);
   for (auto u : v) {
     vector<unsigned> uvp;
-    unsigned ua = 1;
     for (auto& p : Factorize(u)) {
       if (p.power & 1) {
-        ua *= p.prime;
         uvp.push_back(p.prime);
         vp.push_back(p.prime);
       }
     }
-    assert(uvp.size() <= 2);
-    if (ua == 1) {
-      std::cout << "1" << std::endl;
-      return 0;
-    } else {
-      vua.push_back(ua);
-      vvp.push_back(uvp);
-    }
-  }
-  nvector::UniqueUnsorted(vua);
-  if (vua.size() < N) {
-    std::cout << "2" << std::endl;
-    return 0;
+    for (; uvp.size() < 2;) uvp.push_back(1);
+    vvp.push_back(uvp);
   }
   nvector::UniqueUnsorted(vp);
-  vector<unsigned> vpc(vp.size(), 0);
-  vector<unordered_map<unsigned, unsigned>> vm(vp.size());
-  for (auto& uvp : vvp) {
-    auto p0 = uvp[0];
-    auto it0 = lower_bound(vp.begin(), vp.end(), p0);
-    auto i0 = it0 - vp.begin();
-    auto p1 = (uvp.size() > 1) ? uvp[1] : 1u;
-    auto it1 = lower_bound(vp.begin(), vp.end(), p1);
-    auto i1 = it1 - vp.begin();
-    vpc[i0]++;
-    vpc[i1]++;
-    vm[i0][i1] = 1;
-    vm[i1][i0] = 1;
-  }
-  unsigned answer = N + 1;
-  THeap h(vpc, false);
-  for (; !h.Empty();) {
-    auto u = h.ExtractKey();
-    for (auto it1 = vm[u].begin(); it1 != vm[u].end(); ++it1) {
-      for (auto it2 = vm[u].begin(); it2 != it1; ++it2) {
-        auto u1 = it1->first, u2 = it2->first, d12 = it1->second + it2->second;
-        auto& dm = vm[u1][u2];
-        if (dm > 0) {
-          answer = min(answer, dm + d12);
-          dm = min(dm, d12);
-        } else {
-          dm = d12;
-        }
-        vm[u2][u1] = dm;
-      }
-    }
-    for (auto it1 : vm[u]) {
-      auto u1 = it1.first;
-      vm[u1].erase(u);
-      h.Set(u1, vm[u1].size());
-    }
-  }
+  TGraph g(vp.size());
+  for (auto& v : vvp)
+    g.AddEdge(lower_bound(vp.begin(), vp.end(), v[0]) - vp.begin(),
+              lower_bound(vp.begin(), vp.end(), v[1]) - vp.begin(), 1);
+  auto answer = graph::scycle::VertexExclusion(g, N + 1);
   std::cout << ((answer <= N) ? int(answer) : -1) << std::endl;
   return 0;
 }
---
+++ common/graph/graph_ei/shortest_cycle/vertex_exclusion.h
@@ -0,0 +1,78 @@
+#pragma once
+
+#include "common/base.h"
+#include "common/graph/graph_ei.h"
+#include "common/heap/ukvm/dheap.h"
+
+#include <algorithm>
+#include <unordered_map>
+#include <vector>
+
+namespace graph {
+namespace scycle {
+// Find shortest cycle length by vertex exclusion.
+// All edges cost are non-negative.
+// Works for sparse graphs.
+namespace hidden {
+template <class TValue>
+TValue VertexExclusionI(
+    std::vector<std::unordered_map<unsigned, TValue>>& edges,
+    const TValue& max_cost) {
+  TValue best_cost = max_cost;
+  std::vector<unsigned> vpc;
+  vpc.reserve(edges.size());
+  for (auto& m : edges) vpc.push_back(m.size());
+  heap::ukvm::DHeap<4, unsigned> h(vpc, false);
+  for (; !h.Empty();) {
+    auto u = h.ExtractKey();
+    for (auto it1 = edges[u].begin(); it1 != edges[u].end(); ++it1) {
+      for (auto it2 = edges[u].begin(); it2 != it1; ++it2) {
+        auto u1 = it1->first, u2 = it2->first;
+        auto d12 = it1->second + it2->second;
+        auto itu12 = edges[u1].find(u2);
+        if (itu12 == edges[u1].end()) {
+          edges[u1][u2] = edges[u2][u1] = d12;
+        } else {
+          best_cost = std::min(best_cost, itu12->second + d12);
+          if (itu12->second > d12) {
+            edges[u1][u2] = edges[u2][u1] = d12;
+          }
+        }
+      }
+    }
+    for (auto it1 : edges[u]) {
+      auto u1 = it1.first;
+      edges[u1].erase(u);
+      h.Set(u1, edges[u1].size());
+    }
+  }
+  return best_cost;
+}
+}  // namespace hidden
+
+template <class TValue>
+TValue VertexExclusion(const UndirectedGraphEI<TValue>& g, TValue max_cost) {
+  std::vector<std::unordered_map<unsigned, TValue>> edges(g.Size());
+  for (unsigned u = 0; u < g.Size(); ++u) {
+    auto& ue = g.Edges(u);
+    auto& uei = g.EdgesInfo(u);
+    assert(ue.size() == uei.size());
+    for (unsigned j = 0; j < ue.size(); ++j) {
+      auto v = ue[j];
+      if (v == u) {
+        max_cost = std::min(max_cost, uei[j]);
+        continue;
+      }
+      auto it = edges[u].find(v);
+      if (it == edges[u].end()) {
+        edges[u][v] = uei[j];
+      } else {
+        max_cost = std::min(max_cost, it->second + uei[j]);
+        it->second = std::min(it->second, uei[j]);
+      }
+    }
+  }
+  return hidden::VertexExclusionI(edges, max_cost);
+}
+}  // namespace scycle
+}  // namespace graph
