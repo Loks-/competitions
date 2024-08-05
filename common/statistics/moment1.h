cd956fa18fac89cfc6076109d4b7ffadf8952602
Loks- <alexeypoyarkov@gmail.com>
2024-08-05 12:43:40 -0700

    Add constexpr.

--- common/statistics/moment1.h
+++ common/statistics/moment1.h
@@ -10,72 +10,73 @@ class Moment1 {
   TValue s0, s1;
 
  public:
-  Moment1() { Clear(); }
+  constexpr Moment1() { Clear(); }
 
-  void Clear() { s0 = s1 = TValue(0); }
+  constexpr void Clear() { s0 = s1 = TValue(0); }
 
-  void AddSample(const TValue& x, const TValue& w = 1) {
+  constexpr void AddSample(const TValue& x, const TValue& w = 1) {
     s0 += w;
     s1 += x * w;
   }
 
-  void AddValue(const TValue& x) { s1 += x * s0; }
+  constexpr void AddValue(const TValue& x) { s1 += x * s0; }
 
-  void MultWeight(const TValue& x) {
+  constexpr void MultWeight(const TValue& x) {
     s0 *= x;
     s1 *= x;
   }
 
-  void AddStat(const TSelf& r) {
+  constexpr void AddStat(const TSelf& r) {
     s0 += r.s0;
     s1 += r.s1;
   }
 
-  TSelf& operator+=(const TValue& x) {
+  constexpr TSelf& operator+=(const TValue& x) {
     AddValue(x);
     return *this;
   }
 
-  TSelf& operator-=(const TValue& x) {
+  constexpr TSelf& operator-=(const TValue& x) {
     AddValue(-x);
     return *this;
   }
 
-  TSelf& operator*=(const TValue& x) {
+  constexpr TSelf& operator*=(const TValue& x) {
     MultWeight(x);
     return *this;
   }
 
-  TSelf& operator+=(const TSelf& r) {
+  constexpr TSelf& operator+=(const TSelf& r) {
     AddStat(r);
     return *this;
   }
 
-  TSelf operator+(const TValue& x) {
+  constexpr TSelf operator+(const TValue& x) {
     TSelf t(*this);
     t += x;
     return t;
   }
 
-  TSelf operator-(const TValue& x) {
+  constexpr TSelf operator-(const TValue& x) {
     TSelf t(*this);
     t -= x;
     return t;
   }
 
-  TSelf operator*(const TValue& x) {
+  constexpr TSelf operator*(const TValue& x) {
     TSelf t(*this);
     t *= x;
     return t;
   }
 
-  TSelf operator+(const TSelf& r) {
+  constexpr TSelf operator+(const TSelf& r) {
     TSelf t(*this);
     t += r;
     return t;
   }
 
-  TValue RawMoment1() const { return s1 / s0; }
-  TValue Mean() const { return RawMoment1(); }
+  constexpr TValue RawMoment1() const { return s1 / s0; }
+
+  constexpr TValue Mean() const { return RawMoment1(); }
 };
 }  // namespace stat
--- common/statistics/moment2.h
+++ common/statistics/moment2.h
@@ -13,84 +13,84 @@ class Moment2 : public Moment1<TValue> {
   TValue s2;
 
  public:
-  Moment2() { Clear(); }
+  constexpr Moment2() { Clear(); }
 
-  void Clear() {
+  constexpr void Clear() {
     TBase::Clear();
     s2 = TValue(0);
   }
 
-  void AddSample(const TValue& x, const TValue& w = 1) {
+  constexpr void AddSample(const TValue& x, const TValue& w = 1) {
     TBase::AddSample(x, w);
     s2 += x * x * w;
   }
 
-  void MultWeight(const TValue& x) {
+  constexpr void MultWeight(const TValue& x) {
     TBase::MultWeight(x);
     s2 *= x;
   }
 
-  void AddValue(const TValue& x) {
+  constexpr void AddValue(const TValue& x) {
     s2 += x * x * TBase::s0 + 2 * x * TBase::s1;
     TBase::AddValue(x);
   }
 
-  void AddStat(const TSelf& r) {
+  constexpr void AddStat(const TSelf& r) {
     TBase::AddStat(r);
     s2 += r.s2;
   }
 
-  TSelf& operator+=(const TValue& x) {
+  constexpr TSelf& operator+=(const TValue& x) {
     AddValue(x);
     return *this;
   }
 
-  TSelf& operator-=(const TValue& x) {
+  constexpr TSelf& operator-=(const TValue& x) {
     AddValue(-x);
     return *this;
   }
 
-  TSelf& operator*=(const TValue& x) {
+  constexpr TSelf& operator*=(const TValue& x) {
     MultWeight(x);
     return *this;
   }
 
-  TSelf& operator+=(const TSelf& r) {
+  constexpr TSelf& operator+=(const TSelf& r) {
     AddStat(r);
     return *this;
   }
 
-  TSelf operator+(const TValue& x) {
+  constexpr TSelf operator+(const TValue& x) {
     TSelf t(*this);
     t += x;
     return t;
   }
 
-  TSelf operator-(const TValue& x) {
+  constexpr TSelf operator-(const TValue& x) {
     TSelf t(*this);
     t -= x;
     return t;
   }
 
-  TSelf operator*(const TValue& x) {
+  constexpr TSelf operator*(const TValue& x) {
     TSelf t(*this);
     t *= x;
     return t;
   }
 
-  TSelf operator+(const TSelf& r) {
+  constexpr TSelf operator+(const TSelf& r) {
     TSelf t(*this);
     t += r;
     return t;
   }
 
-  TValue RawMoment2() const { return s2 / TBase::s0; }
+  constexpr TValue RawMoment2() const { return s2 / TBase::s0; }
 
-  TValue CentralMoment2() const {
+  constexpr TValue CentralMoment2() const {
     TValue m = TBase::RawMoment1();
     return RawMoment2() - m * m;
   }
 
-  TValue Variance() const { return CentralMoment2(); }
+  constexpr TValue Variance() const { return CentralMoment2(); }
 };
 }  // namespace stat
