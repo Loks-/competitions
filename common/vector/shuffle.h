5ddf97db183589025ab809921125eb6ea3cdb9a5
Loks- <alexeypoyarkov@gmail.com>
2024-08-24 13:09:33 -0700

    Add missing inline.

--- common/vector/shuffle.h
+++ common/vector/shuffle.h
@@ -6,7 +6,7 @@
 
 namespace nvector {
 template <class T>
-void Shuffle(std::vector<T>& v) {
+inline void Shuffle(std::vector<T>& v) {
   std::shuffle(v.begin(), v.end(), std::default_random_engine(17));
 }
 }  // namespace nvector
