e055bfcf9be0932676e79f0387cd1c0afb81ac62
Loks- <alexeypoyarkov@gmail.com>
2024-09-11 11:05:34 -0700

    Add comment.

--- common/local.h
+++ common/local.h
@@ -1,3 +1,9 @@
 #pragma once
 
+/**
+ * @brief Indicates whether the code is running locally or externally.
+ *
+ * This variable is set to true for local runs and will be set to false
+ * for external runs.
+ */
 static constexpr bool local_run = true;
