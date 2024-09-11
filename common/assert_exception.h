9f14d9f261efb4d6cff70151988b551dcce2aa99
Loks- <alexeypoyarkov@gmail.com>
2024-09-11 08:50:18 -0700

    Add comments

--- common/assert_exception.h
+++ common/assert_exception.h
@@ -3,14 +3,30 @@
 #include <iostream>
 #include <string>
 
+/**
+ * @brief Exception class for assertion failures.
+ */
 class AssertException {};
 
-inline void Assert(bool b) {
-  if (!b) throw AssertException();
+/**
+ * @brief Asserts that a condition is true.
+ *
+ * @param condition The condition to check.
+ * @throws AssertException if the condition is false.
+ */
+inline void Assert(bool condition) {
+  if (!condition) throw AssertException();
 }
 
-inline void Assert(bool b, const std::string& message) {
-  if (!b) {
+/**
+ * @brief Asserts that a condition is true, with a custom message.
+ *
+ * @param condition The condition to check.
+ * @param message The message to display if the assertion fails.
+ * @throws AssertException if the condition is false.
+ */
+inline void Assert(bool condition, const std::string& message) {
+  if (!condition) {
     std::cerr << "Exception: " << message << std::endl;
     throw AssertException();
   }
