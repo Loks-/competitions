c8ca7c70deef9915506ffcdc6ba40e6e36c96f40
Loks- <alexeypoyarkov@gmail.com>
2024-09-11 12:17:26 -0700

    Add comments.

--- common/algorithm/binary_search.h
+++ common/algorithm/binary_search.h
@@ -3,22 +3,31 @@
 #include <algorithm>
 
 /**
- * @brief Finds the lower bound using a custom function.
+ * @brief Finds the first element in the range [begin, end) that is not less
+ * than the given value using a custom comparator.
  *
- * @tparam TValue1 The type of the iterator.
- * @tparam TValue2 The type of the value to compare.
- * @tparam TFunction The type of the custom function.
- * @param begin The beginning of the range.
- * @param end The end of the range.
+ * This function performs a binary search from begin to end, applying the
+ * comparator f to each element. If an element is not less than the given value,
+ * the search stops and the iterator to that element is returned. If no such
+ * element is found, the end iterator is returned.
+ *
+ * The comparator function f should be monotonic.
+ *
+ * @tparam TIterator The type of the iterator.
+ * @tparam TValue The type of the value to compare.
+ * @tparam TFunction The type of the comparator function.
+ * @param begin The beginning of the range to search.
+ * @param end The end of the range to search.
  * @param value The value to compare.
- * @param f The custom function.
- * @return The iterator pointing to the lower bound.
+ * @param f The comparator function to apply to each element.
+ * @return The iterator to the first element that is not less than the given
+ * value, or end if no such element is found.
  */
-template <class TValue1, class TValue2, class TFunction>
-constexpr TValue1 LowerBoundF(TValue1 begin, TValue1 end, const TValue2& value,
-                              TFunction& f) {
+template <class TIterator, class TValue, class TFunction>
+constexpr TIterator LowerBoundF(TIterator begin, TIterator end,
+                                const TValue& value, const TFunction& f) {
   for (; begin < end;) {
-    const TValue1 m = (begin + end) / 2;
+    const TIterator m = (begin + end) / 2;
     if (f(m) < value) {
       begin = m + 1;
     } else {
@@ -29,22 +38,31 @@ constexpr TValue1 LowerBoundF(TValue1 begin, TValue1 end, const TValue2& value,
 }
 
 /**
- * @brief Finds the upper bound using a custom function.
+ * @brief Finds the first element in the range [begin, end) that is greater than
+ * the given value using a custom comparator.
  *
- * @tparam TValue1 The type of the iterator.
- * @tparam TValue2 The type of the value to compare.
- * @tparam TFunction The type of the custom function.
- * @param begin The beginning of the range.
- * @param end The end of the range.
+ * This function performs a binary search from begin to end, applying the
+ * comparator f to each element. If an element is greater than the given value,
+ * the search stops and the iterator to that element is returned. If no such
+ * element is found, the end iterator is returned.
+ *
+ * The comparator function f should be monotonic.
+ *
+ * @tparam TIterator The type of the iterator.
+ * @tparam TValue The type of the value to compare.
+ * @tparam TFunction The type of the comparator function.
+ * @param begin The beginning of the range to search.
+ * @param end The end of the range to search.
  * @param value The value to compare.
- * @param f The custom function.
- * @return The iterator pointing to the upper bound.
+ * @param f The comparator function to apply to each element.
+ * @return The iterator to the first element that is greater than the given
+ * value, or end if no such element is found.
  */
-template <class TValue1, class TValue2, class TFunction>
-constexpr TValue1 UpperBoundF(TValue1 begin, TValue1 end, const TValue2& value,
-                              TFunction& f) {
+template <class TIterator, class TValue, class TFunction>
+constexpr TIterator UpperBoundF(TIterator begin, TIterator end,
+                                const TValue& value, const TFunction& f) {
   for (; begin < end;) {
-    const TValue1 m = (begin + end) / 2;
+    const TIterator m = (begin + end) / 2;
     if (f(m) <= value) {
       begin = m + 1;
     } else {
@@ -54,10 +72,30 @@ constexpr TValue1 UpperBoundF(TValue1 begin, TValue1 end, const TValue2& value,
   return begin;
 }
 
-template <class TValue1, class TFunction>
-constexpr TValue1 LowerBoundB(TValue1 begin, TValue1 end, TFunction& f) {
+/**
+ * @brief Finds the first element in the range [begin, end) that satisfies the
+ * predicate f.
+ *
+ * This function performs a binary search from begin to end, applying the
+ * predicate f to each element. If an element satisfies the predicate, the
+ * search stops and the iterator to that element is returned. If no such element
+ * is found, the end iterator is returned.
+ *
+ * The predicate function f should be monotonic.
+ *
+ * @tparam TIterator The type of the iterator.
+ * @tparam TFunction The type of the predicate function.
+ * @param begin The beginning of the range to search.
+ * @param end The end of the range to search.
+ * @param f The predicate function to apply to each element.
+ * @return The iterator to the first element that satisfies the predicate, or
+ * end if no such element is found.
+ */
+template <class TIterator, class TFunction>
+constexpr TIterator LowerBoundB(TIterator begin, TIterator end,
+                                const TFunction& f) {
   for (; begin < end;) {
-    const TValue1 m = (begin + end) / 2;
+    const TIterator m = (begin + end) / 2;
     if (f(m)) {
       end = m;
     } else {
@@ -67,9 +105,25 @@ constexpr TValue1 LowerBoundB(TValue1 begin, TValue1 end, TFunction& f) {
   return begin;
 }
 
-template <class TValue1, class TFunction>
-constexpr TValue1 LowerBoundB(TValue1 begin, TFunction& f) {
-  auto end = std::max(begin, TValue1(1));
+/**
+ * @brief Finds the first element starting from begin that satisfies the
+ * predicate f.
+ *
+ * This function performs an unbounded binary search starting from begin,
+ * applying the predicate f to each element. If an element satisfies the
+ * predicate, the search stops and the iterator to that element is returned.
+ *
+ * The predicate function f should be monotonic.
+ *
+ * @tparam TIterator The type of the iterator.
+ * @tparam TFunction The type of the predicate function.
+ * @param begin The beginning of the range to search.
+ * @param f The predicate function to apply to each element.
+ * @return The iterator to the first element that satisfies the predicate.
+ */
+template <class TIterator, class TFunction>
+constexpr TIterator LowerBoundB(TIterator begin, const TFunction& f) {
+  auto end = std::max(begin, TIterator(1));
   for (; !f(end); end *= 2) begin = end + 1;
   return LowerBoundB(begin, end, f);
 }
