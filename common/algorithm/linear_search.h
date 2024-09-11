75bcc4c5530f012d909b0756598d395ad6849916
Loks- <alexeypoyarkov@gmail.com>
2024-09-11 12:05:17 -0700

    Add comments.

--- common/algorithm/linear_search.h
+++ common/algorithm/linear_search.h
@@ -1,15 +1,47 @@
 #pragma once
 
-template <class TValue1, class TFunction>
-constexpr TValue1 FirstIfB(TValue1 begin, TValue1 end, TFunction& f) {
+/**
+ * @brief Finds the first element in the range [begin, end) that satisfies the
+ * predicate f.
+ *
+ * This function performs a linear search from begin to end, applying the
+ * predicate f to each element. If an element satisfies the predicate, the
+ * search stops and the iterator to that element is returned. If no element
+ * satisfies the predicate, the end iterator is returned.
+ *
+ * @tparam TIterator The type of the iterator.
+ * @tparam TFunction The type of the predicate function.
+ * @param begin The beginning of the range to search.
+ * @param end The end of the range to search.
+ * @param f The predicate function to apply to each element.
+ * @return The iterator to the first element that satisfies the predicate, or
+ * end if no such element is found.
+ */
+template <class TIterator, class TFunction>
+constexpr TIterator FirstIfB(TIterator begin, const TIterator end,
+                             const TFunction& f) {
   for (; begin < end; ++begin) {
     if (f(begin)) break;
   }
   return begin;
 }
 
-template <class TValue1, class TFunction>
-constexpr TValue1 FirstIfB(TValue1 begin, TFunction& f) {
+/**
+ * @brief Finds the first element starting from begin that satisfies the
+ * predicate f.
+ *
+ * This function performs an unbounded linear search starting from begin,
+ * applying the predicate f to each element. If an element satisfies the
+ * predicate, the search stops and the iterator to that element is returned.
+ *
+ * @tparam TIterator The type of the iterator.
+ * @tparam TFunction The type of the predicate function.
+ * @param begin The beginning of the range to search.
+ * @param f The predicate function to apply to each element.
+ * @return The iterator to the first element that satisfies the predicate.
+ */
+template <class TIterator, class TFunction>
+constexpr TIterator FirstIfB(TIterator begin, const TFunction& f) {
   for (;; ++begin) {
     if (f(begin)) return begin;
   }
