4abd3fa22a0f7387f830190219382bf063b72a60
Loks- <alexeypoyarkov@gmail.com>
2024-09-11 11:03:08 -0700

    Add comments.

--- common/heap.h
+++ common/heap.h
@@ -4,7 +4,24 @@
 
 #include <functional>
 
+/**
+ * @brief A default heap implementation with the minimum element on top.
+ *
+ * This is an alias for heap::base::DHeap with a branching factor of 4,
+ * TValue as the value type, and std::less<TValue> as the comparator.
+ *
+ * @tparam TValue The type of the values stored in the heap.
+ */
 template <class TValue>
 using HeapMinOnTop = heap::base::DHeap<4u, TValue, std::less<TValue>>;
+
+/**
+ * @brief A default heap implementation with the maximum element on top.
+ *
+ * This is an alias for heap::base::DHeap with a branching factor of 4,
+ * TValue as the value type, and std::greater<TValue> as the comparator.
+ *
+ * @tparam TValue The type of the values stored in the heap.
+ */
 template <class TValue>
 using HeapMaxOnTop = heap::base::DHeap<4u, TValue, std::greater<TValue>>;
