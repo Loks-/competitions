0e1baad138ff02c0ccb47b5fa338f372377af793
Loks- <alexeypoyarkov@gmail.com>
2024-09-11 10:52:12 -0700

    Add comments.

--- common/hash.h
+++ common/hash.h
@@ -3,5 +3,13 @@
 #include "common/hash/hash.h"
 #include "common/hash/value.h"
 
+/**
+ * @brief A default hash class to use.
+ *
+ * This is an alias for nhash::HashSafe<T>, providing a default
+ * hash implementation for the given type T.
+ *
+ * @tparam T The type to hash.
+ */
 template <class T>
 using DHash = nhash::HashSafe<T>;
