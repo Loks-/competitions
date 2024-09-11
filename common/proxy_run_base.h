a6b2d918948112caa95e4ab9e866382b94342a7f
Loks- <alexeypoyarkov@gmail.com>
2024-09-11 11:59:03 -0700

    Add comments and few consts.

--- common/proxy_run_base.h
+++ common/proxy_run_base.h
@@ -3,7 +3,23 @@
 #include <map>
 #include <string>
 
+/**
+ * @brief Type alias for a pointer to a solution function.
+ *
+ * The solution function is expected to return an integer.
+ */
 using TSolutionPtr = int (*)();
+
+/**
+ * @brief Type alias for a map of solution names to solution function pointers.
+ */
 using TSolutionsMap = std::map<std::string, TSolutionPtr>;
 
+/**
+ * @brief Global map of solution names to their corresponding solution
+ * functions.
+ *
+ * This map is populated with the available solutions and is used to look up
+ * and execute solutions by name.
+ */
 extern TSolutionsMap solutions_map;
--- common/proxy_run_main.h
+++ common/proxy_run_main.h
@@ -7,42 +7,74 @@
 #include <sstream>
 #include <string>
 
+/**
+ * @brief Runs the solution by name.
+ *
+ * @param solution The name of the solution to run.
+ * @return The result of the solution execution.
+ */
 inline int ProxyRun(const std::string& solution) {
-  auto it = solutions_map.find(solution);
+  const auto it = solutions_map.find(solution);
   if (it != solutions_map.end()) return it->second();
   std::cout << "Unknown solution name" << std::endl;
   return -2;
 }
 
+/**
+ * @brief Runs the solution by name with input from a file.
+ *
+ * @param solution The name of the solution to run.
+ * @param input_file_name The name of the input file.
+ * @return The result of the solution execution.
+ */
 inline int ProxyRun(const std::string& solution,
                     const std::string& input_file_name) {
   if (input_file_name.empty()) return ProxyRun(solution);
   std::ifstream input_file(input_file_name);
   auto cin_backup = std::cin.rdbuf(input_file.rdbuf());
-  int solution_results = ProxyRun(solution);
+  const int solution_results = ProxyRun(solution);
   std::cin.rdbuf(cin_backup);
   return solution_results;
 }
 
+/**
+ * @brief Runs the solution by name with input from a file and output to a file.
+ *
+ * @param solution The name of the solution to run.
+ * @param input_file_name The name of the input file.
+ * @param output_file_name The name of the output file.
+ * @return The result of the solution execution.
+ */
 inline int ProxyRun(const std::string& solution,
                     const std::string& input_file_name,
                     const std::string& output_file_name) {
   if (output_file_name.empty()) return ProxyRun(solution, input_file_name);
   std::ofstream output_file(output_file_name);
   auto cout_backup = std::cout.rdbuf(output_file.rdbuf());
-  int solution_results = ProxyRun(solution, input_file_name);
+  const int solution_results = ProxyRun(solution, input_file_name);
   std::cout.rdbuf(cout_backup);
   return solution_results;
 }
 
+/**
+ * @brief Tests a single solution against an expected output.
+ *
+ * @param solution The name of the solution to test.
+ * @param input_file_name The name of the input file.
+ * @param test_file_name The name of the file containing the expected output.
+ * @return 0 if the solution output matches the expected output, otherwise an
+ * error code.
+ */
 inline int ProxyTestOne(const std::string& solution,
                         const std::string& input_file_name,
                         const std::string& test_file_name) {
   std::stringstream solution_output, expected_output;
   auto cout_backup = std::cout.rdbuf(solution_output.rdbuf());
-  int solution_results = ProxyRun(solution, input_file_name);
+  const int solution_results = ProxyRun(solution, input_file_name);
   if (solution_results) return solution_results;
   std::cout.rdbuf(cout_backup);
+
+  // Remove extra spaces and newlines from the solution output
   std::string sraw = solution_output.str(), sfixed;
   sfixed.reserve(sraw.size());
   for (unsigned i = 0; i < sraw.size(); ++i) {
@@ -50,6 +82,8 @@ inline int ProxyTestOne(const std::string& solution,
       continue;
     sfixed.push_back(sraw[i]);
   }
+
+  // Read the expected output from the test file
   std::ifstream test_file(test_file_name);
   expected_output << test_file.rdbuf();
   if (sfixed != expected_output.str()) {
@@ -60,17 +94,26 @@ inline int ProxyTestOne(const std::string& solution,
   return 0;
 }
 
+/**
+ * @brief Tests all solutions for a given problem against an expected output.
+ *
+ * @param problem The name of the problem to test.
+ * @param input_file_name The name of the input file.
+ * @param test_file_name The name of the file containing the expected output.
+ * @return 0 if all solutions output match the expected output, otherwise an
+ * error code.
+ */
 inline int ProxyTestAll(const std::string& problem,
                         const std::string& input_file_name,
                         const std::string& test_file_name) {
-  std::string problem_prefix = problem + "__";
+  const std::string problem_prefix = problem + "__";
   unsigned correct_solutions = 0;
   for (auto it = solutions_map.lower_bound(problem); it != solutions_map.end();
        ++it) {
     if ((it->first != problem) &&
         (it->first.substr(0, problem_prefix.size()) != problem_prefix))
       break;
-    int solutions_results =
+    const int solutions_results =
         ProxyTestOne(it->first, input_file_name, test_file_name);
     if (solutions_results) return solutions_results;
     ++correct_solutions;
@@ -82,6 +125,15 @@ inline int ProxyTestAll(const std::string& problem,
   return 0;
 }
 
+/**
+ * @brief Automatically runs or tests a solution based on the presence of a test
+ * file.
+ *
+ * @param solution The name of the solution to run or test.
+ * @param input_file_name The name of the input file.
+ * @param test_file_name The name of the file containing the expected output.
+ * @return The result of the solution execution or testing.
+ */
 inline int ProxyAuto(const std::string& solution,
                      const std::string& input_file_name,
                      const std::string& test_file_name) {
