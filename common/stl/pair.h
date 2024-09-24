7bd32dc6399833c64bd1462d7afb070792e7da47
Loks- <alexeypoyarkov@gmail.com>
2024-09-23 22:25:38 -0700

    Add empty line.

--- common/stl/pair.h
+++ common/stl/pair.h
@@ -3,6 +3,7 @@
 #include <utility>
 
 namespace std {
+
 template <class T1, class T2>
 constexpr pair<T1, T2>& operator+=(pair<T1, T2>& l, const pair<T1, T2>& r) {
   l.first += r.first;
@@ -26,4 +27,5 @@ template <class T1, class T2>
 constexpr pair<T1, T2> operator-(const pair<T1, T2>& l, const pair<T1, T2>& r) {
   return make_pair(l.first - r.first, l.second - r.second);
 }
+
 }  // namespace std
