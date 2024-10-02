96518d7b14d0cc27a5e88553c0ffa6e30391e166
Loks- <alexeypoyarkov@gmail.com>
2024-10-01 21:39:34 -0700

    Add comments.

--- common/vector/first_missed.h
+++ common/vector/first_missed.h
@@ -4,7 +4,17 @@
 #include <vector>
 
 namespace nvector {
-// Useful for Grundy/Nim value
+
+/**
+ * @brief Finds the smallest non-negative integer not present in the vector.
+ *
+ * This function sorts the input vector and then finds the smallest non-negative
+ * integer that is not present in the vector. It is useful for calculating
+ * Grundy/Nim values.
+ *
+ * @param v The input vector of unsigned integers.
+ * @return The smallest non-negative integer not present in the vector.
+ */
 constexpr unsigned FirstMissed(std::vector<unsigned>& v) {
   std::sort(v.begin(), v.end());
   unsigned k = 0, l = 0;
@@ -13,4 +23,5 @@ constexpr unsigned FirstMissed(std::vector<unsigned>& v) {
   }
   return k;
 }
+
 }  // namespace nvector
