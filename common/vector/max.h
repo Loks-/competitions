93725eab71a3adf43fd64b05b124b736ad1f2164
Loks- <alexeypoyarkov@gmail.com>
2024-10-03 16:05:21 -0700

    Add comments.

--- common/vector/max.h
+++ common/vector/max.h
@@ -1,13 +1,32 @@
 #pragma once
 
+#include "common/base.h"
+
 #include <vector>
 
 namespace nvector {
+
+/**
+ * @brief Computes the maximum element in a vector.
+ *
+ * This function calculates the maximum element in the input vector.
+ *
+ * @tparam T The type of the elements in the vector.
+ * @param v The input vector.
+ * @return The maximum element in the vector.
+ * @note The input vector must not be empty.
+ */
 template <class T>
 constexpr T Max(const std::vector<T>& v) {
-  T r(v[0]);
-  for (unsigned i = 1; i < v.size(); ++i)
-    if (r < v[i]) r = v[i];
-  return r;
+  assert(!v.empty() && "The input vector must not be empty.");
+
+  T max_value = v[0];
+  for (unsigned i = 1; i < v.size(); ++i) {
+    if (max_value < v[i]) {
+      max_value = v[i];
+    }
+  }
+  return max_value;
 }
+
 }  // namespace nvector
--- common/vector/min.h
+++ common/vector/min.h
@@ -1,13 +1,32 @@
 #pragma once
 
+#include "common/base.h"
+
 #include <vector>
 
 namespace nvector {
+
+/**
+ * @brief Computes the minimum element in a vector.
+ *
+ * This function calculates the minimum element in the input vector.
+ *
+ * @tparam T The type of the elements in the vector.
+ * @param v The input vector.
+ * @return The minimum element in the vector.
+ * @note The input vector must not be empty.
+ */
 template <class T>
 constexpr T Min(const std::vector<T>& v) {
-  T r(v[0]);
-  for (unsigned i = 1; i < v.size(); ++i)
-    if (v[i] < r) r = v[i];
-  return r;
+  assert(!v.empty() && "The input vector must not be empty.");
+
+  T min_value = v[0];
+  for (unsigned i = 1; i < v.size(); ++i) {
+    if (v[i] < min_value) {
+      min_value = v[i];
+    }
+  }
+  return min_value;
 }
+
 }  // namespace nvector
