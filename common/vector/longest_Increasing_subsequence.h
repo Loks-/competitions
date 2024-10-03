932fab9306f08b3299e9e16dbfc1504be5b221aa
Loks- <alexeypoyarkov@gmail.com>
2024-10-03 08:48:31 -0700

    Add comments and small refactor.

--- common/vector/longest_Increasing_subsequence.h
+++ common/vector/longest_Increasing_subsequence.h
@@ -4,16 +4,30 @@
 #include <vector>
 
 namespace nvector {
+
+/**
+ * @brief Computes the length of the longest increasing subsequence in a vector.
+ *
+ * This function calculates the length of the longest increasing subsequence
+ * in the input vector using a binary search approach. The time complexity is
+ * O(n log n), where n is the size of the input vector.
+ *
+ * @tparam TValue The type of the elements in the vector.
+ * @param v The input vector.
+ * @return The length of the longest increasing subsequence.
+ */
 template <class TValue>
 constexpr unsigned LongestIncresingSubsequence(const std::vector<TValue>& v) {
-  std::vector<TValue> d;
-  for (unsigned i = 0; i < v.size(); ++i) {
-    unsigned j = std::lower_bound(d.begin(), d.end(), v[i]) - d.begin();
-    if (j == d.size())
-      d.push_back(v[i]);
-    else
-      d[j] = v[i];
+  std::vector<TValue> vd;
+  for (auto& x : v) {
+    auto it = std::lower_bound(vd.begin(), vd.end(), x);
+    if (it == vd.end()) {
+      vd.push_back(x);
+    } else {
+      *it = x;
+    }
   }
-  return d.size();
+  return vd.size();
 }
+
 }  // namespace nvector
