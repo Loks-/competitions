ef45996654fbac1394285a0e1ba18663c568c0db
Loks- <alexeypoyarkov@gmail.com>
2024-10-03 15:39:04 -0700

    Add comments.

--- common/vector/product.h
+++ common/vector/product.h
@@ -1,13 +1,25 @@
 #pragma once
 
-#include <numeric>
 #include <vector>
 
 namespace nvector {
+
+/**
+ * @brief Computes the product of all elements in a vector.
+ *
+ * This function calculates the product of all elements in the input vector.
+ *
+ * @tparam T The type of the elements in the vector.
+ * @param v The input vector.
+ * @return The product of all elements in the vector.
+ */
 template <class T>
 constexpr T Product(const std::vector<T>& v) {
-  T r(1);
-  for (auto& x : v) r *= x;
-  return r;
+  T result(1);
+  for (const auto& x : v) {
+    result *= x;
+  }
+  return result;
 }
+
 }  // namespace nvector
