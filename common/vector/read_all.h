9065691567151f81d744e4c6e9933de30771f37d
Loks- <alexeypoyarkov@gmail.com>
2024-10-03 16:07:36 -0700

    Add comments.

--- common/vector/read_all.h
+++ common/vector/read_all.h
@@ -4,11 +4,24 @@
 #include <vector>
 
 namespace nvector {
+
+/**
+ * @brief Reads all input values of type T from standard input into a vector.
+ *
+ * This function reads values of type T from standard input (cin) until the end
+ * of input and stores them in a vector.
+ *
+ * @tparam T The type of the elements to read.
+ * @return A vector containing all the input values of type T.
+ */
 template <class T>
 inline std::vector<T> ReadAll() {
   T x;
   std::vector<T> v;
-  for (; cin >> x;) v.push_back(x);
+  while (std::cin >> x) {
+    v.push_back(x);
+  }
   return v;
 }
+
 }  // namespace nvector
