9d493fd94415a32806d08f040fe469b4a236f630
Loks- <alexeypoyarkov@gmail.com>
2024-10-03 15:24:48 -0700

    Add comments and switch to template class.

--- common/vector/mask.h
+++ common/vector/mask.h
@@ -1,14 +1,27 @@
 #pragma once
 
-#include "common/base.h"
-
+#include <type_traits>
 #include <vector>
 
 namespace nvector {
-constexpr std::vector<unsigned> MaskToVector(uint64_t mask) {
+
+/**
+ * @brief Converts a bitmask to a vector of set bit positions.
+ *
+ * This function takes a bitmask and returns a vector containing the positions
+ * of the set bits (1s) in the bitmask.
+ *
+ * @tparam T The type of the bitmask (e.g., uint8_t, uint16_t, uint32_t,
+ * uint64_t).
+ * @param mask The bitmask to convert.
+ * @return A vector containing the positions of the set bits in the bitmask.
+ */
+template <typename T>
+constexpr std::vector<unsigned> MaskToVector(T mask) {
+  static_assert(std::is_integral<T>::value, "T must be an integral type.");
   std::vector<unsigned> v;
   for (unsigned i = 0; mask; ++i) {
-    uint64_t b = (1ull << i);
+    T b = (T(1) << i);
     if (mask & b) {
       v.push_back(i);
       mask &= ~b;
@@ -16,4 +29,5 @@ constexpr std::vector<unsigned> MaskToVector(uint64_t mask) {
   }
   return v;
 }
+
 }  // namespace nvector
