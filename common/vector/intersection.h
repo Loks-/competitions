92800d1956ef7451d146d27685b91b55d95b46ca
Loks- <alexeypoyarkov@gmail.com>
2024-10-02 19:02:10 -0700

    Add comments.

--- common/vector/intersection.h
+++ common/vector/intersection.h
@@ -4,6 +4,18 @@
 #include <vector>
 
 namespace nvector {
+
+/**
+ * @brief Computes the size of the intersection of two sorted vectors.
+ *
+ * This function calculates the number of elements that are present in both
+ * input vectors. The input vectors must be sorted.
+ *
+ * @tparam T The type of the elements in the vectors.
+ * @param v1 The first sorted vector.
+ * @param v2 The second sorted vector.
+ * @return The number of elements in the intersection of the two vectors.
+ */
 template <class T>
 constexpr unsigned Intersection(const std::vector<T>& v1,
                                 const std::vector<T>& v2) {
@@ -22,6 +34,18 @@ constexpr unsigned Intersection(const std::vector<T>& v1,
   return r;
 }
 
+/**
+ * @brief Computes the intersection of two sorted vectors.
+ *
+ * This function returns a vector containing the elements that are present in
+ * both input vectors. The input vectors must be sorted.
+ *
+ * @tparam T The type of the elements in the vectors.
+ * @param v1 The first sorted vector.
+ * @param v2 The second sorted vector.
+ * @return A vector containing the elements in the intersection of the two
+ * vectors.
+ */
 template <class T>
 constexpr std::vector<T> IntersectionV(const std::vector<T>& v1,
                                        const std::vector<T>& v2) {
@@ -40,6 +64,17 @@ constexpr std::vector<T> IntersectionV(const std::vector<T>& v1,
   return r;
 }
 
+/**
+ * @brief Computes the size of the intersection of two unsorted vectors.
+ *
+ * This function sorts the input vectors and then calculates the number of
+ * elements that are present in both vectors.
+ *
+ * @tparam T The type of the elements in the vectors.
+ * @param v1 The first unsorted vector.
+ * @param v2 The second unsorted vector.
+ * @return The number of elements in the intersection of the two vectors.
+ */
 template <class T>
 constexpr unsigned IntersectionUnsorted(std::vector<T>& v1,
                                         std::vector<T>& v2) {
@@ -48,6 +83,18 @@ constexpr unsigned IntersectionUnsorted(std::vector<T>& v1,
   return Intersection(v1, v2);
 }
 
+/**
+ * @brief Computes the intersection of two unsorted vectors.
+ *
+ * This function sorts the input vectors and then returns a vector containing
+ * the elements that are present in both vectors.
+ *
+ * @tparam T The type of the elements in the vectors.
+ * @param v1 The first unsorted vector.
+ * @param v2 The second unsorted vector.
+ * @return A vector containing the elements in the intersection of the two
+ * vectors.
+ */
 template <class T>
 constexpr std::vector<T> IntersectionVUnsorted(std::vector<T>& v1,
                                                std::vector<T>& v2) {
@@ -55,4 +102,5 @@ constexpr std::vector<T> IntersectionVUnsorted(std::vector<T>& v1,
   std::sort(v2.begin(), v2.end());
   return IntersectionV(v1, v2);
 }
+
 }  // namespace nvector
--- common/vector/union.h
+++ common/vector/union.h
@@ -6,6 +6,18 @@
 #include <vector>
 
 namespace nvector {
+
+/**
+ * @brief Computes the size of the union of two sorted vectors.
+ *
+ * This function calculates the number of unique elements that are present in
+ * either of the input vectors. The input vectors must be sorted.
+ *
+ * @tparam T The type of the elements in the vectors.
+ * @param v1 The first sorted vector.
+ * @param v2 The second sorted vector.
+ * @return The number of unique elements in the union of the two vectors.
+ */
 template <class T>
 constexpr unsigned Union(const std::vector<T>& v1, const std::vector<T>& v2) {
   unsigned r = 0, i1 = 0, i2 = 0;
@@ -22,6 +34,18 @@ constexpr unsigned Union(const std::vector<T>& v1, const std::vector<T>& v2) {
   return r + (v1.size() - i1) + (v2.size() - i2);
 }
 
+/**
+ * @brief Computes the union of two sorted vectors.
+ *
+ * This function returns a vector containing the unique elements that are
+ * present in either of the input vectors. The input vectors must be sorted.
+ *
+ * @tparam T The type of the elements in the vectors.
+ * @param v1 The first sorted vector.
+ * @param v2 The second sorted vector.
+ * @return A vector containing the unique elements in the union of the two
+ * vectors.
+ */
 template <class T>
 constexpr std::vector<T> UnionV(const std::vector<T>& v1,
                                 const std::vector<T>& v2) {
@@ -42,6 +66,17 @@ constexpr std::vector<T> UnionV(const std::vector<T>& v1,
   return r;
 }
 
+/**
+ * @brief Computes the size of the union of two unsorted vectors.
+ *
+ * This function sorts the input vectors and then calculates the number of
+ * unique elements that are present in either of the vectors.
+ *
+ * @tparam T The type of the elements in the vectors.
+ * @param v1 The first unsorted vector.
+ * @param v2 The second unsorted vector.
+ * @return The number of unique elements in the union of the two vectors.
+ */
 template <class T>
 constexpr unsigned UnionUnsorted(std::vector<T>& v1, std::vector<T>& v2) {
   std::sort(v1.begin(), v1.end());
@@ -49,11 +84,25 @@ constexpr unsigned UnionUnsorted(std::vector<T>& v1, std::vector<T>& v2) {
   return Union(v1, v2);
 }
 
+/**
+ * @brief Computes the union of two unsorted vectors.
+ *
+ * This function creates a new temporary vector by concatenating the input
+ * vectors and then returns a vector containing the unique elements that are
+ * present in either of the vectors.
+ *
+ * @tparam T The type of the elements in the vectors.
+ * @param v1 The first unsorted vector.
+ * @param v2 The second unsorted vector.
+ * @return A vector containing the unique elements in the union of the two
+ * vectors.
+ */
 template <class T>
-constexpr std::vector<T> UnionVUnsorted(std::vector<T>& v1,
-                                        std::vector<T>& v2) {
+constexpr std::vector<T> UnionVUnsorted(const std::vector<T>& v1,
+                                        const std::vector<T>& v2) {
   auto v = v1;
   v.insert(v.end(), v2.begin(), v2.end());
   return UniqueUnsorted(v);
 }
+
 }  // namespace nvector
