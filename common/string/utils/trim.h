4865771413fc4301bde5701ee5fc15f86bf7261e
Loks- <alexeypoyarkov@gmail.com>
2024-10-03 18:51:40 -0700

    Add comments.

--- common/string/utils/trim.h
+++ common/string/utils/trim.h
@@ -3,14 +3,28 @@
 #include <functional>
 #include <locale>
 
-// Trim from start (in place)
+/**
+ * @brief Trims whitespace from the start of a string (in place).
+ *
+ * This function removes all leading whitespace characters from the input
+ * string.
+ *
+ * @param s The input string to trim.
+ */
 constexpr void TrimL(std::string &s) {
   s.erase(s.begin(), std::find_if(s.begin(), s.end(), [](unsigned char ch) {
             return !std::isspace(ch);
           }));
 }
 
-// Trim from end (in place)
+/**
+ * @brief Trims whitespace from the end of a string (in place).
+ *
+ * This function removes all trailing whitespace characters from the input
+ * string.
+ *
+ * @param s The input string to trim.
+ */
 constexpr void TrimR(std::string &s) {
   s.erase(std::find_if(s.rbegin(), s.rend(),
                        [](unsigned char ch) { return !std::isspace(ch); })
@@ -18,25 +32,56 @@ constexpr void TrimR(std::string &s) {
           s.end());
 }
 
-// Trim from both ends (in place)
+/**
+ * @brief Trims whitespace from both ends of a string (in place).
+ *
+ * This function removes all leading and trailing whitespace characters from the
+ * input string.
+ *
+ * @param s The input string to trim.
+ */
 constexpr void Trim(std::string &s) {
   TrimR(s);
   TrimL(s);
 }
 
-// Trim from start (copying)
+/**
+ * @brief Trims whitespace from the start of a string (copying).
+ *
+ * This function returns a new string with all leading whitespace characters
+ * removed.
+ *
+ * @param s The input string to trim.
+ * @return A new string with leading whitespace removed.
+ */
 constexpr std::string TrimLCopy(std::string s) {
   TrimL(s);
   return s;
 }
 
-// Trim from end (copying)
+/**
+ * @brief Trims whitespace from the end of a string (copying).
+ *
+ * This function returns a new string with all trailing whitespace characters
+ * removed.
+ *
+ * @param s The input string to trim.
+ * @return A new string with trailing whitespace removed.
+ */
 constexpr std::string TrimRCopy(std::string s) {
   TrimR(s);
   return s;
 }
 
-// Trim from both ends (copying)
+/**
+ * @brief Trims whitespace from both ends of a string (copying).
+ *
+ * This function returns a new string with all leading and trailing whitespace
+ * characters removed.
+ *
+ * @param s The input string to trim.
+ * @return A new string with leading and trailing whitespace removed.
+ */
 constexpr std::string TrimCopy(std::string s) {
   Trim(s);
   return s;
