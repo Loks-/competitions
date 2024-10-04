fbe340d66274402d43bc164d87e4fc90423ff151
Loks- <alexeypoyarkov@gmail.com>
2024-10-03 18:24:46 -0700

    Add comments.

--- common/string/utils/is_digit.h
+++ common/string/utils/is_digit.h
@@ -1,3 +1,13 @@
 #pragma once
 
+/**
+ * @brief Checks if a character is a digit.
+ *
+ * This function checks if the input character is a digit (0-9).
+ * It is a constexpr function, which allows it to be used in other constexpr
+ * contexts.
+ *
+ * @param c The character to check.
+ * @return true if the character is a digit, false otherwise.
+ */
 constexpr bool IsDigit(char c) { return (c >= '0') && (c <= '9'); }
