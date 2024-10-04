319176f1f0fb397ca4359223dd88e30372ab4d43
Loks- <alexeypoyarkov@gmail.com>
2024-10-03 18:44:45 -0700

    Add comments.

--- common/string/utils/to_vector.h
+++ common/string/utils/to_vector.h
@@ -3,6 +3,15 @@
 #include <string>
 #include <vector>
 
+/**
+ * @brief Converts a string to a vector of characters.
+ *
+ * This function takes an input string and converts it to a vector of
+ * characters.
+ *
+ * @param s The input string to convert.
+ * @return A vector of characters representing the input string.
+ */
 constexpr std::vector<char> ToVector(const std::string& s) {
   return std::vector<char>(s.begin(), s.end());
 }
