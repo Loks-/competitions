bae18141e4c82bbedd93457ef16e6843ced114e3
Loks- <alexeypoyarkov@gmail.com>
2024-10-03 18:18:28 -0700

    Add comments.

--- common/string/utils/end_with.h
+++ common/string/utils/end_with.h
@@ -2,10 +2,24 @@
 
 #include <string>
 
-constexpr bool EndWith(const std::string& str, const std::string& prefix) {
-  if (str.size() < prefix.size()) return false;
-  for (auto its = str.rbegin(), itp = prefix.rbegin(); itp != prefix.rend();) {
-    if (*its++ != *itp++) return false;
+/**
+ * @brief Checks if a string ends with a given suffix.
+ *
+ * This function checks if the input string ends with the specified suffix.
+ *
+ * @param str The input string to check.
+ * @param suffix The suffix to check for.
+ * @return true if the input string ends with the specified suffix, false
+ * otherwise.
+ */
+constexpr bool EndWith(const std::string& str, const std::string& suffix) {
+  if (str.size() < suffix.size()) {
+    return false;
+  }
+  for (auto its = str.rbegin(), itp = suffix.rbegin(); itp != suffix.rend();) {
+    if (*its++ != *itp++) {
+      return false;
+    }
   }
   return true;
 }
--- common/string/utils/start_with.h
+++ common/string/utils/start_with.h
@@ -2,10 +2,24 @@
 
 #include <string>
 
+/**
+ * @brief Checks if a string starts with a given prefix.
+ *
+ * This function checks if the input string starts with the specified prefix.
+ *
+ * @param str The input string to check.
+ * @param prefix The prefix to check for.
+ * @return true if the input string starts with the specified prefix, false
+ * otherwise.
+ */
 constexpr bool StartWith(const std::string& str, const std::string& prefix) {
-  if (str.size() < prefix.size()) return false;
+  if (str.size() < prefix.size()) {
+    return false;
+  }
   for (auto its = str.begin(), itp = prefix.begin(); itp != prefix.end();) {
-    if (*its++ != *itp++) return false;
+    if (*its++ != *itp++) {
+      return false;
+    }
   }
   return true;
 }
