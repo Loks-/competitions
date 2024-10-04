262ad2ae0212d9556aa514ce17efe408c9947a80
Loks- <alexeypoyarkov@gmail.com>
2024-10-03 18:22:47 -0700

    Add comments.

--- common/string/utils/fixed_length.h
+++ common/string/utils/fixed_length.h
@@ -2,6 +2,20 @@
 
 #include <string>
 
+/**
+ * @brief Adjusts the length of a string to a fixed length.
+ *
+ * This function takes an input string and adjusts its length to the specified
+ * length. If the input string is shorter than the specified length, it is
+ * padded with the specified character. If the input string is longer than the
+ * specified length, it is truncated.
+ *
+ * @param input The input string to adjust.
+ * @param length The desired length of the output string.
+ * @param c The character to use for padding if the input string is shorter than
+ * the specified length. Default is space (' ').
+ * @return A string adjusted to the specified length.
+ */
 constexpr std::string FixedLength(const std::string& input, unsigned length,
                                   char c = ' ') {
   std::string s(input);
