5104ac25e0dbff9ab9bfa9d2905ebac3b5418e4e
Loks- <alexeypoyarkov@gmail.com>
2024-10-03 18:21:04 -0700

    Add comments.

--- common/string/utils/erase_all.h
+++ common/string/utils/erase_all.h
@@ -3,8 +3,18 @@
 #include <algorithm>
 #include <string>
 
+/**
+ * @brief Removes all occurrences of a character from a string.
+ *
+ * This function takes a string and a character, and returns a new string
+ * with all occurrences of the specified character removed.
+ *
+ * @param s The input string.
+ * @param c The character to remove from the string.
+ * @return A new string with all occurrences of the specified character removed.
+ */
 constexpr std::string EraseAll(const std::string& s, char c) {
-  std::string t(s);
-  t.erase(std::remove(t.begin(), t.end(), c), t.end());
-  return t;
+  std::string result(s);
+  result.erase(std::remove(result.begin(), result.end(), c), result.end());
+  return result;
 }
