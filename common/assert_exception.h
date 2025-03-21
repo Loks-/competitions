b3e58cd343627aebdfc4c0dee3b677dbc6321d55
Loks- <alexeypoyarkov@gmail.com>
2025-03-20 20:45:39 -0700

    Enhance Assert function with formatted message support and std::format integration.

--- common/assert_exception.h
+++ common/assert_exception.h
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <format>
 #include <iostream>
 #include <string>
 
@@ -27,9 +28,27 @@ inline void Assert(bool condition) {
  * @param message The message to display if the assertion fails.
  * @throws AssertException if the condition is false.
  */
-inline void Assert(bool condition, const std::string& message) {
+inline void Assert(bool condition, std::string_view message) {
   if (!condition) {
-    std::cerr << "Exception: " << message << std::endl;
+    std::cerr << std::format("Assertion failed: {}\n", message);
+    throw AssertException();
+  }
+}
+
+/**
+ * @brief Asserts that a condition is true, with a formatted message.
+ *
+ * @param condition The condition to check.
+ * @param format The format string.
+ * @param args The format arguments.
+ * @throws AssertException if the condition is false.
+ */
+template <typename... Args>
+inline void Assert(bool condition, std::format_string<Args...> format,
+                   Args&&... args) {
+  if (!condition) {
+    std::string message = std::format(format, std::forward<Args>(args)...);
+    std::cerr << std::format("Assertion failed: {}\n", message);
     throw AssertException();
   }
 }
