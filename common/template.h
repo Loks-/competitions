422175fc93102c38c960ad3651c1e964515e5338
Loks- <alexeypoyarkov@gmail.com>
2025-03-21 20:02:27 -0700

    Refactor template.h by renaming template classes and type aliases for improved clarity and consistency. Update documentation to reflect changes in naming conventions.

--- common/template.h
+++ common/template.h
@@ -6,57 +6,44 @@
  * This class is useful for template specialization and SFINAE techniques
  * where a minimal type is needed.
  */
-class TEmpty {};
+class MetaEmpty {};
 
 /**
  * @brief A template class used to represent a boolean constant in template
  * programming.
  *
- * @tparam B The boolean value.
+ * @tparam Value The boolean value.
  */
-template <bool B>
-class TMetaBool {};
+template <bool Value>
+class MetaBool {};
 
 /**
- * @brief Alias for TMetaBool<false>.
+ * @brief Alias for MetaBool<false>.
  *
  * Provides a convenient way to represent false in template metaprogramming.
  */
-using TMetaFalse = TMetaBool<false>;
+using MetaFalse = MetaBool<false>;
 
 /**
- * @brief Alias for TMetaBool<true>.
+ * @brief Alias for MetaBool<true>.
  *
  * Provides a convenient way to represent true in template metaprogramming.
  */
-using TMetaTrue = TMetaBool<true>;
+using MetaTrue = MetaBool<true>;
 
 /**
  * @brief A template class used to represent an unsigned integer constant in
  * template programming.
  *
- * @tparam N The unsigned integer value.
+ * @tparam Value The unsigned integer value.
  */
-template <unsigned N>
-class TMetaUnsigned {};
+template <unsigned Value>
+class MetaUnsigned {};
 
 /**
  * @brief A template class used to wrap a type in template programming.
  *
- * @tparam T The type.
+ * @tparam Type The type to wrap.
  */
-template <typename T>
-class TMetaType {};
-
-/**
- * @brief A function used to suppress unused variable warnings in debug builds
- * while allowing release builds to optimize them away.
- *
- * This function is particularly useful when dealing with debug-only variables
- * or parameters that are only used in debug builds.
- *
- * @tparam T The type of the parameter.
- * @param value The parameter to use.
- */
-template <typename T>
-constexpr void MetaUse([[maybe_unused]] const T& value) {}
+template <typename Type>
+class MetaType {};
