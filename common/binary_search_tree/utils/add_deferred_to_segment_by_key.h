705962bf6b7b2028e0eae089ef095cb7b0103e36
Loks- <alexeypoyarkov@gmail.com>
2025-08-16 21:59:52 -0700

    Sync style for add_deferred_to_segment_* functions.

---
+++ common/binary_search_tree/utils/add_deferred_to_segment_by_key.h
@@ -0,0 +1,59 @@
+#pragma once
+
+#include "common/base.h"
+
+namespace bst {
+namespace utils {
+
+/**
+ * @brief Adds a deferred operation to all nodes in a segment defined by key
+ * range.
+ *
+ * This function splits the tree into three parts based on the given key
+ * range:
+ * - Left part: nodes with keys less than 'begin'
+ * - Middle part: nodes with keys in the range [begin, end)
+ * - Right part: nodes with keys greater than or equal to 'end'
+ *
+ * The deferred operation is applied to all nodes in the middle segment, and
+ * then the three parts are rejoined to form the updated tree.
+ *
+ * Trivial cases are handled gracefully:
+ * - If root is nullptr, returns nullptr
+ * - If begin >= end, returns root unchanged
+ *
+ * @tparam TDeferred The deferred operation type that provides add method.
+ * @tparam TTree The tree type that provides split, join, and TNode types.
+ * @tparam TNode The node type.
+ * @tparam TDeferredValue The type of the deferred operation value to be added.
+ * @param root The root of the tree to modify.
+ * @param begin The beginning of the key range (inclusive).
+ * @param end The end of the key range (exclusive).
+ * @param deferred_value The deferred operation value to add to nodes in the
+ * segment.
+ * @return Pointer to the root of the modified tree.
+ */
+template <class TDeferred, class TTree, class TNode, class TDeferredValue>
+[[nodiscard]] constexpr TNode* add_deferred_to_segment_by_key(
+    TNode* root, const typename TTree::TKey& begin,
+    const typename TTree::TKey& end, const TDeferredValue& deferred_value) {
+  // Handle trivial cases
+  if (!root || !(begin < end)) return root;
+
+  TNode *left_part, *middle_part, *right_part;
+
+  // Split the tree at 'end' to get middle and right parts
+  TTree::split(root, end, middle_part, right_part);
+
+  // Split the middle part at 'begin' to get left and middle parts
+  TTree::split(middle_part, begin, left_part, middle_part);
+
+  // Apply the deferred operation to all nodes in the middle segment
+  if (middle_part) TDeferred::add(middle_part, deferred_value);
+
+  // Rejoin the three parts: (left_part + middle_part) + right_part
+  return TTree::join(TTree::join(left_part, middle_part), right_part);
+}
+
+}  // namespace utils
+}  // namespace bst
